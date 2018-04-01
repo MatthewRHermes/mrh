c4c7bfa9866a16710b2f9e0281b95c45bff5bf83
Matthew Hermes <herme068@umn.edu>
2018-04-01 15:56:15 -0500

    Added an alias for object mrh.dmet.dmet.dmet: it is now mrh.dmet.dmet_object, which makes just a little more sense.

--- dmet/__init__.py
+++ dmet/__init__.py
@@ -1,3 +1,8 @@
 # This is going to be a complicated overlapping set of code with the out-of-date, no-longer-supported QC-DMET by Sebastian Wouters,
 # Hung Pham's code, and my additions or modifications
 
+# I really shouldn't have to write, implicitly, mrh.dmet.dmet.dmet.  That's pretty ridiculous.  Let's alias that down
+
+import dmet
+dmet_object = dmet.dmet
+
