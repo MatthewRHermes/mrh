e097d2bdcba93a94e41d48bd491f2672b662a471
Matthew Hermes <herme068@umn.edu>
2018-05-11 18:36:53 -0500

    Change printed energy in pyscf_casscf.py

--- my_dmet/pyscf_casscf.py
+++ my_dmet/pyscf_casscf.py
@@ -52,7 +52,7 @@ def solve (frag, guess_1RDM, chempot_imp):
     mf.get_ovlp = lambda *args: np.eye(frag.norbs_imp)
     mf._eri = ao2mo.restore(8, frag.impham_TEI, frag.norbs_imp)
     mf.scf(guess_1RDM)
-    print ("CASSCF RHF-step energy: {0}".format (mf.e_tot))
+    print ("CASSCF RHF-step energy: {0}".format (mf.e_tot + frag.impham_CONST))
     #print(mf.mo_occ)    
     '''    
     idx = mf.mo_energy.argsort()
