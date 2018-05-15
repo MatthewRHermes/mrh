883bd6aa6ed13dacf870fc7001b4ba828816d055
Matthew Hermes <herme068@umn.edu>
2018-05-14 20:33:32 -0500

    MC-PDFT work

--- mcpdft_in_pyscf/mcpdft.py
+++ mcpdft_in_pyscf/mcpdft.py
@@ -1,50 +1,49 @@
 import numpy as np
-from pyscf.dft.numint import eval_rho, eval_ao 
-from pyscf.dft.libxc import eval_xc
 from mrh.mcpdft_in_pyscf.otpd import get_ontop_pair_density
 
-deriv_dict = {'LDA': 0,
-              'GGA': 1,
-              'MGGA': 2}
 
-def get_mcpdft_elec_energy (mc, ks, get_E_ot, oneCSDM_ao=None, rho=None, Pi=None):
-    ''' E_MCPDFT = h^p_q l^p_q + 1/2 v^pr_qs l^p_q l^r_s + E_ot[rho,Pi] 
+def get_mcpdft_elec_energy (ks, oneCDMs, twoCDM_amo, ao2amo, get_E_ot, max_memory=20000, hermi=0):
+    ''' E_MCPDFT = h_pq l_pq + 1/2 v_pqrs l_pq l_rs + E_ot[rho,Pi] 
         or, in other terms, 
-        E_MCPDFT = T_KS[rho] + V_ext[rho] + V_coul[rho] + E_ot[rho, Pi]
-                 = E_KSDFT[1rdm] - V_xc[rho] + E_ot[rho, Pi] 
+        E_MCPDFT = T_KS[rho] + E_ext[rho] + E_coul[rho] + E_ot[rho, Pi]
+                 = E_DFT[1rdm] - E_xc[rho] + E_ot[rho, Pi] 
         Args:
-            mc : an instance of a pyscf MC-SCF class
-            ks : an instance of a pyscf UKS class
-            get_E_ot : callable with args (ks, rho, Pi) 
-                which returns the on-top pair density exchange-correlation energy
+            ks : an instance of pyscf UKS class
+            oneCDMs : ndarray of shape (2, nao, nao)
+                containing spin-separated one-body density matrices
+            twoCDM_amo : ndarray of shape (ncas, ncas, ncas, ncas)
+                containing spin-summed two-body cumulant density matrix in an active space
+            ao2amo : ndarray of shape (nao, ncas)
+                containing molecular orbital coefficients for active-space orbitals
+            get_E_ot : callable which returns the on-top pair density exchange-correlation energy as a float
+                see mrh.mcpdft_in_pyscf.transfnal.get_E_ot_translated_rho.__doc__ for details of args
 
         Kwargs:
-            oneCSDM_ao : length 2 tuple of 2d ndarrays with shape=(nao, nao)
-                containing spin-separated one-body density matrices
-            rho : ndarray containing density and derivatives
-                see the documentation of pyscf.dft.numint.eval_rho and
-                pyscf.dft.libxc.eval_xc for its shape
-            Pi : 1d ndarray with shape=(ngrid),
-                containing on-top pair density
+            max_memory : int or float
+                maximum cache size in MB
+                default is 20000
+            hermi : int
+                1 if 1CDMs are assumed hermitian, 0 otherwise
+                default is 0
 
         Returns : float
             The MC-PDFT electronic energy (not including the internuclear constant) 
 
     '''
     xctype = ks._numint._xc_type
+    deriv = ['LDA', 'GGA', 'MGGA'].index (xctype)
 
-    if oneCSDM_ao is None:
-        oneCSDM_ao = mc.make_rdm1s ()
-    if (rho is None) or (Pi is None):
-        ao = eval_ao (ks.mol, ks.grids.coords, deriv=deriv_dict[xctype])
-    if rho is None:
-        rho = np.asarray ([eval_rho (ks.mol, ao, oneCDM, xctype=xctype) for oneCDM in oneCSDM_ao])
-    if Pi is None:
-        Pi = get_ontop_pair_density (mc, ks, rho=rho, ao=ao)
-
-    E_DFT = ks.energy_elec (dm=oneCSDM_ao)
+    E_DFT = ks.energy_elec (dm=oneCDMs)
     E_xc = ks.get_veff (dm=oneCDSM_ao).exc
-    E_ot = get_E_ot (ks, rho, Pi)
-    return E_DFT - E_xc + E_ot
+    E_ot = 0.0
+
+    make_rho = tuple (ks._numint._gen_rho_evaluator (ks.mol, oneCDMs[i,:,:], hermi) for i in range(2))
+    for ao, mask, weight, coords in ks._numint.block_loop (ks.mol, ks.grids, norbs_ao, deriv, max_memory):
+        rho = nd.asarray ([m (0, ao, mask, xctype) for m in make_rho])
+        Pi = get_ontop_pair_density (rho, ao, twoCDM_amo, ao2amo, deriv)        
+        E_ot += get_E_ot (ks, rho, Pi, weight)
+
+    E_MCPDFT = E_DFT - E_xc + E_ot
+    return E_MCPDFT
     
 
--- mcpdft_in_pyscf/otpd.py
+++ mcpdft_in_pyscf/otpd.py
@@ -1,58 +1,61 @@
 import numpy as np
 from mrh.util.rdm import get_2CDM_from_2RDM
-from pyscf.dft.numint import eval_rho, eval_ao 
 from itertools import product
 
-deriv_dict = {'LDA': 0,
-              'GGA': 1,
-              'MGGA': 2}
 
-def get_ontop_pair_density (mc, ks, rho=None, ao=None):
-    r''' Pi(r) = i(r)*j(r)*k(r)*l(r)*g^ik_jl 
-               = rho_up(r)*rho_down(r) + i(r)*j(r)*k(r)*l(r)*l^ik_jl
+
+def get_ontop_pair_density (rho, ao, twoCDM_amo, ao2amo, deriv=0):
+    r''' Pi(r) = i(r)*j(r)*k(r)*l(r)*g_ijkl 
+               = rho[0](r)*rho[1](r) + i(r)*j(r)*k(r)*l(r)*l_ijkl
 
         Args:
-            mc : an instance of a pyscf MC-SCF class
-            ks : an instance of a pyscf UKS class
+            rho : ndarray of shape (2,*,ngrids) 
+                contains spin density [and derivatives] 
+            ao : ndarray of shape (*, ngrids, nao)
+                contains values of aos [and derivatives] 
+            twoCDM_amo : ndarray of shape (mc.ncas, mc.ncas, mc.ncas, mc.ncas)
+                contains spin-summed two-body cumulant density matrix in active space
+            ao2amo : ndarray of shape (nao, ncas)
+                molecular-orbital coefficients for active-space orbitals
 
         Kwargs:
-            rho : ndarray containing density and derivatives
-                see the documentation of pyscf.dft.numint.eval_rho and
-                pyscf.dft.libxc.eval_xc for its shape 
-            ao : ndarray containing values and derivatives of atomic orbitals at grid points
-                see the documentation of pyscf.dft.numint.eval_ao for its shape
+            deriv : derivative order through which to calculate. Default is 0. 
+                deriv > 1 not implemented
 
-        Returns : ndarray of the same shape as rho
-            The on-top pair density and its derivatives
+        Returns : ndarray of shape (ngrids) if deriv=0 and (4, ngrids) if deriv=1
+            The on-top pair density and its derivatives if requested
+
     '''
-    xctype = ks._numint._xc_type
-    deriv = deriv_dict[xctype]
     if deriv > 1:
-        raise NotImplementedError("meta-GGA translated functionals")
+        raise NotImplementedError("on-top density derivatives of order > 1")
+    assert (rho.ndim == ao.ndim), "rho.shape={0}; ao.shape={1}".format (rho.shape, ao.shape)
 
-    if ao is None:
-        # This is probably too big an object for most molecules
-        ao = eval_ao (ks.mol, ks.grids.coords, deriv=deriv)
-    if rho is None:
-        rho = np.asarray ([eval_rho (ks.mol, ao, oneCDM, xctype=xctype) for oneCDM in mc.make_rdm1s ()])
-    ngrids = rho.shape[-1]
-    norbs_ao = ao.shape[-1]
+    # Fix dimensionality of rho and ao
+    if rho.ndim == 2:
+        rho = rho.reshape (rho.shape[0], 1, rho.shape[1])
+        ao = ao.reshape (1, ao.shape[0], ao.shape[1])
 
+    # First cumulant and derivatives (chain rule! product rule!)
     Pi = np.zeros_like (rho[0])
+    Pi[0] = rho[0,0] * rho[1,0]
+    for ideriv in range(1,deriv):
+        Pi[ideriv] = (rho[0,ideriv] * rho[1,0] 
+                    + rho[0,0] * rho[1,ideriv])
 
-    # First cumulant
-    Pi = np.multiply (rho[0], rho[1])
+    # Second cumulant and derivatives (chain rule! product rule!)
+    grid2amo = np.einsum ('ijk,kl->ijl', ao, ao2amo)
+    Pi[0] += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo[0], grid2amo[0], grid2amo[0], grid2amo[0])
+    for ideriv in range(1, deriv):
+        Pi[ideriv] += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo[ideriv], grid2amo[0], grid2amo[0], grid2amo[0])
+        Pi[ideriv] += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo[0], grid2amo[ideriv], grid2amo[0], grid2amo[0])
+        Pi[ideriv] += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo[0], grid2amo[0], grid2amo[ideriv], grid2amo[0])
+        Pi[ideriv] += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo[0], grid2amo[0], grid2amo[0], grid2amo[ideriv])
 
-    # Second cumulant
-    ao2amo = mc.mo_coeff[:,mc.ncore:mc.ncore+mc.ncas]
-    grid2ao = ao.reshape (product (ao.shape[:-1]), ao.shape[-1])
-    grid2amo = np.dot (grid2ao, ao2amo)
-    oneCDM_amo, twoRDM_amo = mc.fcisolver.make_rdm12 ()
-    twoCDM_amo = get_2CDM_from_2RDM (twoRDM_amo, oneCDM_amo)
-    oldshape = grid2amo.shape
-    newshape = (product(oldshape[:-1]), oldshape[-1])
-    Pi_finalshape = oldshape[:-1]
-    Pi += np.einsum ('ijkl,ai,aj,ak,al->a', twoCDM_amo, grid2amo, grid2amo, grid2amo, grid2amo).reshape (rho.shape)
+    # Unfix dimensionality of rho, ao, and Pi
+    if Pi.shape[0] == 1:
+        Pi = Pi.reshape (Pi.shape[1])
+        rho = rho.reshape (rho.shape[0], rho.shape[2])
+        ao = ao.reshape (ao.shape[1], ao.shape[2])
 
     return Pi
 
