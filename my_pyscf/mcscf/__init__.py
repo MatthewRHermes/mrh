deb446f9382eaf23b1336b76f0766e2eea476330
Matthew Hermes <herme068@umn.edu>
2018-05-25 18:40:08 -0500

    Fix oversight in my_pyscf/mcscf/__init__.py

--- my_pyscf/mcscf/__init__.py
+++ my_pyscf/mcscf/__init__.py
@@ -1,11 +1,11 @@
 # Ohh boy
-from mrh.my_pyscf.mcscf import mc1step_inact
+from mrh.my_pyscf.mcscf import mc1step_constrained
 from pyscf.mcscf import addons
 
 def constrCASSCF(mf, ncas, nelecas, **kwargs):
     from pyscf import scf
     mf = scf.addons.convert_to_rhf(mf)
-    return mc1step_inact.CASSCF (mf, ncas, nelecas, **kwargs)
+    return mc1step_constrained.CASSCF (mf, ncas, nelecas, **kwargs)
 
 constrRCASSCF = constrCASSCF
 
