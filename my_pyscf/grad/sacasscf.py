7d239a6a6af434e8f8516f292a7a0acc115ada3c
Matthew R Hermes <herme068@herme068.umn.edu>
2019-04-16 19:57:08 -0500

    Some errors in L_dot_dE functions fixed

--- my_pyscf/grad/sacasscf.py
+++ my_pyscf/grad/sacasscf.py
@@ -94,7 +94,8 @@ def Lorb_dot_dgorb_dx (Lorb, mc, mo_coeff=None, ci=None, atmlst=None, mf_grad=No
     # MRH: end new density matrix terms
     # MRH: wrap the integral instead of the density matrix. I THINK the sign is the same!
     # mo sets 0 and 2 should be transposed, 1 and 3 should be not transposed; this will lead to correct sign
-    # Except I can't do this for the external index, because the external index is contracted 
+    # Except I can't do this for the external index, because the external index is contracted to ovlp matrix,
+    # not the 2RDM
     aapaL  = ao2mo.kernel(mol, (moL_cas, mo_cas, mo_coeff, mo_cas), compact=False)
     aapaL += ao2mo.kernel(mol, (mo_cas, moL_cas, mo_coeff, mo_cas), compact=False) 
     aapaL += ao2mo.kernel(mol, (mo_cas, mo_cas, mo_coeff, moL_cas), compact=False) 
@@ -200,8 +201,7 @@ def Lci_dot_dgci_dx (Lci, weights, mc, mo_coeff=None, ci=None, atmlst=None, mf_g
     This involves removing all core-core and nuclear-nuclear terms and making the substitution
     sum_I w_I<L_I|p'q|I> + c.c. -> <0|p'q|0>
     sum_I w_I<L_I|p'r'sq|I> + c.c. -> <0|p'r'sq|0>
-    The active-core terms (sum_I w_I<L_I|x'iyi|I>, sum_I w_I <L_I|x'iiy|I>, c.c.) must be retained.
-    For simplicity the Lci should be weight-summed already on entry. '''
+    The active-core terms (sum_I w_I<L_I|x'iyi|I>, sum_I w_I <L_I|x'iiy|I>, c.c.) must be retained.'''
     if mo_coeff is None: mo_coeff = mc.mo_coeff
     if ci is None: ci = mc.ci
     if mf_grad is None: mf_grad = mc._scf.nuc_grad_method()
@@ -235,17 +235,19 @@ def Lci_dot_dgci_dx (Lci, weights, mc, mo_coeff=None, ci=None, atmlst=None, mf_g
 # gfock = Generalized Fock, Adv. Chem. Phys., 69, 63
     dm_core = np.dot(mo_core, mo_core.T) * 2
     dm_cas = reduce(np.dot, (mo_cas, casdm1, mo_cas.T))
-    aapa = ao2mo.kernel(mol, (mo_cas, mo_cas, mo_occ, mo_cas), compact=False)
-    aapa = aapa.reshape(ncas,ncas,nocc,ncas)
+    aapa = ao2mo.kernel(mol, (mo_cas, mo_cas, mo_coeff, mo_cas), compact=False)
+    aapa = aapa.reshape(ncas,ncas,nmo,ncas)
     vj, vk = mc._scf.get_jk(mol, (dm_core, dm_cas))
     h1 = mc.get_hcore()
     vhf_c = vj[0] - vk[0] * .5
     vhf_a = vj[1] - vk[1] * .5
     # MRH: delete h1 + vhf_c from the first line below (core and core-core stuff)
-    gfock = reduce(np.dot, (mo_occ.T, vhf_a, mo_occ)) * 2
-    gfock[:,ncore:nocc] = reduce(np.dot, (mo_occ.T, h1 + vhf_c, mo_cas, casdm1))
-    gfock[:,ncore:nocc] += np.einsum('uviw,vuwt->it', aapa, casdm2)
-    dme0 = reduce(np.dot, (mo_occ, (gfock+gfock.T)*.5, mo_occ.T))
+    # Also extend gfock to span the whole space
+    gfock = np.zeros_like (dm_cas)
+    gfock[:nocc,:nocc]   = reduce(np.dot, (mo_occ.T, vhf_a, mo_occ)) * 2
+    gfock[:,ncore:nocc]  = reduce(np.dot, (mo_coeff.T, h1 + vhf_c, mo_cas, casdm1))
+    gfock[:,ncore:nocc] += np.einsum('uvpw,vuwt->pt', aapa, casdm2)
+    dme0 = reduce(np.dot, (mo_coeff, (gfock+gfock.T)*.5, mo_coeff.T))
     aapa = vj = vk = vhf_c = vhf_a = h1 = gfock = None
 
     vhf1c, vhf1a = mf_grad.get_veff(mol, (dm_core, dm_cas))
@@ -290,7 +292,7 @@ def Lci_dot_dgci_dx (Lci, weights, mc, mo_coeff=None, ci=None, atmlst=None, mf_g
                              shls_slice=shls_slice).reshape(3,p1-p0,nf,nao_pair)
             de_eri[k] -= np.einsum('xijw,ijw->x', eri1, dm2_ao) * 2
             eri1 = None
-        # MRH: dm1 -> dm_cas in the line below
+        # MRH: dm1 -> dm_cas in the line below. Also eliminate core-core terms
         de_eri[k] += np.einsum('xij,ij->x', vhf1c[:,p0:p1], dm_cas[p0:p1]) * 2
         de_eri[k] += np.einsum('xij,ij->x', vhf1a[:,p0:p1], dm_core[p0:p1]) * 2
 
@@ -384,12 +386,15 @@ class Gradients (lagrange.Gradients):
         if atmlst is None: atmlst = self.atmlst
         if verbose is None: verbose = self.verbose
         if mo is None: mo = self.base.mo_coeff
-        if ci is None: ci = self.base.ci[iroot]
+        if ci is None: ci = self.base.ci
         if eris is None and self.eris is None:
             eris = self.eris = self.base.ao2mo (mo)
         elif eris is None:
             eris = self.eris
-        return casscf_grad.kernel (self.base, mo_coeff=mo, ci=ci, atmlst=atmlst, mf_grad=mf_grad, verbose=verbose)
+        fcasscf = self.make_fcasscf ()
+        fcasscf.mo_coeff = mo
+        fcasscf.ci = ci[iroot]
+        return casscf_grad.kernel (fcasscf, mo_coeff=mo, ci=ci[iroot], atmlst=atmlst, mf_grad=mf_grad, verbose=verbose)
 
     def get_LdotJnuc (self, Lvec, iroot=None, atmlst=None, verbose=None, mo=None, ci=None, eris=None, mf_grad=None, **kwargs):
         if iroot is None: iroot = self.iroot
@@ -444,24 +449,36 @@ class Gradients (lagrange.Gradients):
         nelecas = self.base.nelecas
         nocc = ncore + ncas
         nlag = self.nlag
+        ci = np.asarray (self.base.ci).reshape (nroots, -1)
+        err = Aop (Lvec) + bvec
+        eorb = self.base.unpack_uniq_var (err[:ngorb])
+        eci = err[ngorb:].reshape (nroots, -1)
         borb = self.base.unpack_uniq_var (bvec[:ngorb])
         bci = bvec[ngorb:].reshape (nroots, -1)
         Lorb = self.base.unpack_uniq_var (Lvec[:ngorb])
         Lci = Lvec[ngorb:].reshape (nroots, ndet)
-        Lci_ci_ovlp = np.asarray (ci).reshape (nroots,-1) @ Lci.conjugate ().T
-        Lci_Lci_ovlp = Lci @ Lci.conjugate ().T
+        Lci_ci_ovlp = np.asarray (ci).reshape (nroots,-1).conjugate () @ Lci.T
+        Lci_Lci_ovlp = Lci.conjugate () @ Lci.T
+        ci_ci_ovlp = ci.conjugate () @ ci.T
         lib.logger.debug (self, "{} gradient RHS, inactive-active orbital rotations:\n{}".format (
             self.base.__class__.__name__, borb[:ncore,ncore:nocc]))
         lib.logger.debug (self, "{} gradient RHS, inactive-external orbital rotations:\n{}".format (
             self.base.__class__.__name__, borb[:ncore,nocc:]))
         lib.logger.debug (self, "{} gradient RHS, active-external orbital rotations:\n{}".format (
             self.base.__class__.__name__, borb[ncore:nocc,nocc:]))
+        lib.logger.debug (self, "{} gradient residual, inactive-active orbital rotations:\n{}".format (
+            self.base.__class__.__name__, eorb[:ncore,ncore:nocc]))
+        lib.logger.debug (self, "{} gradient residual, inactive-external orbital rotations:\n{}".format (
+            self.base.__class__.__name__, eorb[:ncore,nocc:]))
+        lib.logger.debug (self, "{} gradient residual, active-external orbital rotations:\n{}".format (
+            self.base.__class__.__name__, eorb[ncore:nocc,nocc:]))
         lib.logger.debug (self, "{} gradient Lagrange factor, inactive-active orbital rotations:\n{}".format (
             self.base.__class__.__name__, Lorb[:ncore,ncore:nocc]))
         lib.logger.debug (self, "{} gradient Lagrange factor, inactive-external orbital rotations:\n{}".format (
             self.base.__class__.__name__, Lorb[:ncore,nocc:]))
         lib.logger.debug (self, "{} gradient Lagrange factor, active-external orbital rotations:\n{}".format (
             self.base.__class__.__name__, Lorb[ncore:nocc,nocc:]))
+        '''
         lib.logger.debug (self, "{} gradient RHS, inactive-inactive orbital rotations (redundant!):\n{}".format (
             self.base.__class__.__name__, borb[:ncore,:ncore]))
         lib.logger.debug (self, "{} gradient RHS, active-active orbital rotations (redundant!):\n{}".format (
@@ -474,14 +491,23 @@ class Gradients (lagrange.Gradients):
             self.base.__class__.__name__, Lorb[ncore:nocc,ncore:nocc]))
         lib.logger.debug (self, "{} gradient Lagrange factor, external-external orbital rotations (redundant!):\n{}".format (
             self.base.__class__.__name__, Lorb[nocc:,nocc:]))
+        '''
         lib.logger.debug (self, "{} gradient Lagrange factor, CI part overlap with true CI SA space:\n{}".format ( 
             self.base.__class__.__name__, Lci_ci_ovlp))
         lib.logger.debug (self, "{} gradient Lagrange factor, CI part self overlap matrix:\n{}".format ( 
             self.base.__class__.__name__, Lci_Lci_ovlp))
+        lib.logger.debug (self, "{} gradient Lagrange factor, CI vector self overlap matrix:\n{}".format ( 
+            self.base.__class__.__name__, ci_ci_ovlp))
         neleca, nelecb = _unpack_nelec (nelecas)
         spin = neleca - nelecb + 1
         csf = CSFTransformer (ncas, neleca, nelecb, spin)
+        ecsf = csf.vec_det2csf (eci, normalize=False, order='C')
+        err_norm_det = linalg.norm (err)
+        err_norm_csf = linalg.norm (np.append (eorb, ecsf.ravel ()))
+        lib.logger.debug (self, "{} gradient: determinant residual = {}, CSF residual = {}".format (
+            self.base.__class__.__name__, err_norm_det, err_norm_csf))
         bci_lbls, bci_csf = csf.printable_largest_csf (bci, 10, isdet=True, normalize=False, order='C')
+        eci_lbls, eci_csf = csf.printable_largest_csf (eci, 10, isdet=True, normalize=False, order='C')
         Lci_lbls, Lci_csf = csf.printable_largest_csf (Lci, 10, isdet=True, normalize=False, order='C')
         ncsf = bci_csf.shape[1]
         for iroot in range (self.nroots):
@@ -490,6 +516,9 @@ class Gradients (lagrange.Gradients):
             lib.logger.debug (self, "CI gradient:")
             for icsf in range (ncsf):
                 lib.logger.debug (self, '{} {}'.format (bci_lbls[iroot,icsf], bci_csf[iroot,icsf]))
+            lib.logger.debug (self, "CI residual:")
+            for icsf in range (ncsf):
+                lib.logger.debug (self, '{} {}'.format (eci_lbls[iroot,icsf], eci_csf[iroot,icsf]))
             lib.logger.debug (self, "CI Lagrange vector:")
             for icsf in range (ncsf):
                 lib.logger.debug (self, '{} {}'.format (Lci_lbls[iroot,icsf], Lci_csf[iroot,icsf]))
@@ -527,6 +556,7 @@ class Gradients (lagrange.Gradients):
         AciD = Adiag[self.ngorb:].reshape (self.nroots, -1)
         eorb = self.e_avg
         eci = self.e_states 
+        '''
         Aorb = np.zeros ((self.ngorb, self.ngorb))
         fdum = np.zeros ((self.nlag))
         for idum in range (self.ngorb):
@@ -534,10 +564,10 @@ class Gradients (lagrange.Gradients):
             Aorb[idum,:] = Aop (fdum)[:self.ngorb]
             fdum[idum] = 0
         Aorb_inv = linalg.inv (Aorb)
-        Rorb = AorbD - (eorb - level_shift) 
+        '''
+        Rorb = AorbD.copy () 
         Rorb[abs(Rorb)<1e-8] = 1e-8
         Rorb = 1./Rorb
-        Rorb = Aorb_inv
         Rci = AciD - ((eci - level_shift) * self.weights)[:,None]
         Rci[abs(Rci)<1e-8] = 1e-8
         Rci = 1./Rci
@@ -555,7 +585,7 @@ class Gradients (lagrange.Gradients):
         def my_precond (x):
             # Orb part
             xorb = x[:self.ngorb]
-            xorb = Rorb @ xorb
+            xorb = Rorb * xorb
 
             # CI part
             xci = x[self.ngorb:].reshape (self.nroots, -1)
