f57a6439055b893f0e8acdaf6436ab9fd2cb148d
RiddhishPandharkar <pandh009@umn.edu>
2019-06-20 12:06:36 -0500

    No newton solver called

--- my_dmet/pyscf_casscf.py
+++ my_dmet/pyscf_casscf.py
@@ -224,20 +224,21 @@ def solve (frag, guess_1RDM, chempot_imp):
     t_start = time.time()
     E_CASSCF = mc.kernel(imp2mo, ci0)[0]
     if not mc.converged:
-        if np.any (np.abs (frag.impham_OEI_S) > 1e-8):
-            raise NotImplementedError('Gradient and Hessian fixes for nonsinglet environment of Newton-descent CASSCF algorithm')
-        mc = mc.newton ()
-        E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
-    if not mc.converged:
+#        if np.any (np.abs (frag.impham_OEI_S) > 1e-8):
+#            raise NotImplementedError('Gradient and Hessian fixes for nonsinglet environment of Newton-descent CASSCF algorithm')
+#            mc = mc.newton ()
+#        E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
+#    if not False:
         print ('Assuming ci vector is poisoned; discarding...')
         imp2mo = mc.mo_coeff.copy ()
         mc = mcscf.CASSCF(mf, CASorb, CASe)
+        mc.max_cycle_macro = 50 if frag.imp_maxiter is None else frag.imp_maxiter  ##added by Riddhish. Encore first order have same macros as first
         smult = abs_2S + 1 if frag.target_S is not None else (frag.nelec_imp % 2) + 1
         mc.fcisolver = csf_solver (mf.mol, smult)
         E_CASSCF = mc.kernel(imp2mo)[0]
-        if not mc.converged:
-            mc = mc.newton ()
-            E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
+#        if not mc.converged:
+#            mc = mc.newton ()
+#            E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
     assert (mc.converged)
 
     '''
