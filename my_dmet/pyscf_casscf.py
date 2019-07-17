06c63474b51b3354beec60854c44ae3957446f7b
RiddhishPandharkar <pandh009@umn.edu>
2019-07-17 16:07:16 -0500

    newton baypass with minimal change in code first call to the newton is made only when there is no spin potential. If newton is called the second time, it WILL raise the implementation error for spin potential.

--- my_dmet/pyscf_casscf.py
+++ my_dmet/pyscf_casscf.py
@@ -224,17 +224,7 @@ def solve (frag, guess_1RDM, chempot_imp):
 
     t_start = time.time()
     E_CASSCF = mc.kernel(imp2mo, ci0)[0]
-    if not mc.converged:
-        print ('Restarting another first order mc solver...')  ##RP added another attempt at a 1st order solver with the same number of iterations before we go to the newton solver. 
-        imp2mo = mc.mo_coeff.copy ()
-        mc = mcscf.CASSCF(mf, CASorb, CASe)
-        smult = abs_2S + 1 if frag.target_S is not None else (frag.nelec_imp % 2) + 1
-        mc.fcisolver = csf_solver (mf.mol, smult)
-        E_CASSCF = mc.kernel(imp2mo)[0]
-
-    if not mc.converged:
-        if np.any (np.abs (frag.impham_OEI_S) > 1e-8):
-            raise NotImplementedError('Gradient and Hessian fixes for nonsinglet environment of Newton-descent CASSCF algorithm')
+    if not ( mc.converged and np.all (np.abs (frag.impham_OEI_S) < 1e-8) ):
         mc = mc.newton ()
         E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
     if not mc.converged:
@@ -245,6 +235,8 @@ def solve (frag, guess_1RDM, chempot_imp):
         mc.fcisolver = csf_solver (mf.mol, smult)
         E_CASSCF = mc.kernel(imp2mo)[0]
         if not mc.converged:
+            if np.any (np.abs (frag.impham_OEI_S) > 1e-8):
+                raise NotImplementedError('Gradient and Hessian fixes for nonsinglet environment of Newton-descent CASSCF algorithm')
             mc = mc.newton ()
             E_CASSCF = mc.kernel(mc.mo_coeff, mc.ci)[0]
     assert (mc.converged)
