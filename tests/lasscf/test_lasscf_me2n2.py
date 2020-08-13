154ac69fe517512b784dfd31d36330e3f5ec4aef
Matthew R Hermes <herme068@herme068.umn.edu>
2020-08-13 11:20:23 -0500

    Simplify Me2N2 test scripting

--- tests/lasscf/test_lasscf_me2n2.py
+++ tests/lasscf/test_lasscf_me2n2.py
@@ -18,7 +18,43 @@ import unittest
 import numpy as np
 from pyscf import lib, gto, scf, dft, fci, mcscf, df
 from me2n2_struct import structure as struct
-import me2n2_driver as driver
+from mrh.my_dmet import localintegrals, dmet, fragments
+from mrh.my_dmet.fragments import make_fragment_atom_list, make_fragment_orb_list
+
+def run (mf, CASlist=None, **kwargs):
+    # I/O
+    # --------------------------------------------------------------------------------------------------------------------
+    mol = mf.mol
+    my_kwargs = {'calcname':           'me2n2_lasscf',
+                 'doLASSCF':           True,
+                 'debug_energy':       False,
+                 'debug_reloc':        False,
+                 'nelec_int_thresh':   1e-5,
+                 'num_mf_stab_checks': 0}
+    bath_tol = 1e-8
+    my_kwargs.update (kwargs)
+    
+    # Set up the localized AO basis
+    # --------------------------------------------------------------------------------------------------------------------
+    myInts = localintegrals.localintegrals(mf, range(mol.nao_nr ()), 'meta_lowdin')
+    
+    # Build fragments from atom list
+    # --------------------------------------------------------------------------------------------------------------------
+    N2 = make_fragment_atom_list (myInts, list (range(2)), 'CASSCF(4,4)', name="N2")
+    N2.target_S = N2.target_MS = mol.spin // 2
+    Me1 = make_fragment_atom_list (myInts, list (range(2,6)), 'RHF', name='Me1')
+    Me2 = make_fragment_atom_list (myInts, list (range(6,10)), 'RHF', name='Me2')
+    N2.bath_tol = Me1.bath_tol = Me2.bath_tol = bath_tol
+    fraglist = [N2, Me1, Me2] 
+    
+    # Generate active orbital guess 
+    # --------------------------------------------------------------------------------------------------------------------
+    me2n2_dmet = dmet (myInts, fraglist, **my_kwargs)
+    me2n2_dmet.generate_frag_cas_guess (mf.mo_coeff, caslst=CASlist, force_imp=True, confine_guess=False)
+    
+    # Calculation
+    # --------------------------------------------------------------------------------------------------------------------
+    return me2n2_dmet.doselfconsistent ()
 
 r_nn = 3.0
 mol = struct (3.0, '6-31g')
@@ -42,16 +78,16 @@ def tearDownModule():
 
 class KnownValues(unittest.TestCase):
     def test_lasscf (self):
-        self.assertAlmostEqual (driver.run (mf), mc.e_tot, 7)
+        self.assertAlmostEqual (run (mf), mc.e_tot, 7)
 
     def test_lasscf_df (self):
-        self.assertAlmostEqual (driver.run (mf_df), mc_df.e_tot, 7)
+        self.assertAlmostEqual (run (mf_df), mc_df.e_tot, 7)
 
     def test_lasscf_hs (self):
-        self.assertAlmostEqual (driver.run (mf_hs), mf_hs.e_tot, 7)
+        self.assertAlmostEqual (run (mf_hs), mf_hs.e_tot, 7)
 
     def test_lasscf_hs_df (self):
-        self.assertAlmostEqual (driver.run (mf_hs_df), mf_hs_df.e_tot, 7)
+        self.assertAlmostEqual (run (mf_hs_df), mf_hs_df.e_tot, 7)
 
 if __name__ == "__main__":
     print("Full Tests for LASSCF me2n2")
