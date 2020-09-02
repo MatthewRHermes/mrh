Fix Hessian error in lasci.py

my_pyscf/mcscf/lasci.py
@@ -183,15 +183,17 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
         h1s = las.get_hcore ()[None,:,:] + veff
         h1s = np.dot (h1s, mo_coeff)
         self.h1s = np.dot (moH_coeff, h1s).transpose (1,0,2)
-        self.h1frs = np.stack ([self.h1s[:,ncore:nocc,ncore:nocc].copy (),] * len (self.casdm1fs), axis=0)
-        for ix, casdm1s in enumerate (self.casdm1fs):
+        self.h1frs = np.zeros ((len (self.casdm1frs), self.nroots, 2, ncas, ncas), dtype=self.dtype)
+        self.h1frs[:,:,:,:,:] = self.h1s[None,None,:,ncore:nocc,ncore:nocc].copy ()
+        for ix, casdm1rs in enumerate (self.casdm1frs):
             i = sum (ncas_sub[:ix])
             j = i + ncas_sub[ix]
-            casdm1 = casdm1s[0] + casdm1s[1]
-            self.h1frs[ix,:,:,:] -= np.tensordot (casdm1,
-                self.eri_cas[i:j,i:j,:,:], axes=2)[None,:,:] # double-counting: J
-            self.h1frs[ix,:,:,:] += np.tensordot (casdm1s,
-                self.eri_cas[:,i:j,i:j,:], axes=((1,2),(2,1))) # double-counting: K
+            for jx, casdm1s in enumerate (casdm1rs):
+                casdm1 = casdm1s[0] + casdm1s[1]
+                self.h1frs[ix,jx,:,:,:] -= np.tensordot (casdm1,
+                    self.eri_cas[i:j,i:j,:,:], axes=2)[None,:,:] # double-counting: J
+                self.h1frs[ix,jx,:,:,:] += np.tensordot (casdm1s,
+                    self.eri_cas[:,i:j,i:j,:], axes=((1,2),(2,1))) # double-counting: K
 
         # Fock1 matrix (for gradient and subtrahend terms in Hx)
         self.fock1 = sum ([f @ d for f,d in zip (list (self.h1s), list (self.dm1s))])
@@ -201,14 +203,14 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
         h1 = (self.h1s + (moH_coeff @ las.get_hcore () @ mo_coeff)[None,:,:]) / 2
         self.e_tot = las.energy_nuc () + np.dot (h1.ravel (), self.dm1s.ravel ()) + np.tensordot (self.eri_cas, self.casdm2c, axes=4) / 2
 
-        # CI stuff
+        # CI stuff TODO: SA-LASSCF generalization work
         self.linkstrl = []
         self.linkstr = []
         for fcibox, no, ne, csf in zip (self.fciboxes, ncas_sub, nelecas_sub, self.ugg.ci_transformers):
             fcibox.orbsym = csf.orbsym
             self.linkstrl.append (fcibox.states_gen_linkstr (no, ne, True)) 
             self.linkstr.append (fcibox.states_gen_linkstr (no, ne, False))
-        self.hci0 = self.Hci_all ([0.0,] * len (ci), self.h1frs, self.eri_cas, ci)
+        self.hci0 = self.Hci_all ([0.0,] * len (ci), self.h1frs[:,0], self.eri_cas, ci)
         self.e0 = [hc.dot (c) for hc, c in zip (self.hci0, ci)]
         self.hci0 = [hc - c*e for hc, c, e in zip (self.hci0, ci, self.e0)]
 
@@ -230,25 +232,27 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
     def shape (self):
         return ((self.ugg.nvar_tot, self.ugg.nvar_tot))
 
-    def Hci (self, fcibox, no, ne, h0e, h1s, h2e, ci, linkstrl=None):
+    def Hci (self, fcibox, no, ne, h0r, h1rs, h2, ci, linkstrl=None):
+        h0r = [h0r] # TODO: extra dimension (here and in hc below)
         ci = [ci] # TODO: extra dimension (here and in hc below)
-        h1s = [h1s]
-        h = fcibox.states_absorb_h1e (h1s, h2e, no, ne, 0.5)
-        hc = fcibox.states_contract_2e (h, ci, no, ne, link_index=linkstrl)[0].ravel ()
-        return hc
+        h1rs = [h1rs]
+        hr = fcibox.states_absorb_h1e (h1rs, h2, no, ne, 0.5)
+        hcr = fcibox.states_contract_2e (hr, ci, no, ne, link_index=linkstrl)
+        hcr = [hc + (h0 * c) for hc, h0, c in zip (hcr, h0r, ci)]
+        return hcr[0]
 
-    def Hci_all (self, h0e_sub, h1frs, h2e, ci_sub):
-        ''' Assumes h2e is in the active superspace MO basis and h1frs is in the full MO basis '''
+    def Hci_all (self, h0fr, h1frs, h2, ci_sub):
+        ''' Assumes h2 is in the active superspace MO basis and h1frs is in the full MO basis '''
         hc = []
-        for isub, (fcibox, h0e, h1s, ci) in enumerate (zip (self.fciboxes, h0e_sub, h1frs, ci_sub)):
+        for isub, (fcibox, h0, h1s, ci) in enumerate (zip (self.fciboxes, h0fr, h1frs, ci_sub)):
             if self.linkstrl is not None: linkstrl = self.linkstrl[isub] 
             ncas = self.ncas_sub[isub]
             nelecas = self.nelecas_sub[isub]
             i = sum (self.ncas_sub[:isub])
             j = i + ncas
-            h2e_i = h2e[i:j,i:j,i:j,i:j]
-            h1e_i = h1s[:,i:j,i:j]
-            hc.append (self.Hci (fcibox, ncas, nelecas, h0e, h1e_i, h2e_i, ci, linkstrl=linkstrl))
+            h2_i = h2[i:j,i:j,i:j,i:j]
+            h1s_i = h1s[:,i:j,i:j]
+            hc.append (self.Hci (fcibox, ncas, nelecas, h0, h1s_i, h2_i, ci, linkstrl=linkstrl))
         return hc
 
     def make_odm1s2c_sub (self, kappa):
@@ -306,7 +310,7 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
         return tdm1frs, tdm2c    
 
     def get_veff_Heff (self, odm1s_sub, tdm1s_sub):
-        ''' Returns the veff for the orbital part and the h1e shifts for the CI part arising from the contraction
+        ''' Returns the veff for the orbital part and the h1s shifts for the CI part arising from the contraction
         of shifted or 'effective' 1-rdms in the two sectors with the Hamiltonian. Return values do not include
         veffs with the external indices rotated (i.e., in the CI part). Uses the cached eris for the latter in the hope that
         this is faster than calling get_jk with many dms. '''
@@ -431,13 +435,13 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
         I do NOT add h.c. (multiply by 2) at the end. '''
         ncore, nocc = self.ncore, self.nocc
         kappa1_cas = kappa1[ncore:nocc, ncore:nocc]
-        h1s = np.dot (self.h1frs, kappa1_cas)
+        h1s = np.dot (self.h1frs[:,0], kappa1_cas) # TODO: SA-LASSCF
         h1s += h1s.transpose (0,1,3,2)
-        h2e = np.dot (self.eri_cas, kappa1_cas)
-        h2e += h2e.transpose (2,3,0,1)
-        h2e += h2e.transpose (1,0,3,2)
+        h2 = np.dot (self.eri_cas, kappa1_cas)
+        h2 += h2.transpose (2,3,0,1)
+        h2 += h2.transpose (1,0,3,2)
         h1s += h1s_prime
-        Kci0 = self.Hci_all ([0.0,] * len (self.ci), h1s, h2e, self.ci)
+        Kci0 = self.Hci_all ([0.0,] * len (self.ci), h1s, h2, self.ci)
         Kci0 = [Kc - c*(c.dot (Kc)) for Kc, c in zip (Kci0, self.ci)]
         # ^ The definition of the unitary group generator compels you to do this always!!!
         return Kci0
@@ -445,7 +449,7 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
     def ci_response_diag (self, ci1):
         ci1HmEci0 = [c.dot (Hci) for c, Hci in zip (ci1, self.hci0)]
         s01 = [c1.dot (c0) for c1,c0 in zip (ci1, self.ci)]
-        ci2 = self.Hci_all ([-e for e in self.e0], self.h1frs, self.eri_cas, ci1)
+        ci2 = self.Hci_all ([-e for e in self.e0], self.h1frs[:,0], self.eri_cas, ci1)
         ci2 = [x-(y*z) for x,y,z in zip (ci2, self.ci, ci1HmEci0)]
         ci2 = [x-(y*z) for x,y,z in zip (ci2, self.hci0, s01)]
         return [x*2 for x in ci2]
@@ -460,14 +464,13 @@ class LASCI_HessianOperator (sparse_linalg.LinearOperator):
         # Split-c and split-x, for inactive-external rotations, requires I calculate a bunch
         # of extra eris (g^aa_ii, g^ai_ai)
         Hci_diag = []
-        for ix, (fcibox, norb, nelec, h1e_full, csf) in enumerate (zip (self.fciboxes, 
+        for ix, (fcibox, norb, nelec, h1rs, csf) in enumerate (zip (self.fciboxes, 
          self.ncas_sub, self.nelecas_sub, self.h1frs, self.ugg.ci_transformers)):
             i = sum (self.ncas_sub[:ix])
             j = i + norb
-            h2e = self.eri_cas[i:j,i:j,i:j,i:j]
-            h1e = (h1e_full[0,i:j,i:j],h1e_full[1,i:j,i:j])
-            h1e = [h1e] # TODO: extra dimension
-            Hci_diag.append (csf.pack_csf (fcibox.states_make_hdiag_csf (h1e, h2e, norb, nelec)[0]))
+            h2 = self.eri_cas[i:j,i:j,i:j,i:j]
+            h1rs = h1rs[:,:,i:j,i:j]
+            Hci_diag.append (csf.pack_csf (fcibox.states_make_hdiag_csf (h1rs, h2, norb, nelec)[0]))
         Hdiag = np.concatenate ([Horb_diag[self.ugg.uniq_orb_idx]] + Hci_diag)
         Hdiag += self.ah_level_shift
         Hdiag[np.abs (Hdiag)<1e-8] = 1e-8