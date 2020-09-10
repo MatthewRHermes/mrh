Bugfix test-LASSCF Hessian DF

my_pyscf/mcscf/lasscf_testing.py
@@ -107,7 +107,6 @@ class LASSCF_HessianOperator (lasci.LASCI_HessianOperator):
 
     def _init_df (self):
         lasci.LASCI_HessianOperator._init_df (self)
-        _ERIS = mc_df._ERIS if isinstance (self.las, lasci._DFLASCI) else mc_ao2mo._ERIS
         if isinstance (self.las, lasci._DFLASCI):
             self.cas_type_eris = mc_df._ERIS (self.las, self.mo_coeff, self.with_df)
         else:
@@ -140,6 +139,15 @@ class LASSCF_HessianOperator (lasci.LASCI_HessianOperator):
         ocm2[:,:,:,ncore:nocc] += ocm2_cas
         ocm2 = np.asfortranarray (ocm2) # Make largest index slowest-moving
         return odm1fs, ocm2
+
+    def get_veff (self, dm1s_mo=None):
+        mo = self.mo_coeff
+        moH = mo.conjugate ().T
+        nmo = mo.shape[-1]
+        dm1_mo = dm1s_mo.sum (0)
+        dm1_ao = np.dot (mo, np.dot (dm1_mo, moH))
+        veff_ao = np.squeeze (self.las.get_veff (dm1s=dm1_ao))
+        return np.dot (moH, np.dot (veff_ao, mo))
 
     def get_veff_Heff (self, odm1fs, tdm1frs):
         ncore, ncas, nmo = self.ncore, self.ncas, self.nmo
@@ -194,6 +202,7 @@ class LASSCF_HessianOperator (lasci.LASCI_HessianOperator):
             praa = self.cas_type_eris.ppaa[p]
             para = self.cas_type_eris.papa[p]
             paaa = praa[ncore:nocc]
+            assert (np.allclose (paaa, self.h2eff_sub[p]))
             # g_pabc d_qabc + g_prab d_qrab + g_parb d_qarb + g_pabr d_qabr (Formal)
             #        d_cbaq          d_abqr          d_aqbr          d_qabr (Symmetry of ocm2)
             # g_pcba d_abcq + g_prab d_abqr + g_parc d_aqcr + g_pbcr d_qbcr (Relabel)

tests/lasscf/test_me2n2.py
@@ -17,6 +17,7 @@ import copy
 import unittest
 import numpy as np
 from pyscf import lib, gto, scf, dft, fci, mcscf, df
+from pyscf.mcscf import newton_casscf
 from me2n2_struct import structure as struct
 from mrh.my_pyscf.mcscf.lasscf_testing import LASSCF
 
@@ -44,21 +45,123 @@ def tearDownModule():
 
 
 class KnownValues(unittest.TestCase):
-    def test_lasscf (self):
+    def test_energy (self):
         las = LASSCF (mf, (4,), (4,), spin_sub=(1,)).set (conv_tol_grad=1e-5).run ()
         self.assertAlmostEqual (las.e_tot, mc.e_tot, 8)
 
-    def test_lasscf_df (self):
+    def test_energy_df (self):
         las = LASSCF (mf_df, (4,), (4,), spin_sub=(1,)).set (conv_tol_grad=1e-5).run ()
         self.assertAlmostEqual (las.e_tot, mc_df.e_tot, 8)
 
-    def test_lasscf_hs (self):
+    def test_energy_hs (self):
         las = LASSCF (mf_hs, (4,), ((4,0),), spin_sub=(5,)).set (conv_tol_grad=1e-5).run ()
         self.assertAlmostEqual (las.e_tot, mf_hs.e_tot, 8)
 
-    def test_lasscf_hs_df (self):
+    def test_energy_hs_df (self):
         las = LASSCF (mf_hs_df, (4,), ((4,0),), spin_sub=(5,)).set (conv_tol_grad=1e-5).run ()
         self.assertAlmostEqual (las.e_tot, mf_hs_df.e_tot, 8)
+
+    def test_derivatives (self):
+        np.random.seed(1)
+        las = LASSCF (mf, (4,), (4,), spin_sub=(1,)).set (max_cycle_macro=1, ah_level_shift=0).run ()
+        ugg = las.get_ugg (las, mf.mo_coeff, las.ci)
+        ci0_csf = np.random.rand (ugg.ncsf_sub[0][0])
+        ci0_csf /= np.linalg.norm (ci0_csf)
+        ci0 = ugg.ci_transformers[0][0].vec_csf2det (ci0_csf)
+        las_gorb, las_gci = las.get_grad (mo_coeff=mf.mo_coeff, ci=[[ci0]])[:2]
+        las_grad = np.append (las_gorb, las_gci)
+        las_hess = las.get_hop (las, ugg, mo_coeff=mf.mo_coeff, ci=[[ci0]])
+        self.assertAlmostEqual (lib.fp (las_grad), lib.fp (las_hess.get_grad ()), 8)
+        cas_grad, _, cas_hess, _ = newton_casscf.gen_g_hop (mc, mf.mo_coeff, ci0, mc.ao2mo (mf.mo_coeff))
+        _pack_ci, _unpack_ci = newton_casscf._pack_ci_get_H (mc, mf.mo_coeff, ci0)[-2:]
+        def pack_cas (kappa, ci1):
+            return np.append (mc.pack_uniq_var (kappa), _pack_ci (ci1))
+        def unpack_cas (x):
+            return mc.unpack_uniq_var (x[:ugg.nvar_orb]), _unpack_ci (x[ugg.nvar_orb:])
+        def cas2las (y, mode='hx'):
+            yorb, yci = unpack_cas (y)
+            yc = yci[0].ravel ().dot (ci0.ravel ())
+            yci[0] -= yc * ci0
+            yorb *= (0.5 if mode=='hx' else 1)
+            return ugg.pack (yorb, [yci])
+        def las2cas (y, mode='x'):
+            yorb, yci = ugg.unpack (y)
+            yc = yci[0][0].ravel ().dot (ci0.ravel ())
+            yci[0][0] -= yc * ci0
+            yorb *= (0.5 if mode=='x' else 1)
+            return pack_cas (yorb, yci[0])
+        cas_grad = cas2las (cas_grad)
+        self.assertAlmostEqual (lib.fp (las_grad), lib.fp (cas_grad), 8)
+        x = np.random.rand (ugg.nvar_tot)
+        # orb on input
+        x_las = x.copy ()
+        x_las[ugg.nvar_orb:] = 0.0
+        x_cas = las2cas (x_las, mode='x')
+        hx_las = las_hess._matvec (x_las)
+        hx_cas = cas2las (cas_hess (x_cas), mode='x')
+        self.assertAlmostEqual (lib.fp (hx_las), lib.fp (hx_cas), 8)
+        # CI on input
+        x_las = x.copy ()
+        x_las[:ugg.nvar_orb] = 0.0
+        x_cas = las2cas (x_las, mode='hx')
+        hx_las = las_hess._matvec (x_las)
+        hx_cas = cas2las (cas_hess (x_cas), mode='hx')
+        self.assertAlmostEqual (lib.fp (hx_las), lib.fp (hx_cas), 8)
+        # I have to do these separately because there is no straightforward way
+        # for H_co, H_oc, and H_cc to all be simultaneously correct given the
+        # convention difference 
+
+    def test_derivatives_df (self):
+        np.random.seed(1)
+        las = LASSCF (mf_df, (4,), (4,), spin_sub=(1,)).set (max_cycle_macro=1, ah_level_shift=0).run ()
+        ugg = las.get_ugg (las, mf_df.mo_coeff, las.ci)
+        ci0_csf = np.random.rand (ugg.ncsf_sub[0][0])
+        ci0_csf /= np.linalg.norm (ci0_csf)
+        ci0 = ugg.ci_transformers[0][0].vec_csf2det (ci0_csf)
+        las_gorb, las_gci = las.get_grad (mo_coeff=mf_df.mo_coeff, ci=[[ci0]])[:2]
+        las_grad = np.append (las_gorb, las_gci)
+        las_hess = las.get_hop (las, ugg, mo_coeff=mf_df.mo_coeff, ci=[[ci0]])
+        self.assertAlmostEqual (lib.fp (las_grad), lib.fp (las_hess.get_grad ()), 8)
+        cas_grad, _, cas_hess, _ = newton_casscf.gen_g_hop (mc_df, mf_df.mo_coeff, ci0, mc_df.ao2mo (mf_df.mo_coeff))
+        _pack_ci, _unpack_ci = newton_casscf._pack_ci_get_H (mc_df, mf_df.mo_coeff, ci0)[-2:]
+        def pack_cas (kappa, ci1):
+            return np.append (mc_df.pack_uniq_var (kappa), _pack_ci (ci1))
+        def unpack_cas (x):
+            return mc_df.unpack_uniq_var (x[:ugg.nvar_orb]), _unpack_ci (x[ugg.nvar_orb:])
+        def cas2las (y, mode='hx'):
+            yorb, yci = unpack_cas (y)
+            yc = yci[0].ravel ().dot (ci0.ravel ())
+            yci[0] -= yc * ci0
+            yorb *= (0.5 if mode=='hx' else 1)
+            return ugg.pack (yorb, [yci])
+        def las2cas (y, mode='x'):
+            yorb, yci = ugg.unpack (y)
+            yc = yci[0][0].ravel ().dot (ci0.ravel ())
+            yci[0][0] -= yc * ci0
+            yorb *= (0.5 if mode=='x' else 1)
+            return pack_cas (yorb, yci[0])
+        cas_grad = cas2las (cas_grad)
+        self.assertAlmostEqual (lib.fp (las_grad), lib.fp (cas_grad), 8)
+        x = np.random.rand (ugg.nvar_tot)
+        # orb on input
+        x_las = x.copy ()
+        x_las[ugg.nvar_orb:] = 0.0
+        x_cas = las2cas (x_las, mode='x')
+        hx_las = las_hess._matvec (x_las)
+        hx_cas = cas2las (cas_hess (x_cas), mode='x')
+        self.assertAlmostEqual (lib.fp (hx_las[:ugg.nvar_orb]), lib.fp (hx_cas[:ugg.nvar_orb]), 8)
+        self.assertAlmostEqual (lib.fp (hx_las[ugg.nvar_orb:]), lib.fp (hx_cas[ugg.nvar_orb:]), 8)
+        # CI on input
+        x_las = x.copy ()
+        x_las[:ugg.nvar_orb] = 0.0
+        x_cas = las2cas (x_las, mode='hx')
+        hx_las = las_hess._matvec (x_las)
+        hx_cas = cas2las (cas_hess (x_cas), mode='hx')
+        self.assertAlmostEqual (lib.fp (hx_las[:ugg.nvar_orb]), lib.fp (hx_cas[:ugg.nvar_orb]), 8)
+        self.assertAlmostEqual (lib.fp (hx_las[ugg.nvar_orb:]), lib.fp (hx_cas[ugg.nvar_orb:]), 8)
+        # I have to do these separately because there is no straightforward way
+        # for H_co, H_oc, and H_cc to all be simultaneously correct given the
+        # convention difference 
 
 if __name__ == "__main__":
     print("Full Tests for LASSCF me2n2")