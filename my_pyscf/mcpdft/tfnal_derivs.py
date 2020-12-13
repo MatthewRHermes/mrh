0badde75487081a604a28fff7a5d77ce6bc75ead
Matthew R Hermes <herme068@herme068.umn.edu>
2020-12-13 15:07:07 -0600

    mcpdft.tfnal_deriv packed Jacobian-vector product
    "Packed" means in terms of sigma = rho'.rho' instead of rho'.

--- my_pyscf/mcpdft/tfnal_derivs.py
+++ my_pyscf/mcpdft/tfnal_derivs.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import linalg
+from pyscf import lib
 from pyscf.lib import logger
 
 def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
@@ -38,6 +39,7 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
     if nderiv > 4:
         raise NotImplementedError ("Translation of meta-GGA functionals")
     rho_t = otfnal.get_rho_translated (Pi, rho, weights=weights)
+    rho_tot = rho_t.sum (0)
     xc_grid = otfnal._numint.eval_xc (otfnal.otxc, (rho_t[0,:,:], rho_t[1,:,:]), spin=1, 
         relativity=0, deriv=dderiv, verbose=otfnal.verbose)[:dderiv+1]
     eot = xc_grid[0] * rho_t[:,0,:].sum (0)
@@ -66,8 +68,16 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
             vxc[0,1:4,:]  = rho_t[0,1:4] * vsigma[:,0] * 2 
             vxc[0,1:4,:] += rho_t[1,1:4] * vsigma[:,1]     
             vxc[1,1:4,:]  = rho_t[0,1:4] * vsigma[:,1]     
-            vxc[1,1:4,:] += rho_t[1,1:4] * vsigma[:,2] * 2 
+            vxc[1,1:4,:] += rho_t[1,1:4] * vsigma[:,2] * 2
         vot = otfnal.jTx_op (rho, Pi, vxc)
+        x = [vrho[:,0], vrho[:,1], vsigma[:,0], vsigma[:,1], vsigma[:,2]] 
+        vot_test = jTx_op_transl (otfnal, rho, Pi, x)
+        print ("vot rho test:", linalg.norm (vot_test[0]-vot[0][0]))
+        print ("vot Pi test:", linalg.norm (vot_test[1]-vot[1]))
+        if nderiv > 1:
+            print ("vot drho/dx test:", linalg.norm (2*vot_test[2]*rho_tot[1] - vot[0][1]))
+            print ("vot drho/dy test:", linalg.norm (2*vot_test[2]*rho_tot[2] - vot[0][2]))
+            print ("vot drho/dz test:", linalg.norm (2*vot_test[2]*rho_tot[3] - vot[0][3]))
     if dderiv > 1:
         raise NotImplementedError ("Translation of density derivatives of higher order than 1")
         # I should implement this entirely in terms of the gradient norm, since that reduces the
@@ -76,6 +86,54 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
         frho, frhosigma, fsigma = xc_grid[2]
 
     return eot, vot, fot
+
+def jTx_op_transl (otfnal, rho, Pi, x, Rmax=1, zeta_deriv=False):
+    if zeta_deriv: raise NotImplementedError ("fully-translated functional density derivatives")
+    if rho.shape[1] > 4: raise NotImplementedError ("translated mGGA functional density derivatives")
+    nderiv, ngrid = rho.shape[1:]
+    nderiv_zeta = nderiv if zeta_deriv else 1
+    rho_tot = rho.sum (0)
+    ncol = 2 + int (nderiv > 1) + (2*int (nderiv_zeta > 1))
+    R = otfnal.get_ratio (Pi[0:nderiv_zeta,:], rho_tot[0:nderiv_zeta,:]/2)
+
+    # Zetaless part
+    jTx = np.zeros ((ncol, ngrid), dtype=x[0].dtype)
+    jTx[0] = (x[0] + x[1]) / 2.0
+    if ncol > 2: jTx[2] = (x[2] + x[3] + x[4]) / 4.0
+
+    # Indexing
+    idx = (rho_tot[0] >= 1e-15) & (Pi[0] >= 1e-15) & (Rmax > R[0])
+    R = R[:,idx]
+    rho_tot = rho_tot[:,idx]
+    x = [xi[idx] for xi in x]
+    
+    # Intermediates (TODO: use a parallel calculator for the million tiny dot products involved in calculating sigma)
+    zeta = np.empty_like (R)
+    zeta[0] = np.sqrt (1.0 - R[0])
+    sigma = (rho_tot[1:4].conj ()*rho_tot[1:4]).sum (0)
+    zeta0inv = 1 / zeta[0]
+    rho0inv = 1 / rho_tot[0]
+    x0spin = x[0] - x[1]
+    x1charge = x[2] + x[4] - x[3]
+    x1spin = x[2] - x[4]
+
+    # 0-0 derivatives
+    jTx[0,idx] += zeta0inv * x0spin / 2.0
+    jTx[1,idx] -= zeta0inv * rho0inv * x0spin
+    if nderiv > 1:
+        # 0-1 derivatives
+        fac = R[0] * sigma * rho0inv / 2.0
+        jTx[0,idx] += fac * x1charge
+        jTx[0,idx] += fac * zeta0inv * x1spin
+        fac = -sigma * rho0inv * rho0inv
+        jTx[1,idx] += fac * x1charge
+        jTx[1,idx] += fac * zeta0inv * x1spin
+        # 1-1 derivatives
+        fac = (1-R[0]) / 4
+        jTx[2,idx] += fac * x1charge
+        jTx[2,idx] += (zeta[0]/2) * x1spin
+
+    return jTx
 
 def get_dEot_drho (otfnal, rho, Pi, vxc, Rmax=1, zeta_deriv=False):
     r''' get the functional derivative dE_ot/drho
