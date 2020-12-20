tLDA and ftGGA gradient syntax bugfix
Massive errors remain, unfortunately, in both tLDA and ftGGA.

my_pyscf/grad/mcpdft.py
@@ -114,7 +114,7 @@ def mcpdft_HellmanFeynman_grad (mc, ot, veff1, veff2, mo_coeff=None, ci=None, at
     make_rho = ot._numint._gen_rho_evaluator (mol, dm1, 1)[0]
     dvxc = np.zeros ((3,nao))
     idx = np.array ([[1,4,5,6],[2,5,7,8],[3,6,8,9]], dtype=np.int_) # For addressing particular ao derivatives
-    if ot.xctype == 'LDA': idx = idx[:,0] # For LDAs no second derivatives
+    if ot.xctype == 'LDA': idx = idx[:,0:1] # For LDAs no second derivatives
     diag_idx = np.arange(ncas) # for puvx
     diag_idx = diag_idx * (diag_idx+1) // 2 + diag_idx
     casdm2_pack = (twoCDM + twoCDM.transpose (0,1,3,2)).reshape (ncas**2, ncas, ncas)
@@ -146,8 +146,8 @@ def mcpdft_HellmanFeynman_grad (mc, ot, veff1, veff2, mo_coeff=None, ci=None, at
             ao = ot._numint.eval_ao (mol, coords[ip0:ip1], deriv=ot.dens_deriv+1, non0tab=mask) # Need 1st derivs for LDA, 2nd for GGA, etc.
             t1 = logger.timer (mc, 'PDFT HlFn quadrature atom {} ao grids'.format (ia), *t1)
             if ot.xctype == 'LDA': # Might confuse the rho and Pi generators if I don't slice this down
-                aoval = ao[:1]
-            elif ot.xctype == 'GGA':
+                aoval = ao[0]
+            if ot.xctype == 'GGA':
                 aoval = ao[:4]
             rho = make_rho (0, aoval, mask, ot.xctype) / 2.0
             rho = np.stack ((rho,)*2, axis=0)
@@ -155,6 +155,8 @@ def mcpdft_HellmanFeynman_grad (mc, ot, veff1, veff2, mo_coeff=None, ci=None, at
             Pi = get_ontop_pair_density (ot, rho, aoval, dm1s, twoCDM, mo_cas, ot.dens_deriv, mask)
             t1 = logger.timer (mc, 'PDFT HlFn quadrature atom {} Pi calc'.format (ia), *t1)
 
+            if ot.xctype == 'LDA':
+                aoval = ao[:1]
             moval_occ = _grid_ao2mo (mol, aoval, mo_occ, mask)
             t1 = logger.timer (mc, 'PDFT HlFn quadrature atom {} ao2mo grid'.format (ia), *t1)
             aoval = np.ascontiguousarray ([ao[ix].transpose (0,2,1) for ix in idx[:,:ndao]]).transpose (0,1,3,2)

my_pyscf/mcpdft/otfnal.py
@@ -327,11 +327,6 @@ class ftransfnal (transfnal):
 
     Pi_deriv = 1
 
-    def eval_ot (self, rho, Pi, dderiv=0, weights=None):
-        if dderiv>0:
-            raise NotImplementedError ("density derivatives for fully-translated functionals")
-        return transfnal.eval_ot (self, rho, Pi, dderiv=dderiv, weights=weights)
-
     def get_rho_translated (self, Pi, rho, **kwargs):
         r''' Compute the "fully-translated" alpha and beta densities
         and their derivatives. This is the same as "translated" except
@@ -415,25 +410,29 @@ class ftransfnal (transfnal):
         cfnal.otxc = 'f' + cfnal.otxc
         return xfnal, cfnal
 
-    def jT_op (self, rho, Pi, x, **kwargs):
+    def jT_op (self, x, rho, Pi, **kwargs):
         r''' Evaluate jTx = (x.j)T where j is the Jacobian of the translated densities
         in terms of the untranslated density and pair density
 
         Args:
+            x : ndarray of shape (2,*,ngrids)
+                Usually, a functional derivative of the on-top xc energy wrt
+                translated densities
             rho : ndarray of shape (2,*,ngrids)
                 containing spin-density [and derivatives]
             Pi : ndarray with shape (*,ngrids)
                 containing on-top pair density [and derivatives]
-            x : ndarray of shape (2,*,ngrids)
-                Usually, a functional derivative of the on-top xc energy wrt
-                translated densities
 
         Returns: ndarray of shape (*,ngrids)
             Usually, a functional derivative of the on-top pair density exchange-correlation
             energy wrt to total density and its derivatives
             The potential must be spin-symmetric in pair-density functional theory
         '''
-        jTx = transfnal.jT_op (self, rho, Pi, x, **kwargs)
+        ntc = 2 + int(self.dens_deriv>0)
+        ncol = 2 + 3*int(self.dens_deriv>0)
+        ngrid = rho.shape[-1]
+        jTx = np.zeros ((ncol,ngrid), dtype=x[0].dtype)
+        jTx[:ntc,:] = transfnal.jT_op (self, x, rho, Pi, **kwargs)
         rho = rho.sum (0)
         R = self.get_ratio (Pi[0:4,:], rho[0:4,:]/2)
         zeta = self.get_zeta (R[0], fn_deriv=2)

my_pyscf/mcpdft/tfnal_derivs.py
@@ -7,7 +7,7 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
     r''' get the integrand of the on-top xc energy and its functional derivatives wrt rho and Pi 
 
     Args:
-        rho : ndarray of shape (2,*ngrids)
+        rho : ndarray of shape (2,*,ngrids)
             containing spin-density [and derivatives]
         Pi : ndarray with shape (*,ngrids)
             containing on-top pair density [and derivatives]
@@ -35,6 +35,9 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
     '''
     if dderiv > 2:
         raise NotImplementedError ("Translation of density derivatives of higher order than 2")
+    if rho.ndim == 2: rho = rho[:,None,:]
+    if Pi.ndim == 1: Pi = Pi[None,:]
+    assert (rho.shape[0] == 2)
     nderiv = rho.shape[1]
     nderiv_Pi = Pi.shape[1]
     if nderiv > 4:
@@ -54,7 +57,8 @@ def eval_ot (otfnal, rho, Pi, dderiv=1, weights=None):
     vot = fot = None
     if dderiv > 0:
         vrho, vsigma = xc_grid[1][:2]
-        vxc = list (vrho.T) + list (vsigma.T)
+        vxc = list (vrho.T)
+        if otfnal.dens_deriv: vxc = vxc + list (vsigma.T)
         vot = otfnal.jT_op (vxc, rho, Pi)
         vot = _unpack_sigma_vector (vot, deriv1=rho_deriv, deriv2=Pi_deriv)
     if dderiv > 1:
@@ -184,7 +188,7 @@ def _ftGGA_jT_op_m2z (x, rho, zeta, srP, sPP):
     jTx = np.empty_like (x)
     jTx[0] = ((x[3] + 2*x[4]*zeta[0])*srP*zeta[1]
               + 2*rho*x[4]*sPP*zeta[1]*zeta[1])
-    jTx[1] = 2*x[3]*rho*srp*zeta[1]
+    jTx[1] = 2*x[3]*rho*srP*zeta[1]
     jTx[2] = 0
     jTx[3] = (x[3] + 2*x[4]*zeta[0])*rho
     jTx[4] = x[4]*rho*rho
@@ -203,14 +207,14 @@ def _ftGGA_jT_op_z2R (x, zeta, srP, sPP):
 def _ftGGA_jT_op_R2Pi (x, rho, R, srr, srP, sPP):
     if rho.ndim > 1: rho = rho[0]
     if R.ndim > 1: R = R[0]
-    ri = np.empty_like (jTx)
+    jTx = np.empty_like (x)
+    ri = np.empty_like (x)
     ri[0,:] = 1.0
     idx = rho>1e-15
     ri[0,idx] /= rho[idx]
     for i in range (4):
         ri[i+1] = ri[i]*ri[0]
 
-    jTx = np.empty_like (x)
     jTx[0] = (-2*R*x[1]*ri[0]
                + x[3]*(6*R*ri[1]*srr - 8*sPP*ri[2])
                + x[4]*(-24*R*R*ri[2]*srr + 80*R*ri[3]*srP 