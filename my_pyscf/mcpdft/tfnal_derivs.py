70914aeea888a8a2396278d9ceeedd9a95b62458
Matthew R Hermes <herme068@herme068.umn.edu>
2020-12-19 22:01:37 -0600

    mcpdft tGGA translation hessian safety commit

--- my_pyscf/mcpdft/tfnal_derivs.py
+++ my_pyscf/mcpdft/tfnal_derivs.py
@@ -211,3 +211,98 @@ def ftGGA_jT_op (otfnal, rho, Pi, x):
 
     return jTx
 
+def gentLDA_d_jT_op (otfnal, rho, Pi, x):
+    ngrid = rho.shape[-1]
+    f = np.zeros ((3,ngrid), dtype=x[0].dtype)
+
+    # ab -> cs
+    xm = (x[0] - x[1]) / 2.0
+
+    # Indexing
+    rho = rho.sum (0)
+    idx = rho > 1e-15
+    rho = rho[0:1,idx]
+    Pi = Pi[0:1,idx]
+    xm = xm[idx]
+
+    # Intermediates
+    R = otfnal.get_ratio (Pi, rho/2)
+    zeta = otfnal.get_zeta (R, fn_deriv=2)[1:]
+    zeta[0] += 2*R[0]*zeta[1] # sloppy notation...
+    zeta = 4*zeta*xm[None,:]/rho[None,:] # sloppy notation...
+
+    # without further ceremony
+    f[0] = R*zeta[0]
+    f[1] = -zeta[0]/rho
+    f[2] = 4*zeta[1]/rho/rho
+
+    return f
+
+def tGGA_d_jT_op (otfnal, rho, Pi, x):
+    # Generates contributions to the first five elements
+    # of the lower-triangular packed Hessian
+    ngrid = rho.shape[-1]
+    f = np.zeros ((5,ngrid), dtype=x[0].dtype)
+
+    # Indexing
+    rho = rho.sum (0)
+    idx = rho > 1e-15
+    rho = rho[0:4,idx]
+    Pi = Pi[0:1,idx]
+    x = x[:,idx]
+
+    # ab -> cs
+    xcm = (x[2] - x[4]) / 2.0
+    xmm = (x[2] + x[4] - x[3]) / 4.0
+
+    # Intermediates
+    R = otfnal.get_ratio (Pi, rho/2)
+    zeta = otfnal.get_zeta (R, fn_deriv=2)
+    sigma = (rho[1:4]*rho[1:4]).sum (0)
+    rho = rho[0]
+    R = R[0,:]
+    rho2 = rho*rho
+    rho3 = rho2*rho
+    rho4 = rho3*rho
+
+    # coefficient of dsigma dz
+    xcm += 2*zeta[0]*xmm
+    f[3] = -4*xcm*R*zeta[1]/rho
+    f[4] = 8*xcm*zeta[1]/rho2
+    
+    # coefficient of d^2 z
+    xcm *= sigma
+    f[0] = 2*xcm*R*(3*zeta[1] + 2*R*zeta[2])/rho2
+    f[1] = -8*xcm*(zeta[1] - 4*R*zeta[2])/rho3
+    f[2] = 16*xcm*zeta[2]/rho4
+
+    # coefficient of dz dz
+    xmm *= 8*sigma*zeta[1]*zeta[1]/rho2
+    f[0] += xmm*R*R
+    f[1] -= 2*xmm*R/rho
+    f[2] += 4*xmm/rho2
+    
+    return f
+
+def ftGGA_d_jT_op (otfnal, rho, Pi, x):
+    raise NotImplementedError ("Second density derivatives for fully-translated GGA functionals")
+    # Generates contributions to the first five elements,
+    # then 6,7, then 10,11
+    # of the lower-triangular packed Hessian
+    # (I.E., no double gradient derivatives)
+    ngrid = rho.shape[-1]
+    # rho,rho ; Pi,rho ; Pi,Pi ; s(rho,rho),rho ; s(rho,rho),Pi ([0:5])
+    frr = np.zeros ((5,ngrid), dtype=x[0].dtype) 
+    # s(Pi,rho),rho ; s(Pi,rho),Pi ([6:8])
+    fPr = np.zeros ((2,ngrid), dtype=x[0].dtype)
+    # s(Pi,Pi),rho ; s(Pi,Pi),Pi ([10:12])
+    fPP = np.zeros ((2,ngrid), dtype=x[0].dtype)
+    # This is a bad idea why I am I doing this
+    f = [[frr[0], frr[1], frr[3], fPr[0], fPP[0]],
+         [frr[1], frr[2], frr[4], fPr[1], fPP[1]],
+         [frr[3], frr[4], None,   None,   None],
+         [fPr[0], fPr[1], None,   None,   None],
+         [fPP[0], fPP[1], None,   None,   None]]
+
+
+
