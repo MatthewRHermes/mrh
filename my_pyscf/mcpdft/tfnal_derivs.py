pdft second density derivatives less incorrect
Translated (as opposed to fully-translated) second derivatives
seem to involve a small difference of two large numbers.

my_pyscf/mcpdft/tfnal_derivs.py
@@ -330,10 +330,10 @@ def ftGGA_jT_op (x, rho, Pi, R, zeta):
 def gentLDA_d_jT_op (x, rho, Pi, R, zeta):
     rho = rho[0]
     Pi = Pi[0]
+    R = R[0]
     ngrid = rho.shape[-1]
     f = np.zeros ((3,ngrid), dtype=x[0].dtype)
     zeta = zeta[1:]
-    if R.ndim > 1: R = R[0]
 
     # ab -> cs
     xm = (x[0] - x[1]) / 2.0
@@ -349,13 +349,13 @@ def gentLDA_d_jT_op (x, rho, Pi, R, zeta):
     # Intermediates
     #R = otfnal.get_ratio (Pi, rho/2)
     #zeta = otfnal.get_zeta (R, fn_deriv=2)[1:]
-    zeta[0] += 2*R*zeta[1] # sloppy notation...
-    zeta = 4*zeta*xm[None,:]/rho[None,:] # sloppy notation...
+    #print (np.amax (np.abs (R)), np.amax (np.abs (zeta[0])), np.amax (np.abs (zeta[1])))
+    zeta *= 2*xm[None,:]/rho[None,:] # sloppy notation...
 
     # without further ceremony
-    f[0,idx] = R*zeta[0]
-    f[1,idx] = -zeta[0]/rho
-    f[2,idx] = 4*zeta[1]/rho/rho
+    f[0,idx] = R*(zeta[0]+2*R*zeta[1])
+    f[1,idx] = -4*(zeta[0]+R*zeta[1])/rho
+    f[2,idx] = 8*zeta[1]/rho/rho
 
     return f
 

tests/mcpdft/test_N2.py
@@ -85,7 +85,7 @@ def test_densgrad (otxc):
         E = np.dot (eot0, weight)
         dE_num = np.dot (eotD, weight)
         dE_lin = np.dot (eotD_lin, weight)
-        eotD_quad = eotD_lin + (vot1[0]*rhoT.sum (0)).sum (0) + (vot1[1][:nPi]*PiT[:nPi]).sum (0)
+        eotD_quad = (vot1[0]*rhoT.sum (0)).sum (0) + (vot1[1][:nPi]*PiT[:nPi]).sum (0)
         dE_quad = np.dot (eotD_quad, weight)
         return dE_num, dE_lin, dE_quad
 