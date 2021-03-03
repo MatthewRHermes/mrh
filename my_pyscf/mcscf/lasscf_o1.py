803218d12dd09b16d71d9aef7144f361565beae9
Matthew R Hermes <herme068@herme068.umn.edu>
2021-03-02 20:48:36 -0600

    Cleanup lasscf_o1

--- my_pyscf/mcscf/lasscf_o1.py
+++ my_pyscf/mcscf/lasscf_o1.py
@@ -32,8 +32,8 @@ def make_schmidt_spaces (h_op):
     g_vec = h_op.get_grad ()
     prec_op = h_op.get_prec ()
     hx_vec = prec_op (-g_vec)
-    gorb = ugg.unpack (g_vec)[0]
-    hxorb = ugg.unpack (hx_vec)[0]
+    gorb1 = ugg.unpack (g_vec)[0]
+    gorb2 = gorb1 + ugg.unpack (hx_vec)[0]
 
     def _svd (metric, q, p):
         m, n = q.shape[1], p.shape[1]
@@ -51,6 +51,23 @@ def make_schmidt_spaces (h_op):
         lib.logger.debug (h_op, '%s size of pspace = %d, qspace = %d', tag, np, nq)
         return k
 
+    def _grad_svd (tag, geff, umat_p, umat_q, ncoup=0):
+        umat_q = _svd (geff, umat_q, umat_p)[0]
+        umat_p = np.append (umat_p, umat_q[:,:ncoup], axis=1)
+        umat_q = umat_q[:,ncoup:]
+        k = _check ('after {} SVD'.format (tag), umat_p, umat_q)
+        return umat_p, umat_q, k
+
+    def _schmidt (tag, umat_p, umat_q, thresh=1e-8):
+        umat_q, svals = _svd (dm1, umat_q, umat_p)
+        ncoup = np.count_nonzero (np.abs (svals) > thresh)
+        umat_p = np.append (umat_p, umat_q[:,:ncoup], axis=1)
+        umat_q = umat_q[:,ncoup:]
+        dm_pp = umat_p.conj ().T @ dm1 @ umat_p
+        lib.logger.debug (h_op, 'number of electrons in p-space after %s Schmidt = %e', tag, np.trace (dm_pp))
+        k = _check ('after {} Schmidt'.format (tag), umat_p, umat_q)
+        return umat_p, umat_q, k
+
     def _make_single_space (mask):
         # Active orbitals other than the current should be in
         # neither the p-space nor the q-space
@@ -60,38 +77,16 @@ def make_schmidt_spaces (h_op):
         umat_p = np.diag (mask.astype (mo_coeff.dtype))[:,mask]
         umat_q = np.eye (nmo)
         umat_q = np.append (umat_q[:,:ncore], umat_q[:,nocc:])
+        # At any of these steps we might run out of orbitals...
         k = _check ('initial', umat_p, umat_q)
         if k == 0: return umat_p
-        # Initial gradient SVD
-        umat_q = _svd (gorb, umat_q, umat_p)[0]
-        umat_p = np.append (umat_p, umat_q[:,:k], axis=1)
-        umat_q = umat_q[:,k:]
-        k = _check ('after gradient SVD', umat_p, umat_q)
+        umat_p, umat_q, k = _grad_svd ('g', gorb1, umat_p, umat_q, ncoup=k)
         if k == 0: return umat_p
-        # First Schmidt decomposition
-        umat_q, svals = _svd (dm1, umat_q, umat_p)
-        ncoup = np.count_nonzero (np.abs (svals)>1e-8)
-        umat_p = np.append (umat_p, umat_q[:,:ncoup], axis=1)
-        umat_q = umat_q[:,ncoup:]
-        dm_pp = umat_p.conj ().T @ dm1 @ umat_p
-        lib.logger.debug (h_op, 'number of electrons in p-space after first Schmidt = %e', np.trace (dm_pp))
-        k = _check ('after first Schmidt', umat_p, umat_q)
+        umat_p, umat_q, k = _schmidt ('first', umat_p, umat_q)
         if k == 0: return umat_p
-        # (g + H.x)_qp SVD 
-        umat_q = _svd (gorb, umat_q, umat_p)[0]
-        ncoup = min (k, 3*nlas)
-        umat_p = np.append (umat_p, umat_q[:,:ncoup], axis=1)
-        umat_q = umat_q[:,ncoup:]
-        k = _check ('after g+H.x SVD', umat_p, umat_q)
+        umat_p, umat_q, k = _grad_svd ('g+hx', gorb2, umat_p, umat_q, ncoup=min(k,3*nlas))
         if k == 0: return umat_p
-        # Second Schmidt decomposition
-        umat_q, svals = _svd (dm1, umat_q, umat_p)
-        ncoup = np.count_nonzero (np.abs (svals)>1e-8)
-        umat_p = np.append (umat_p, umat_q[:,:ncoup], axis=1)
-        umat_q = umat_q[:,ncoup:]
-        dm_pp = umat_p.conj ().T @ dm1 @ umat_p
-        lib.logger.debug (h_op, 'number of electrons in p-space after second Schmidt = %e', np.trace (dm_pp))
-        _check ('after second Schmidt', umat_p, umat_q)
+        umat_p, umat_q, k = _schmidt ('second', umat_p, umat_q)
         return umat_p
 
     orbsym = getattr (mo_coeff, 'orbsym', np.zeros (nmo))
