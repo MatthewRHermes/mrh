2ec555e0db318782302d7e4a8a79d7f03c64c67b
Matthew R Hermes <herme068@herme068.umn.edu>
2021-03-19 18:55:29 -0500

    Fock-space UCC contraction debugging

--- exploratory/unitary_cc/uccsd_sym0.py
+++ exploratory/unitary_cc/uccsd_sym0.py
@@ -1,9 +1,79 @@
 import numpy as np
+import time, ctypes, math
 from scipy import linalg
-from mrh.lib
+from mrh.lib.helper import load_library
+
+libfsucc = load_library ('libfsucc')
 
 # "sym0" means no spin or number symmetry at all: the full fock space,
 # with a single flat CI vector corresponding to determinant strings
 # identical to CI vector index numbers in a 64-bit unsigned integer format
+
+'''
+    fn(vv.ctypes.data_as(ctypes.c_void_p),
+       ao.ctypes.data_as(ctypes.c_void_p),
+       mo.ctypes.data_as(ctypes.c_void_p),
+       ctypes.c_int(nao), ctypes.c_int (nmo),
+       ctypes.c_int(ngrids), ctypes.c_int(mol.nbas),
+       pnon0tab, pshls_slice, pao_loc)
+'''
+
+def _op (norb, a_idxs, i_idxs, tamps, psi, inplace=True, transpose=False, derivs=[]):
+    ''' Evaluate U|Psi> = ... e^t2 e^t1 e^t0 |Psi>, where tn are single
+        generators
+
+        Args:
+            a_idxs : list of len (ngen)
+                ndarrays listing +cr,-an operators
+            i_idxs : list of len (ngen)
+                ndarrays listing +an,-cr operators
+            tamps : ndarray of shape (ngen)
+                amplitudes/angles
+            psi : ndarray of len (2**norb)
+                spinless fock-space CI array
+
+        Kwargs:
+            inplace : logical
+                Setting to False creates a copy
+            transpose : logical
+                If true, generators are applied in reverse order with
+                amplitudes * -1
+            derivs : list or ndarray
+                List of amplitudes by which to differentiate (by adding pi/2)
+                This function returns a ~single~ value or derivative vector
+                of order len (derivs)
+
+        Returns:
+            upsi : ndarray of len (2**norb)
+                new spinless fock-space CI array
+    '''
+    upsi = psi.view () if inplace else psi.copy ()
+    sgn = 1 - (2*int (transpose))
+    my_tamps = tamps * sgn
+    for ideriv in derivs: my_tamps[ideriv] += math.pi * 0.5
+    ngen = len (a_idxs)
+    gen_range = range (ngen-1, -1, -1) if transpose else range (ngen)
+    for igen in gen_range:
+        libfsucc.FSUCCcontract1 (a_idxs[igen].ctypes.data_as (ctypes.c_void_p),
+            i_idxs[igen].ctypes.data_as (ctypes.c_void_p),
+            ctypes.c_double (sgn*tamps[igen]),
+            upsi.ctypes.data_as (ctypes.c_void_p),
+            ctypes.c_uint (norb),
+            ctypes.c_uint (len (a_idxs[igen])),
+            ctypes.c_uint (len (i_idxs[igen])))
+    return upsi
+
+
+if __name__ == '__main__':
+    norb = 4 
+    psi = np.zeros (2**norb)
+    psi[3] = 1.0
+    a = [np.array ([2], dtype=np.uint8), np.array ([0,2], dtype=np.uint8)]
+    i = [np.array ([1], dtype=np.uint8), np.array ([3,1], dtype=np.uint8)]
+    tamps = [math.pi/4, 0.33]
+    upsi = _op (norb, a, i, tamps, psi, inplace=False)
+    for i in range (2**norb):
+        print (bin (i), psi[i], upsi[i])
+    print (psi.dot (psi), upsi.dot (upsi))
 
 
--- lib/fsucc.c
+++ lib/fsucc.c
@@ -14,22 +14,32 @@
 #define MINMAX
 #endif
 
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
+
 /* Fock-Space Unitary Coupled Cluster functions
    Cf. Eqs (1)-(12) of JCTC 17 841 2021
-   (DOI:10.1021/acs.jctc.0c01052) */
+   (DOI:10.1021/acs.jctc.0c01052) 
 
-void FSUCCcontract1 (unsigned int * aidx, unsigned int * iidx, double tamp,
-    double * psi, unsigned int norb, unsigned int nexc);
+   No spin, no symmetry, no derivatives, not even any looping over amplitudes.
+   Do all of that in the Python caller because none of it can easily be
+   multithreaded.
+
+   TODO: double-check that this actually works for number-symmetry-breaking
+   generators (it should work for 1P or 1H operators, but I haven't checked 
+   nPmH in general
+*/
+
+void FSUCCcontract1 (uint8_t * aidx, uint8_t * iidx, double tamp,
+    double * psi, unsigned int norb, unsigned int na, unsigned int ni)
 {
     /* Evaluate U|Psi> = e^(t a0'a1'...i1i0 - i0'i1'...a1a0) |Psi> 
-       The normal-order convention is
-       i0 < i1 < i2 < ...
-       a0 < a1 < a2 < ...
        Pro tip: add pi/2 to the amplitude to evaluate dU/dt |Psi>
 
        Input:
-            aidx : array of shape (nexc); identifies +cr,-an ops
-            iidx : array of shape (nexc); identifies +an,-cr ops
+            aidx : array of shape (na); identifies +cr,-an ops
+            iidx : array of shape (ni); identifies +an,-cr ops
                 Note: creation operators are applied left < right;
                 annihilation operators are applied right < left
             tamp : the amplitude or angle
@@ -40,10 +50,20 @@ void FSUCCcontract1 (unsigned int * aidx, unsigned int * iidx, double tamp,
                 if you don't want to modify the input
     */
 
-    int int_one = 1;
-    uint64_t ndet = (1<<(norb-nexc)); // 2**(norb-nexc)
-    double ct = cos (tamp); // (ct -st) (ia) -> (ia)
-    double st = sin (tamp); // (st  ct) (ai) -> (ai)
+    const int int_one = 1;
+    const double ct = cos (tamp); // (ct -st) (ia) -> (ia)
+    const double st = sin (tamp); // (st  ct) (ai) -> (ai)
+    int r;
+    uint64_t det_i = 0; // i is occupied
+    for (r = 0; r < ni; r++){ det_i |= (1<<iidx[r]); }
+    uint64_t det_a = 0; // a is occupied
+    for (r = 0; r < na; r++){ det_a |= (1<<aidx[r]); }
+    // all other spinorbitals in det_i, det_a unoccupied
+    uint64_t ndet = (1<<norb); // 2**norb
+    for (r = 0; r < norb; r++){ if ((det_i|det_a) & (1<<r)){
+        ndet >>= 1; // pop 1 spinorbital per unique i,a
+        // we only sum over the spectator-spinorbital determinants
+    }}
 
 #pragma omp parallel default(shared)
 {
@@ -56,51 +76,47 @@ void FSUCCcontract1 (unsigned int * aidx, unsigned int * iidx, double tamp,
 #pragma omp for schedule(static)
 
     for (det = 0; det < ndet; det++){
-        // "det" here is the string of uninvolved spinorbitals
-        // To find the full det string I have to insert i, a
+        // "det" here is the string of spectator spinorbitals
+        // To find the full det string I have to insert i, a in ascending order
         det_00 = det;
-        det_ia = 0;
-        for (p = 0; p < nexc; p++){
-            det_ia |= ((1<<iidx[p]) | (1<<aidx[p]));
-        } // det_ia now identifies any of either i or a
-        for (p = 0; p < MAX (iidx[nexc-1], aidx[0]);  p++){
-            if (det_ia & (1<<p)){
-                det_ai  = det_00 >> p;     // lr  -> l
-                det_00 &= ((1<<p)-1);      // lr  -> r
-                det_00 |= det_ai << (p+1); // l,r -> l0r
+        for (p = 0; p < norb; p++){
+            if ((det_i|det_a) & (1<<p)){
+                det_00 = (((det_00 >> p) << (p+1)) // move left bits 1 left
+                         | (det_00 & ((1<<p)-1))); // keep right bits
             } 
-        }
-        // det_00 now has unoccupied orbitals in the i,a positions
-        // Next, make strings describing i,a orbitals occupied in their
-        // proper positions
-        det_ia = 0;
-        det_ai = 0;
-        for (p = 0; p < nexc; p++){
-            det_ia |= (1<<iidx[p]); // i is occupied
-            det_ai |= (1<<aidx[p]); // a is occupied
-        }
-        det_ia |= det_00;
-        det_ai |= det_00;
-        // The sign for the whole excitation is the product of the
-        // sign incurred by doing this to det_ia
+        } // det_00: spectator spinorbitals; all i, a bits unset
+        det_ia = det_00 | det_i;
+        det_ai = det_00 | det_a;
+        // The sign for the whole excitation is the product of the sign incurred
+        // by doing this to det_ia:
         // ...i2'...i1'...i0'|0> -> i0'i1'i2'...|0>
-        // and doing this to det_ai
-        // ...a2'...a1'...a0'|0> -> a0'a1'a2'...|0>
-        sgnbit = 0;
-        for (p = 0; p < nexc; p++){
+        // and doing this to det_ai:
+        // ...a2'...a1'...a0'|0> -> a0'a1'a2'...|0>.
+        // To implement this without assuming normal-ordered generators
+        // (i.e., i0 < i1 < i2 or a0 < a1 < a2)
+        // we need to pop creation operators from the string in the order that
+        // we move them to the front. Repurpose det_00 for this.
+        sgnbit = 0; // careful to only modify the first bit of this
+        det_00 = det_ia;
+        for (p = 0; p < ni; p++){
             for (q = iidx[p]+1; q < norb; q++){
-                sgnbit ^= (1 & (det>>q));
+                sgnbit ^= (det_00 & (1<<q))>>q; // c1'c2' = -c2'c1' sign toggle
             }
+            det_00 ^= (1<<iidx[p]); // pop ip
+        }
+        det_00 = det_ai;
+        for (p = 0; p < na; p++){
             for (q = aidx[p]+1; q < norb; q++){
-                sgnbit ^= (1 & (det>>q));
+                sgnbit ^= (det_00 & (1<<q))>>q; // c1'c2' = c2'c1' sign toggle
             }
+            det_00 ^= (1<<aidx[p]); // pop ap
         }
         sgn = int_one - 2*((int) sgnbit);
         // The rest of the math is trivial
         cia = sgn * psi[det_ia];
         cai = sgn * psi[det_ai];
-        psi[det_ia] = (ct*cia) - (st*cia);
-        psi[det_ai] = (st*cia) + (ct*cia);
+        psi[det_ia] = (ct*cia) - (st*cai);
+        psi[det_ai] = (st*cia) + (ct*cai);
     }
 
 }
