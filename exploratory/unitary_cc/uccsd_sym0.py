35da6ed8fb9456bb2f2be4eb4e1ce582471437a8
Matthew R Hermes <herme068@herme068.umn.edu>
2021-03-19 23:32:47 -0500

    FSUCC operator object

--- exploratory/unitary_cc/uccsd_sym0.py
+++ exploratory/unitary_cc/uccsd_sym0.py
@@ -63,17 +63,72 @@ def _op (norb, a_idxs, i_idxs, tamps, psi, inplace=True, transpose=False, derivs
             ctypes.c_uint (len (i_idxs[igen])))
     return upsi
 
+class FSUCCOperator (object):
+
+    def __init__(self, norb, a_idxs, i_idxs):
+        self.norb = norb
+        self.ngen = ngen = len (a_idxs)
+        self.a_idxs = [np.ascontiguousarray (a, dtype=np.uint8) for a in a_idxs]
+        self.i_idxs = [np.ascontiguousarray (i, dtype=np.uint8) for i in i_idxs]
+        assert (len (self.i_idxs) == ngen)
+        self.amps = np.zeros (ngen)
+        self.assert_sanity (nodupes=True)
+
+    def assert_sanity (self, nodupes=True):
+        ''' check for nilpotent generators, too many cr/an ops, or orbital
+            indices out of range. nodupes -> check for duplicates under
+            permutation symmetry (expensive) '''
+        norb, ngen = self.norb, self.ngen
+        pq_sorted = []
+        for a, i in zip (self.a_idxs, self.i_idxs):
+            p = np.append (i, a)
+            errstr = 'a,i={},{} invalid for norb={}'.format (a,i,norb)
+            assert (np.amax (p) < norb), errstr
+            a_maxcnt = np.amax (np.unique (a, return_counts=True)[1])
+            errstr = 'a={} is nilpotent'.format (a)
+            assert (a_maxcnt < 2), errstr
+            i_maxcnt = np.amax (np.unique (i, return_counts=True)[1])
+            errstr = 'i={} is nilpotent'.format (a)
+            assert (i_maxcnt < 2), errstr
+            # passing these three implies that there aren't too many ops
+            a_sorted, i_sorted = np.sort (a), np.sort (i)
+            errstr = 'undefined amplitude detected (i==a)'
+            assert (not np.all (a_sorted==i_sorted)), errstr
+            pq_sorted.append (tuple (sorted ([tuple (a_sorted), tuple (i_sorted)])))
+        if nodupes:
+            pq_sorted = set (pq_sorted)
+            errstr = 'duplicate generators detected'
+            assert (len (pq_sorted) == ngen), errstr
+
+    def __call__(self, ket, transpose=False, inplace=False):
+        return _op(self.norb, self.a_idxs, self.i_idxs, self.amps, ket,
+            inplace=inplace, transpose=transpose)
 
 if __name__ == '__main__':
-    norb = 4 
+    norb = 4
+    def pbin (n):
+        s = bin (n)[2:]
+        m = norb - len (s)
+        if m: s = ''.join (['0',]*m) + s
+        return s
     psi = np.zeros (2**norb)
     psi[3] = 1.0
-    a = [np.array ([2], dtype=np.uint8), np.array ([0,2], dtype=np.uint8)]
-    i = [np.array ([1], dtype=np.uint8), np.array ([3,1], dtype=np.uint8)]
-    tamps = [math.pi/4, 0.33]
+    a = [np.array ([2], dtype=np.uint8), np.array ([0,2], dtype=np.uint8), np.array ([2,0], dtype=np.uint8)]
+    i = [np.array ([1], dtype=np.uint8), np.array ([3,1], dtype=np.uint8), np.array ([3,2], dtype=np.uint8)]
+    tamps = [math.pi/4, 0.33, 0.5]
     upsi = _op (norb, a, i, tamps, psi, inplace=False)
-    for i in range (2**norb):
-        print (bin (i), psi[i], upsi[i])
-    print (psi.dot (psi), upsi.dot (upsi))
+    for ix in range (2**norb):
+        print (pbin (ix), psi[ix], upsi[ix])
+    print ("<psi|psi> =",psi.dot (psi), "<psi|U|psi> =",psi.dot (upsi),"<psi|U'U|psi> =",upsi.dot (upsi))
+    upsi = _op (norb, a, i, tamps, upsi, transpose=True)
+    for ix in range (2**norb):
+        print (pbin (ix), psi[ix], upsi[ix])
+    print ("<psi|psi> =",psi.dot (psi), "<psi|U|psi> =",psi.dot (upsi),"<psi|U'U|psi> =",upsi.dot (upsi))
 
-
+    a, i = np.tril_indices (norb, k=-1)
+    uop = FSUCCOperator (norb, a, i)
+    uop.amps = (1 - 2*np.random.rand (uop.ngen))*math.pi
+    upsi = uop (psi)
+    for ix in range (2**norb):
+        print (pbin (ix), psi[ix], upsi[ix])
+    print ("<psi|psi> =",psi.dot (psi), "<psi|U|psi> =",psi.dot (upsi),"<psi|U'U|psi> =",upsi.dot (upsi))
