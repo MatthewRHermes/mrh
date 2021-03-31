70e5cb6e7223cb1a01a639881e61db861e2db37e
Matthew R Hermes <herme068@herme068.umn.edu>
2021-03-30 19:30:08 -0500

    Bugfix in my_pyscf.molden
    Keep calling mc members clean!

--- my_pyscf/tools/molden.py
+++ my_pyscf/tools/molden.py
@@ -4,7 +4,9 @@ from pyscf.tools.molden import from_mcscf as pyscf_from_mcscf
 import numpy as np
 
 def from_sa_mcscf (mc, fname, state=None, cas_natorb=False, cas_mo_energy=False, **kwargs):
-    if state is None: return pyscf_from_mcscf (mc, fname, cas_natorb=cas_natorb, **kwargs)
+    if state is None: 
+        if cas_mo_energy: return pyscf_from_mcscf (mc, fname, cas_natorb=cas_natorb, **kwargs)
+        else: return from_mcscf (mc, fname, cas_natorb=cas_natorb, **kwargs)
     casdm1 = mc.fcisolver.states_make_rdm1 (mc.ci, mc.ncas, mc.nelecas)[state]
     mo_coeff, mo_ci, mo_energy = mc.canonicalize (ci=mc.ci[state], cas_natorb=cas_natorb, casdm1=casdm1)
     if not cas_mo_energy:
@@ -13,7 +15,7 @@ def from_sa_mcscf (mc, fname, state=None, cas_natorb=False, cas_mo_energy=False,
     # and apply them also to the StateAverageMCSCFSolver instance
     mo_occ = np.zeros_like (mo_energy)
     mo_occ[:mc.ncore] = 2.0
-    ci = mc.ci
+    ci = [c.copy () for c in mc.ci]
     ci[state] = mo_ci
     mo_occ[mc.ncore:][:mc.ncas] = mc.fcisolver.states_make_rdm1 (ci, mc.ncas, mc.nelecas)[state].diagonal ()
     return from_mo (mc.mol, fname, mo_coeff, occ=mo_occ, ene=mo_energy, **kwargs)
