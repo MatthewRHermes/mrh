fc6403ec9362fb3ff3961d0444fbcee85474ba73
Thais Renee Scott <trscott@midway2-login2.rcc.local>
2021-04-16 08:18:37 -0500

    renamed cmspdft3 to cmspdft

---
+++ my_pyscf/grad/cmspdft.py
@@ -0,0 +1,17 @@
+import numpy as np
+import time
+from itertools import product
+from scipy import linalg
+from pyscf import gto, dft, ao2mo, fci, mcscf, lib
+from pyscf.lib import logger, temporary_env
+from pyscf.mcscf import mc_ao2mo
+from pyscf.mcscf.addons import StateAverageMCSCFSolver, state_average_mix, state_average_mix_, state_average
+from mrh.util.rdm import get_2CDM_from_2RDM, get_2CDMs_from_2RDMs
+from pyscf.fci import direct_spin1
+from mrh.my_pyscf.mcpdft import pdft_veff
+from mrh.my_pyscf.mcpdft.otpd import get_ontop_pair_density
+from mrh.my_pyscf.mcpdft.otfnal import otfnal, transfnal, ftransfnal
+from mrh.my_pyscf.mcpdft import cmspdft
+from mrh.my_pyscf import mcpdft
+from mrh.my_pyscf.grad.mcpdft import Gradients
+
