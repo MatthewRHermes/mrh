c184067bd87e6e1a484138d3d2d5ab35d6a947d4
Matthew R Hermes <herme068@herme068.umn.edu>
2021-05-24 12:46:22 -0500

    More lasucc example

--- examples/lasucc/c4h6_equil_631g_frzci.py
+++ examples/lasucc/c4h6_equil_631g_frzci.py
@@ -3,7 +3,7 @@ from scipy import linalg
 from pyscf import scf, mcscf
 from pyscf.lib import logger
 from c4h6_struct import structure
-from mrh.my_pyscf.tools import molden
+from mrh.my_pyscf.tools import molden as molden
 from mrh.my_pyscf.fci import csf_solver
 from mrh.my_pyscf.mcscf.lasscf_o0 import LASSCF
 from mrh.exploratory.citools.lasci_ominus1 import FCISolver as lasci
@@ -14,11 +14,13 @@ norb = 8
 nelec = 8
 norb_f = (4,4)
 nelec_f = ((2,2),(2,2))
-mol = structure (3.0, 3.0, output='c4h6_stretched_631g_frzci.log', verbose=logger.DEBUG)
+mol = structure (0.0, 0.0, output='c4h6_equil_631g_frzci.log', verbose=logger.DEBUG)
 mf = scf.RHF (mol).run ()
 
 # CASSCF (for orbital initialization)
-mc = mcscf.CASSCF (mf, norb, nelec).set (fcisolver = csf_solver (mol, smult=1)).run ()
+mc = mcscf.CASSCF (mf, norb, nelec).set (fcisolver = csf_solver (mol, smult=1))
+mo_coeff = mc.sort_mo ([11,12,14,15,16,17,21,24])
+mc.kernel (mo_coeff)
 mo_coeff = mc.mo_coeff.copy ()
 
 # LASSCF (for comparison)
@@ -29,13 +31,13 @@ e_las = las.kernel (mo_loc)[0]
 mo_loc = las.mo_coeff.copy ()
 ci0_f = [np.squeeze (fockspace.hilbert2fock (ci[0], no, ne))
     for ci, no, ne in zip (las.ci, las.ncas_sub, las.nelecas_sub)]
-molden.from_lasscf (las, 'c4h6_stretched_lasscf88_631g.molden')
+molden.from_lasscf (las, 'c4h6_equil_lasscf88_631g.molden')
 
 # CASCI (for comparison)
 mc = mcscf.CASCI (mf, 8, 8).set (mo_coeff = mo_loc,
     fcisolver = csf_solver (mol, smult=1)).run ()
 e_cas = mc.e_tot
-molden.from_mcscf (mc, 'c4h6_stretched_casci88_631g.molden', cas_natorb=True)
+molden.from_mcscf (mc, 'c4h6_equil_casci88_631g.molden', cas_natorb=True)
 
 # LASUCCSD
 ucc = mcscf.CASCI (mf, 8, 8).set (fcisolver = lasucc (mol))
@@ -43,12 +45,12 @@ ucc.fcisolver.norb_f = [4,4]
 ucc.fcisolver.frozen = 'ci'
 ucc.fcisolver.get_init_guess = lambda *args: ci0_f
 try:
-    mo_loc = np.load ('c4h6_stretched_631g_frzci.mo.npy')
+    mo_loc = np.load ('c4h6_equil_631g_frzci.mo.npy')
     # ^ In principle, this shouldn't be necessary, however this system happens
     # to have big gauge invariance in its orbital frame. If there was less
     # symmetry, you wouldn't need to cache this part.
-    psi = ucc.fcisolver.load_psi ('c4h6_stretched_631g_frzci.psi.npy',
-        norb, nelec, norb_f, frozen='ci')
+    psi = ucc.fcisolver.load_psi ('c4h6_equil_631g_frzci.psi.npy',
+        norb, nelec, norb_f)
     ucc.fcisolver.psi = psi
     print ("Found cached wave function data")
     print ("This calculation should terminate after 1 cycle")
@@ -59,12 +61,12 @@ except FileNotFoundError as e:
     print (("After this run finishes successfully, the wave function "
             "data will be cached to disk"))
 ucc.kernel (mo_loc)
-molden.from_mcscf (ucc, 'c4h6_stretched_lasuccsd_631g_frzci.molden', cas_natorb=True)
+molden.from_mcscf (ucc, 'c4h6_equil_lasuccsd_631g_frzci.molden', cas_natorb=True)
 e_luc = ucc.e_tot
 
 # Cache result for the next time you run this
-np.save ('c4h6_stretched_631g_frzci.mo', ucc.mo_coeff)
-ucc.fcisolver.save_psi ('c4h6_stretched_631g_frzci.psi', ucc.fcisolver.psi)
+np.save ('c4h6_equil_631g_frzci.mo', ucc.mo_coeff)
+ucc.fcisolver.save_psi ('c4h6_equil_631g_frzci.psi', ucc.fcisolver.psi)
 
 # Some post-processing
 print ('\nLASSCF: %.12f, CASCI: %.12f, LASUCC: %.12f' % (e_las, e_cas, e_luc))
@@ -78,5 +80,4 @@ print ('LASUCC quintet weight: %.5f' % (w_quintet))
 logger.info (ucc, 'LASUCC singlet weight: %.5f', w_singlet)
 logger.info (ucc, 'LASUCC triplet weight: %.5f', w_triplet)
 logger.info (ucc, 'LASUCC quintet weight: %.5f', w_quintet)
-
 
