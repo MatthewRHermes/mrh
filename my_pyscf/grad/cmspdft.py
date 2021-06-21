16271834d6cefff49f379294a8574d678cd4424a
Matthew R Hermes <herme068@herme068.umn.edu>
2021-06-21 12:36:13 -0500

    Syntax check grad/cmspdft.py sarot_response* fns

--- my_pyscf/grad/cmspdft.py
+++ my_pyscf/grad/cmspdft.py
@@ -1,6 +1,8 @@
 import numpy as np
-from pyscf import ao2mo
+from scipy import linalg
+from pyscf import ao2mo, lib
 from pyscf.lib import logger
+from pyscf.mcscf import newton_casscf
 import copy
 
 def sarot_response (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
@@ -12,6 +14,7 @@ def sarot_response (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     if eris is None: eris = mc.ao2mo (mo)
     ncore, ncas, nelecas = mc.ncore, mc.ncas, mc.nelecas
     nroots, nocc = mc_grad.nroots, ncore + ncas
+    nmo = mo.shape[1]
 
     # CI vector shift
     L = np.zeros ((nroots, nroots), dtype=Lis.dtype)
@@ -25,22 +28,23 @@ def sarot_response (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     diag_idx = np.arange (nroots)
     diag_idx = diag_idx * (diag_idx+1) // 2 + diag_idx
     tdm1 = np.stack (mc.fcisolver.states_trans_rdm12 (ci_arr[tril_idx[0]],
-        ci_arr[tril_idx[1]], ncas, nelecas), axis=0)
+        ci_arr[tril_idx[1]], ncas, nelecas)[0], axis=0)
     dm1 = tdm1[diag_idx,:,:]
     edm1 = np.stack (mc.fcisolver.states_trans_rdm12 (Lci, ci, ncas,
         nelecas)[0], axis=0)
     edm1 += edm1.transpose (0,2,1)
 
     # Potentials
-    eri_cas = np.zeros ([ncas,]*4, dtype=dm1.dtype)
-    for i in range (ncore, nocc):
-        eri_cas[i,:,:,:] = eris.ppaa[i][ncore:nocc,:,:]
-    vj = np.tensordot (dm1, eri_cas, axes=2)
-    evj = np.tensordot (edm1, eri_cas, axes=2)
+    aapa = np.zeros ([ncas,ncas,nmo,ncas], dtype=dm1.dtype)
+    for i in range (ncas):
+        j = i + ncore
+        aapa[i,:,:,:] = eris.papa[j][:,:,:]
+    vj = np.tensordot (dm1, aapa, axes=2)
+    evj = np.tensordot (edm1, aapa, axes=2)
 
     # Constants (state-integrals)
-    tvj = np.tensordot (tdm1, eri_cas, axes=2)
-    w = np.tensordot (vj, tdm1, axes=((1,2),(1,2)))
+    tvj = np.tensordot (tdm1, aapa[:,:,ncore:nocc,:], axes=2)
+    w = np.tensordot (tvj, tdm1, axes=((1,2),(1,2)))
     w = ao2mo.restore (1, w, nroots)
     w_IJIJ = np.einsum ('ijij->ij', w)
     w_IIJJ = np.einsum ('iijj->ij', w)
@@ -50,18 +54,19 @@ def sarot_response (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     const_IJ -= np.dot (L, w_IJJJ)
 
     # Orbital degree of freedom
-    Rorb = sum ([np.dot (v, ed) + np.dot (ev, d) for v, d, ev, ed
-        in zip (vj, dm1, evj, edm1)])
+    Rorb = np.zeros ((nmo,nmo), dtype=vj[0].dtype)
+    Rorb[:,ncore:nocc] = sum ([np.dot (v, ed) + np.dot (ev, d) 
+        for v, d, ev, ed in zip (vj, dm1, evj, edm1)])
     Rorb -= Rorb.T
     
     # CI degree of freedom
     def contract (v,c): return mc.fcisolver.contract_1e (v, c, ncas, nelecas)
     Rci = np.tensordot (const_IJ, ci_arr, axes=1) # Delta_IJ |J> term
     vci = np.stack ([contract (v,c) for v, c in zip (vj, ci)], axis=0)
+    Rci -= np.tensordot (L, vci, axes=1) # |W_J>z_{IJ} term
     for I in range (nroots):
         Rci[I] += 2 * contract (vj[I], Lci[I]) # 2 v_I |J>z_{IJ} term
         Rci[I] += 2 * contract (evj[I], ci[I]) # 2 veff_I |I> term
-        Rci[I] -= np.tensordot (L, vci, axes=1) # |W_J>z_{IJ} term
         cc = np.dot (ci[I].ravel ().conj (), Rci[I].ravel ())
         Rci[I] -= ci[I] * cc # Q_I operator
 
@@ -85,7 +90,8 @@ def sarot_response_o0 (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     L[np.tril_indices (nroots, k=-1)] = Lis[:]
     L -= L.T
     ci_arr = np.asarray (ci)
-    Lci = np.tensordot (L, ci_arr, axes=1)
+    Lci = list (np.tensordot (L, ci_arr, axes=1))
+    x = mc_grad.pack_uniq_var (np.zeros ((nmo,nmo)), Lci)
 
     # Fake Hamiltonian!
     h1e_mo = moH @ mc.get_hcore () @ mo
@@ -93,15 +99,23 @@ def sarot_response_o0 (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     for i in range (nmo): feris.papa[i][:,:,:] = 0.0
     feris.vhf_c[:,:] = -h1e_mo.copy ()
 
-    # Fake 2RDM!
+    # Fake 2TDM!
     def trans_rdm12 (ci1, ci0, *args, **kwargs):
         dm1, dm2 = mc.fcisolver.states_trans_rdm12 (ci1, ci0, *args, **kwargs)
         for d1, d2 in zip (dm1, dm2):
             d2[:,:,:,:] = np.multiply.outer (d1, d1)
+        dm1 = sum ([d1 * w for d1, w in zip (dm1, mc.weights)])
+        dm2 = sum ([d2 * w for d2, w in zip (dm2, mc.weights)])
         return dm1, dm2
 
+    # Fake Newton CASSCF!
+    with lib.temporary_env (mc.fcisolver, trans_rdm12=trans_rdm12):
+        hx = newton_casscf.gen_g_hop (mc, mo, ci, feris, verbose=0)[2](x)
+    hx[:mc_grad.ngorb] *= nroots
 
-def sarot_grad (mc_grad, Lis, atmlst=atmlst, mo=None, ci=None, eris=None,
+    return hx
+
+def sarot_grad (mc_grad, Lis, atmlst=None, mo=None, ci=None, eris=None,
         mf_grad=None, **kwargs):
     ''' Returns geometry derivative of Q.x '''
 
@@ -155,5 +169,38 @@ def sarot_grad (mc_grad, Lis, atmlst=atmlst, mo=None, ci=None, eris=None,
     logger.debug (mc, "CMS-PDFT Lis lagrange renorm component:\n{}".format (de_renorm))
     de = de_direct + de_renorm
     return de
+
+if __name__ == '__main__':
+    import math
+    from pyscf import scf, gto, mcscf
+    from mrh.my_pyscf.fci import csf_solver
+    xyz = '''O  0.00000000   0.08111156   0.00000000
+             H  0.78620605   0.66349738   0.00000000
+             H -0.78620605   0.66349738   0.00000000'''
+    mol = gto.M (atom=xyz, basis='6-31g', symmetry=False, output='sipdft.log',
+        verbose=lib.logger.DEBUG)
+    mf = scf.RHF (mol).run ()
+    mc = mcscf.CASSCF (mf, 4, 4).set (fcisolver = csf_solver (mol, 1))
+    mc = mc.state_average ([1.0/3,]*3).run ()
+    ci_arr = np.asarray (mc.ci)
+
+    mc_grad = mc.nuc_grad_method ()
+    Lis = math.pi * (np.random.rand ((3)) - 0.5)
+    eris = mc.ao2mo (mc.mo_coeff)
+
+    dw_test = sarot_response (mc_grad, Lis, mo=mc.mo_coeff, ci=mc.ci, eris=eris)
+    dworb_test, dwci_test = mc_grad.unpack_uniq_var (dw_test)
+    dwci_test = np.asarray (dwci_test)
+    dwis_test = np.einsum ('pab,qab->pq', dwci_test, ci_arr)
+    dwci_test -= np.einsum ('pq,qab->pab', dwis_test, ci_arr.conj ())
+    dw_ref = sarot_response_o0 (mc_grad, Lis, mo=mc.mo_coeff, ci=mc.ci, eris=eris)
+    dworb_ref, dwci_ref = mc_grad.unpack_uniq_var (dw_ref)
+    dwci_ref = np.asarray (dwci_ref)
+    dwis_ref = np.einsum ('pab,qab->pq', dwci_ref, ci_arr)
+    dwci_ref -= np.einsum ('pq,qab->pab', dwis_ref, ci_arr.conj ())
+
+    print ("dworb:", linalg.norm (dworb_test-dworb_ref), linalg.norm (dworb_ref))
+    print ("dwci:", linalg.norm (dwci_test-dwci_ref), linalg.norm (dwci_ref))
+    print ("dwis:", linalg.norm (dwis_test-dwis_ref), linalg.norm (dwis_ref))
 
 
