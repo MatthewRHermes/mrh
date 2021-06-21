f69f0be40f1a25f6d2130915273ff54476ac6395
Matthew R Hermes <herme068@herme068.umn.edu>
2021-06-21 16:01:53 -0500

    grad.cmspdft.sarot_response orb sector sanity pass

--- my_pyscf/grad/cmspdft.py
+++ my_pyscf/grad/cmspdft.py
@@ -70,7 +70,7 @@ def sarot_response (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
         cc = np.dot (ci[I].ravel ().conj (), Rci[I].ravel ())
         Rci[I] -= ci[I] * cc # Q_I operator
 
-    return mc_grad.pack_uniq_var (Rorb, Rci)
+    return mc_grad.pack_uniq_var (2*Rorb, 2*Rci)
 
 def sarot_response_o0 (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     ''' Alternate implementation: monkeypatch everything but active-active
@@ -96,24 +96,79 @@ def sarot_response_o0 (mc_grad, Lis, mo=None, ci=None, eris=None, **kwargs):
     # Fake Hamiltonian!
     h1e_mo = moH @ mc.get_hcore () @ mo
     feris = mc.ao2mo (mo)
-    for i in range (nmo): feris.papa[i][:,:,:] = 0.0
+    for i in range (nmo):
+        feris.papa[i][:,:,:] = 0.0
+        feris.ppaa[i][:ncore,:,:] = 0.0
+        feris.ppaa[i][nocc:,:,:] = 0.0
     feris.vhf_c[:,:] = -h1e_mo.copy ()
+    from pyscf.mcscf.newton_casscf import _pack_ci_get_H as getH
+    from pyscf.mcscf import addons
+    def _pack_ci_get_H (mc1, mo1, ci1):
+        ci1, _, _Hdiag, linkstrl, linkstr, _pack_ci, _unpack_ci = getH (mc1,
+            mo1, ci1)
+        dm1 = mc.fcisolver.states_make_rdm1 (ci1, ncas, nelecas)
+        _state_arg = addons.StateAverageMixFCISolver_state_args
+        if isinstance (mc.fcisolver, addons.StateAverageMixFCISolver):
+            def _Hci (h1, h2, ci2):
+                hci = []
+                tm1 = mc.fcisolver.states_trans_rdm12 (ci2, ci1, ncas, nelecas)[0]
+                for s, args, kwargs in enumerate (mc.fcisolver._loop_solver (
+                        _state_arg (ci2), _state_arg (ci1), _state_arg (dm1))):
+                    ci2i, ci1i, dm1i = args[0:3]
+                    nelec = mc.fcisolver._get_nelec (s, nelecas)
+                    op1 = np.tensordot (tm1i, h2, axes=2)
+                    op2 = np.tensordot (dm1i, h2, axes=2)
+                    hci.extend ((s.contract_1e (h1, ci1i[j], ncas, nelec)
+                               + s.contract_1e (op1[j], ci2i[j], ncas, nelec)
+                               + s.contract_1e (op2[j], ci2i[j], ncas, nelec))
+                               for j in range (len (ci2i)))
+                return hci
+        else:
+            def _Hci (h1, h2, ci2):
+                hci = []
+                tm1 = mc.fcisolver.states_trans_rdm12 (ci2, ci1, ncas, nelecas)[0]
+                for ix, (dm1i, tm1i, ci1i, ci2i) in enumerate (zip (dm1, tm1, ci1, ci2)):
+                    op = h1 + np.tensordot (h2, dm1i, axes=2)
+                    hci1 = mc.fcisolver.contract_1e (op, ci2i, ncas, nelecas)
+                    if abs (ci1i.dot (ci2i)) < 0.5: # Chain rule
+                        op = np.tensordot (h2, tm1i, axes=2)
+                        hci2 = mc.fcisolver.contract_1e (op, ci1i, ncas, nelecas)
+                        hci2 -= ci1i * ci1i.dot (hci2)
+                        hci1 += hci2
+                    hci.append (hci1)
+                return hci
+        return ci1, _Hci, _Hdiag, linkstrl, linkstr, _pack_ci, _unpack_ci
 
     # Fake 2TDM!
+    dm1 = mc.fcisolver.states_make_rdm1 (ci, ncas, nelecas)
     def trans_rdm12 (ci1, ci0, *args, **kwargs):
-        dm1, dm2 = mc.fcisolver.states_trans_rdm12 (ci1, ci0, *args, **kwargs)
-        for d1, d2 in zip (dm1, dm2):
-            d2[:,:,:,:] = np.multiply.outer (d1, d1)
-        dm1 = sum ([d1 * w for d1, w in zip (dm1, mc.weights)])
-        dm2 = sum ([d2 * w for d2, w in zip (dm2, mc.weights)])
-        return dm1, dm2
+        tm1, tm2 = mc.fcisolver.states_trans_rdm12 (ci1, ci0, *args, **kwargs)
+        for t1, t2, d1, w in zip (tm1, tm2, dm1, mc.weights):
+            t2[:,:,:,:] = w * (np.multiply.outer (t1, d1)
+                             + np.multiply.outer (d1, t1))
+            t1[:,:] *= w
+        return sum (tm1), sum (tm2)
 
     # Fake Newton CASSCF!
-    with lib.temporary_env (mc.fcisolver, trans_rdm12=trans_rdm12):
+    with lib.temporary_env (newton_casscf, _pack_ci_get_H=_pack_ci_get_H):
+     with lib.temporary_env (mc.fcisolver, trans_rdm12=trans_rdm12):
         hx = newton_casscf.gen_g_hop (mc, mo, ci, feris, verbose=0)[2](x)
-    hx[:mc_grad.ngorb] *= nroots
+    hx_orb, hx_ci = mc_grad.unpack_uniq_var (hx)
+    hx_orb *= nroots
+    hx_ci = np.asarray (hx_ci)
+    hx_is = np.einsum ('pab,qab->pq', hx_ci, ci_arr.conj ())
+    hx_ci -= np.einsum ('pq,qab->pab', hx_is, ci_arr)
+    
+    # IS degrees of freedom using cmspdft module
+    from mrh.my_pyscf.mcpdft.cmspdft import e_coul
+    Q, dQ, d2Q = e_coul (mc, ci)
+    d2Qx = np.dot (d2Q, Lis)
+    hx_is = np.zeros ((nroots,nroots), dtype=d2Qx.dtype)
+    hx_is[np.tril_indices (nroots, k=-1)] = d2Qx
+    hx_is -= hx_is.T
+    hx_ci += np.einsum ('pq,qab->pab', hx_is, ci_arr)
 
-    return hx
+    return mc_grad.pack_uniq_var (hx_orb, hx_ci)
 
 def sarot_grad (mc_grad, Lis, atmlst=None, mo=None, ci=None, eris=None,
         mf_grad=None, **kwargs):
@@ -191,16 +246,28 @@ if __name__ == '__main__':
     dw_test = sarot_response (mc_grad, Lis, mo=mc.mo_coeff, ci=mc.ci, eris=eris)
     dworb_test, dwci_test = mc_grad.unpack_uniq_var (dw_test)
     dwci_test = np.asarray (dwci_test)
-    dwis_test = np.einsum ('pab,qab->pq', dwci_test, ci_arr)
-    dwci_test -= np.einsum ('pq,qab->pab', dwis_test, ci_arr.conj ())
+    dwis_test = np.einsum ('pab,qab->pq', dwci_test, ci_arr.conj ())
+    dwci_test -= np.einsum ('pq,qab->pab', dwis_test, ci_arr)
     dw_ref = sarot_response_o0 (mc_grad, Lis, mo=mc.mo_coeff, ci=mc.ci, eris=eris)
     dworb_ref, dwci_ref = mc_grad.unpack_uniq_var (dw_ref)
     dwci_ref = np.asarray (dwci_ref)
-    dwis_ref = np.einsum ('pab,qab->pq', dwci_ref, ci_arr)
-    dwci_ref -= np.einsum ('pq,qab->pab', dwis_ref, ci_arr.conj ())
+    dwis_ref = np.einsum ('pab,qab->pq', dwci_ref, ci_arr.conj ())
+    dwci_ref -= np.einsum ('pq,qab->pab', dwis_ref, ci_arr)
 
     print ("dworb:", linalg.norm (dworb_test-dworb_ref), linalg.norm (dworb_ref))
     print ("dwci:", linalg.norm (dwci_test-dwci_ref), linalg.norm (dwci_ref))
     print ("dwis:", linalg.norm (dwis_test-dwis_ref), linalg.norm (dwis_ref))
 
+    #dwci_test = dwci_test.reshape (3,36)
+    #dwci_ref = dwci_ref.reshape (3,36)
+    #dwci_test_norm = linalg.norm (dwci_test, axis=1)
+    #dwci_ref_norm = linalg.norm (dwci_ref, axis=1)
+    #n = dwci_test_norm * dwci_ref_norm
+    #dwci_err_norm = linalg.norm (dwci_test-dwci_ref, axis=1)
+    #dwci_err_ovlp = (dwci_test * dwci_ref).sum (1) / np.sqrt (n)
+    #print (dwci_test_norm, dwci_ref_norm)
+    #print (dwci_err_norm)
+    #print (dwci_err_ovlp)
 
+    print (dwis_test)
+    print (dwis_ref)
