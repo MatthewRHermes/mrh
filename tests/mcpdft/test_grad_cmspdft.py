d8f8e57f8224a125420a3ba9e3cb7dd5c246d1cc
Matthew R Hermes <herme068@herme068.umn.edu>
2021-07-24 12:46:43 -0500

    Add H2 CMS(2)tPBE(2,2)/sto-3g grad unittest

--- tests/mcpdft/test_grad_cmspdft.py
+++ tests/mcpdft/test_grad_cmspdft.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy import linalg
 from pyscf import gto, scf, df, mcscf, lib
+from pyscf.data.nist import BOHR
 from mrh.my_pyscf import mcpdft
 from mrh.my_pyscf.fci import csf_solver
 from mrh.my_pyscf.grad.cmspdft import sarot_response, sarot_grad, sarot_response_o0, sarot_grad_o0
+from mrh.my_pyscf.grad import sipdft as sipdft_grad
 from mrh.my_pyscf.df.grad import dfsacasscf
 import unittest, math
 
@@ -31,17 +33,36 @@ def get_mc_ref (mol, ri=False, sam=False):
         mc = mc.state_average ([0.5,0.5])
     mc.conv_tol = 1e-12
     return mc.run ()
-mc_list = [[[get_mc_ref (m, ri=i, sam=j) for i in (0,1)] for j in (0,1)] for m in (mol_nosymm, mol_symm)]
+#mc_list = [[[get_mc_ref (m, ri=i, sam=j) for i in (0,1)] for j in (0,1)] for m in (mol_nosymm, mol_symm)]
+mc_list = [] # Crunch within unittest.main for accurate clock
+def get_mc_list ():
+    if len (mc_list) == 0:
+        for m in [mol_nosymm, mol_symm]:
+            mc_list.append ([[get_mc_ref (m, ri=i, sam=j) for i in (0,1)] for j in (0,1)])
+    return mc_list
+def do_diatomic (a1='H', a2='H', r=1.6, fnal='tPBE', basis='sto-3g', ncas=2,
+        nelecas=2, nroots=2, symmetry=False):
+    xyz = '{:s} 0 0 0; {:s} {:.2f} 0 0'.format (a1, a2, r)
+    mf = scf.RHF (gto.M (atom=xyz, basis=basis, symmetry=symmetry,
+        output='/dev/null', verbose=0)).run ()
+    mc = mcpdft.CASSCF (mf, fnal, ncas, nelecas, grids_level=9)
+    mc.fcisolver = csf_solver (mf.mol, smult=1)
+    mc = mc.state_interaction ([1.0/nroots,]*nroots, 'cms').run ()
+    mc_grad = sipdft_grad.Gradients (mc)
+    de = [mc_grad.kernel (state=i) [1,0] / BOHR for i in range (nroots)]
+    mf.mol.stdout.close ()
+    return de
+
 def tearDownModule():
-    global mol_nosymm, mol_symm, mc_list, Lis
+    global mol_nosymm, mol_symm, mc_list, Lis, get_mc_list, do_diatomic
     mol_nosymm.stdout.close ()
     mol_symm.stdout.close ()
-    del mol_nosymm, mol_symm, mc_list, Lis
+    del mol_nosymm, mol_symm, mc_list, Lis, get_mc_list, do_diatomic
 
 class KnownValues(unittest.TestCase):
 
     def test_sarot_response_sanity (self):
-        for mcs, stype in zip (mc_list, ('nosymm','symm')):
+        for mcs, stype in zip (get_mc_list (), ('nosymm','symm')):
          for mca, atype in zip (mcs, ('nomix','mix')):
           if atype == 'mix': continue # TODO: enable state-average-mix
           for mc, itype in zip (mca, ('conv', 'DF')):
@@ -68,7 +89,7 @@ class KnownValues(unittest.TestCase):
                 self.assertAlmostEqual (lib.fp (dwis_test), lib.fp (dwis_ref), 8)
 
     def test_sarot_grad_sanity (self):
-        for mcs, stype in zip (mc_list, ('nosymm','symm')):
+        for mcs, stype in zip (get_mc_list (), ('nosymm','symm')):
          for mca, atype in zip (mcs, ('nomix','mix')):
           for mc, itype in zip (mca, ('conv', 'DF')):
             ci_arr = np.asarray (mc.ci)
@@ -81,6 +102,12 @@ class KnownValues(unittest.TestCase):
                 dh_ref = sarot_grad_o0 (mc_grad, Lis, mo=mc.mo_coeff, ci=mc.ci, eris=eris)
                 self.assertAlmostEqual (lib.fp (dh_test), lib.fp (dh_ref), 8)
 
+    def test_grad_h2 (self):
+        de = do_diatomic ()
+        with self.subTest (root=0):
+            self.assertAlmostEqual (de[0], 0.03622639, 4) # numerical
+        with self.subTest (root=1):
+            self.assertAlmostEqual (de[1], -0.06910341, 4) # numerical
 
 if __name__ == "__main__":
     print("Full Tests for CMS-PDFT gradient objective fn derivatives")
