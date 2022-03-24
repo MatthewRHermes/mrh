e4dc883e9166c247c541706fccd5776a42351e78
Matthew R Hermes <mrhermes@uchicago.edu>
2022-03-24 14:16:16 -0500

    Found logic problem in orb_scf
    mc1step.update_jk_in_ah only gets called for ncore>0, but I need
    the MC-PDFT version of this to be called for any ncore.

--- my_pyscf/mcpdft/orb_scf.py
+++ my_pyscf/mcpdft/orb_scf.py
@@ -55,13 +55,25 @@ def mc1step_gen_g_hop (mc, mo, u, casdm1, casdm2, eris):
     def get_hcore (mol=None):
         return mc._scf.get_hcore (mol) + veff1
     with lib.temporary_env (mc, get_hcore=get_hcore):
-        g_orb, _, h_op, h_diag = mc1step.gen_g_hop (mc, mo, u, casdm1, casdm2,
+        g_orb, _, h_op0, h_diag = mc1step.gen_g_hop (mc, mo, u, casdm1, casdm2,
             veff2)
     gorb_update = get_gorb_update (mc, mo)
+    def h_op (x):
+        # OT second density derivative
+        dg = h_op0 (x)
+        x1 = mc.unpack_uniq_var (x)
+        x2 = mc.update_jot_in_ah (mo, x1, casdm1, veff2)
+        return dg + mc.pack_uniq_var(x2-x2.T)
     return g_orb, gorb_update, h_op, h_diag
 
-def mc1step_update_jk_in_ah (mc, mo_coeff, x1, casdm1, veff2):
-    ''' Wrapper to mc1step.update_jk_in_ah for minimizing the PDFT
+def mc1step_update_jk_in_ah (*args, **kwargs):
+    ''' Wrapper to nullify mc1step.update_jk_in_ah, which is called
+        only when nocc>0 (inapplicable to MC-PDFT). Hybrid fnals might
+        need something here, though. ''' 
+    return 0,0
+
+def update_jot_in_ah (mc, mo_coeff, x1, casdm1, veff2):
+    ''' Replacement for mc1step.update_jk_in_ah for minimizing the PDFT
         energy instead of the CASSCF energy by varying orbitals '''
     ncore, ncas = mc.ncore, mc.ncas
     nocc = ncore + ncas
@@ -76,15 +88,12 @@ def mc1step_update_jk_in_ah (mc, mo_coeff, x1, casdm1, veff2):
     # Coulomb 
     ddm1 += ddm1.T
     dvj = mo_coeff.conj ().T @ mc._scf.get_j (dm=ddm1) @ mo_coeff
-    dg = dm1 @ dvj # convention of mc1step: density matrix index first
+    x2 = dm1 @ dvj # convention of mc1step: density matrix index first
 
     # OT
-    dg[:nocc] += veff2.eot_h_op (x1)[0]
+    x2[:nocc] += veff2.eot_h_op (x1)[0]
 
-    # comply with return signature
-    dg_a = dg[ncore:nocc]
-    dg_c = dg[:ncore,ncore:]
-    return dg_a, dg_c    
+    return x2 
 
 
 if __name__ == '__main__':
@@ -95,13 +104,17 @@ if __name__ == '__main__':
     mf = scf.RHF (mol).run ()
     mc = mcpdft.CASSCF (mf, 'tPBE', 2, 2).run ()
     print ("Ordinary H2 tPBE energy:",mc.e_tot)
+    opt_mo = mc.mo_coeff.copy ()
+    opt_ci = mc.ci.copy ()
 
     nao, nmo = mc.mo_coeff.shape
     ncore, ncas, nelecas = mc.ncore, mc.ncas, mc.nelecas
     nocc = ncore+ncas
 
     casdm1, casdm2 = mc.fcisolver.make_rdm12 (mc.ci, ncas, nelecas)
-    mc.update_jk_in_ah = mc1step_update_jk_in_ah
+    from functools import partial
+    mc.update_jk_in_ah = partial (mc1step_update_jk_in_ah, mc)
+    mc.update_jot_in_ah = partial (update_jot_in_ah, mc)
     g_orb, gorb_update, h_op, h_diag = mc1step_gen_g_hop (mc,
         mc.mo_coeff, 1, casdm1, casdm2, None)
 
@@ -120,13 +133,33 @@ if __name__ == '__main__':
 
     mc.mo_coeff = np.dot (mc.mo_coeff, u0)
     e_tot, e_ot = mc.energy_tot ()
+    g_orb1 = mc1step_gen_g_hop (mc,
+        mc.mo_coeff, 1, casdm1, casdm2, None)[0]
     print ("H2 tPBE energy after rotating orbitals:", e_tot)
+    print ("g_orb after rotating orbitals (should be same as gorb_update):", g_orb1)
 
+    e_tot, e_cas, mc.ci = mc.casci (mc.mo_coeff)
+    e_tot, e_ot = mc.energy_tot ()
+    casdm1, casdm2 = mc.fcisolver.make_rdm12 (mc.ci, ncas, nelecas)
+    g_orb2 = mc1step_gen_g_hop (mc,
+        mc.mo_coeff, 1, casdm1, casdm2, None)[0]
+    print ("H2 tPBE energy after rotating orbitals and then reoptimizing ci:", e_tot)
+    print ("g_orb after both updates:", g_orb2)
+    
+    print (opt_ci)
+    print (mc.ci)
+    mc.mo_coeff, mc.ci = opt_mo, opt_ci
     print ("\nNow attempting to minimize...")
-    mcv0 = mcpdft.CASSCF (mc, 'tPBE', 2, 2, ci_min='epdft')
+    mcv0 = mcpdft.CASSCF (mf, 'tPBE', 2, 2).set (mo_coeff=opt_mo, ci=opt_ci)
     class orbvarMCPDFT (mcv0.__class__):
         gen_g_hop = mc1step_gen_g_hop
         update_jk_in_ah = mc1step_update_jk_in_ah
+        update_jot_in_ah = update_jot_in_ah
+        def casci (self, mo_coeff, ci0=None, eris=None, verbose=None, envs=None):
+            e_tot, e_cas, fcivec = mcv0.__class__.casci (
+                self, mo_coeff, ci0=ci0, eris=eris, verbose=verbose, envs=envs)
+            epdft = mcv0.energy_tot (mo_coeff=mo_coeff, ci=fcivec)[0]
+            return e_tot, e_cas, fcivec
 
     mcv = orbvarMCPDFT (mcv0._scf, mcv0.ncas, mcv0.nelecas, my_ot=mcv0.otxc)
     _keys = mcv._keys.copy ()
@@ -135,4 +168,9 @@ if __name__ == '__main__':
     mcv.kernel ()
     print ("H2 tPBE energy after attempted minimization:", mcv.e_tot, '(MCSCF E = ', mcv.e_mcscf, ')')
 
+    casdm1, casdm2 = mcv.fcisolver.make_rdm12 (mcv.ci, ncas, nelecas)
+    mcv.update_jk_in_ah = mc1step_update_jk_in_ah
+    g_orb, gorb_update, h_op, h_diag = mc1step_gen_g_hop (mcv,
+        mcv.mo_coeff, 1, casdm1, casdm2, None)
+    print ("g_orb:", g_orb)
 
