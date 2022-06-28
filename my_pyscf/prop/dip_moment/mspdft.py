dcd694068a46fc15468b4a14160b6601abbedb69
Aleksandr Lykhin <alexlykhin@gmail.com>
2022-06-28 17:26:27 -0500

    cleaning and importing redundant functions

--- my_pyscf/prop/dip_moment/mspdft.py
+++ my_pyscf/prop/dip_moment/mspdft.py
@@ -1,76 +1,11 @@
-from pyscf.mcscf import newton_casscf
-from pyscf.grad import rks as rks_grad
-from pyscf.dft import gen_grid
-from pyscf.lib import logger, pack_tril, current_memory, tag_array
-from pyscf.grad import sacasscf
-from pyscf.mcscf.casci import cas_natorb
-from mrh.my_pyscf.mcpdft.otpd import get_ontop_pair_density, _grid_ao2mo
-from mrh.my_pyscf.mcpdft.pdft_veff import _contract_vot_rho, _contract_ao_vao
-from mrh.util.rdm import get_2CDM_from_2RDM
+from pyscf.lib import logger
 from functools import reduce
-from scipy import linalg
 import numpy as np
-import time, gc
 from pyscf.data import nist
 from pyscf import lib
-from mrh.my_pyscf.grad import mcpdft
 from mrh.my_pyscf.grad import mspdft
-from pyscf.fci import direct_spin1
-from mrh.my_pyscf.grad.mspdft import _unpack_state
-
-# TODO: state-average-mix generalization ?
-def make_rdm1_heff_offdiag (mc, ci, si_bra, si_ket): 
-    '''Compute <bra|O|ket> - sum_i <i|O|i>, where O is the 1-RDM
-    operator product, and |bra> and |ket> are both states spanning the
-    vector space of |i>, which are multi-determinantal many-electron
-    states in an active space.
-
-    Args:
-        mc : object of class CASCI or CASSCF
-            Only "ncas" and "nelecas" are used, to determine Hilbert
-            of ci
-        ci : ndarray or list of length (nroots)
-            Contains CI vectors spanning a model space
-        si_bra : ndarray of shape (nroots)
-            Coefficients of ci elements for state |bra>
-        si_ket : ndarray of shape (nroots)
-            Coefficients of ci elements for state |ket>
-
-    Returns:
-        casdm1 : ndarray of shape [ncas,]*2
-            Contains O = p'q case
-    '''
-    ncas, nelecas = mc.ncas, mc.nelecas
-    nroots = len (ci)
-    ci_arr = np.asarray (ci)
-    ci_bra = np.tensordot (si_bra, ci_arr, axes=1)
-    ci_ket = np.tensordot (si_ket, ci_arr, axes=1)
-    casdm1, _ = direct_spin1.trans_rdm12 (ci_bra, ci_ket, ncas, nelecas)
-    ddm1 = np.zeros ((nroots, ncas, ncas), dtype=casdm1.dtype)
-    for i in range (nroots):
-        ddm1[i,...], _ = direct_spin1.make_rdm12 (ci[i], ncas, nelecas)
-    si_diag = si_bra * si_ket
-    casdm1 -= np.tensordot (si_diag, ddm1, axes=1)
-    return casdm1
-
-def get_guage_origin(mol,origin):
-    charges = mol.atom_charges()
-    coords  = mol.atom_coords()
-    mass    = mol.atom_mass_list()
-    if isinstance(origin,str):
-        if origin.upper() == 'COORD_CENTER':
-            center = (0,0,0)
-        elif origin.upper() == 'MASS_CENTER':
-            center = np.einsum('i,ij->j', mass, coords) / mass.sum()
-        elif origin.upper() == 'CHARGE_CENTER':
-            center = np.einsum('z,zx->x', charges, coords) / charges.sum()
-        else:
-            raise RuntimeError ("Gauge origin is not recognized")
-    elif isinstance(center,str):
-        center = origin
-    else:
-        raise RuntimeError ("Gauge origin must be a string or tuple")
-    return center
+from mrh.my_pyscf.grad.mspdft import _unpack_state, make_rdm12_heff_offdiag
+from mrh.my_pyscf.prop.dip_moment.mcpdft import get_guage_origin, nuclear_dipole
 
 def sipdft_HellmanFeynman_dipole (mc, si_bra=None, si_ket=None,
         state=None, mo_coeff=None, ci=None, si=None, origin='Coord_Center'):
@@ -78,10 +13,6 @@ def sipdft_HellmanFeynman_dipole (mc, si_bra=None, si_ket=None,
     if mo_coeff is None: mo_coeff = mc.mo_coeff
     if ci is None: ci = mc.ci
     if si is None: si = mc.si
-    if mc.frozen is not None:
-        raise NotImplementedError
-    t0 = (logger.process_clock (), logger.perf_counter ())
-
     ket, bra = _unpack_state (state)
     if si_bra is None: si_bra = mc.si[:,bra]
     if si_ket is None: si_ket = mc.si[:,ket]
@@ -106,7 +37,7 @@ def sipdft_HellmanFeynman_dipole (mc, si_bra=None, si_ket=None,
         dm_i = dm_cas + dm_core
         dm_diag += amp * dm_i
     # Off-diagonal part
-    casdm1 = make_rdm1_heff_offdiag (mc, ci, si_bra, si_ket)
+    casdm1, _ = make_rdm12_heff_offdiag (mc, ci, si_bra, si_ket)
     dm_off = reduce(np.dot, (mo_cas, casdm1, mo_cas.T))
 
     dm = dm_diag + dm_off
@@ -116,16 +47,6 @@ def sipdft_HellmanFeynman_dipole (mc, si_bra=None, si_ket=None,
         ao_dip = mol.intor_symmetric('int1e_r', comp=3)
     elec_term = -np.einsum('xij,ij->x', ao_dip, dm).real
     return elec_term
-
-def nuclear_dipole(mc,origin='Coord_Center'):
-    '''Compute nuclear contribution wrt gauge origin of the dipole moment'''
-    mol = mc.mol
-    center = get_guage_origin(mol,origin)
-    charges = mol.atom_charges()                           
-    coords  = mol.atom_coords()
-    coords -= center
-    nucl_term = np.einsum('i,ix->x', charges, coords)
-    return nucl_term
 
 class ElectricDipole (mspdft.Gradients):
 
@@ -137,9 +58,6 @@ class ElectricDipole (mspdft.Gradients):
             eris, veff1, veff2, and d2f should be available to all top-level
             functions: get_wfn_response, get_Aop_Adiag, get_ham_repsonse, and
             get_LdotJnuc
- 
-            freeze_is == True sets the is component of the response to zero
-            for debugging purposes
         '''
         if state is None: state = self.state
         if mo is None: mo = self.base.mo_coeff
