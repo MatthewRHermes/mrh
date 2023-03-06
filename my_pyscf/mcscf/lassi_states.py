cb441cc35711eba34eea5703d2264a5b255c2023
Matthew R Hermes <mrhermes@uchicago.edu>
2023-03-06 13:47:03 -0600

    lassi_states.py check duplicates

--- my_pyscf/mcscf/lassi_states.py
+++ my_pyscf/mcscf/lassi_states.py
@@ -6,7 +6,7 @@ from mrh.my_pyscf.fci.csfstring import ImpossibleSpinError
 import itertools
 
 class SingleLASState (object):
-    def __init__(self, las, spins, smults, charges, nlas=None, nelelas=None, stdout=None,
+    def __init__(self, las, spins, smults, charges, weight, nlas=None, nelelas=None, stdout=None,
                  verbose=None):
         if nlas is None: nlas = las.ncas_sub
         if nelelas is None: nelelas = [sum (_unpack_nelec (x)) for x in las.nelecas_sub]
@@ -17,6 +17,7 @@ class SingleLASState (object):
         self.nfrag = len (nlas)
         self.spins, self.smults = np.asarray (spins), np.asarray (smults)
         self.charges = np.asarray (charges)
+        self.weight = weight
         self.stdout, self.verbose = stdout, verbose
         
         self.nelec = self.nelelas - self.charges
@@ -25,6 +26,16 @@ class SingleLASState (object):
         self.nhole = 2*self.nlas - self.nelec 
         self.nholea = self.nlas - self.neleca
         self.nholeb = self.nlas - self.nelecb
+
+    def __eq__(self, other):
+        if self.nfrag != other.nfrag: return False
+        return (np.all (self.spins==other.spins) and 
+                np.all (self.smults==other.smults) and
+                np.all (self.charges==other.charges))
+
+    def __hash__(self):
+        return hash (tuple ([self.nfrag,] + list (self.spins) + list (self.smults)
+                            + list (self.charges)))
 
     def possible_excitation (self, i, a, s):
         i, a, s = np.atleast_1d (i, a, s)
@@ -58,11 +69,30 @@ class SingleLASState (object):
         a_nelecb = (self.nelelas[a]-charges[a]-spins[a]) // 2
         i_ncsf = CSFTransformer (self.nlas[i], i_neleca, i_nelecb, smults[i]).ncsf
         a_ncsf = CSFTransformer (self.nlas[a], a_neleca, a_nelecb, smults[a]).ncsf
+        if (i_neleca==self.nlas[i]) and (i_nelecb==self.nlas[i]) and (smults[i]>1):
+            raise ImpossibleSpinError ("norb = {}; nelec = {}; smult = {}".format (
+                self.nlas[i], (i_neleca, i_nelecb), smults[i]))
+        if (a_neleca==self.nlas[a]) and (a_nelecb==self.nlas[a]) and (smults[a]>1):
+            raise ImpossibleSpinError ("norb = {}; nelec = {}; smult = {}".format (
+                self.nlas[a], (a_neleca, a_nelecb), smults[a]))
+        if (i_neleca==0) and (i_nelecb==0) and (smults[i]>1):
+            raise ImpossibleSpinError ("norb = {}; nelec = {}; smult = {}".format (
+                self.nlas[i], (i_neleca, i_nelecb), smults[i]))
+        if (a_neleca==0) and (a_nelecb==0) and (smults[a]>1):
+            raise ImpossibleSpinError ("norb = {}; nelec = {}; smult = {}".format (
+                self.nlas[a], (a_neleca, a_nelecb), smults[a]))
+        log.info ("spin={} electron from {} to {}".format (dm, i, a))
         log.info ("c,m,s=[{},{},{}]->c,m,s=[{},{},{}]; {},{} CSFs".format (
             self.charges, self.spins, self.smults,
             charges, spins, smults,
             i_ncsf, a_ncsf))
-        return SingleLASState (self.las, spins, smults, charges, nlas=self.nlas,
+        assert (i_neleca>=0)
+        assert (i_nelecb>=0)
+        assert (a_neleca>=0)
+        assert (a_nelecb>=0)
+        assert (i_ncsf)
+        assert (a_ncsf)
+        return SingleLASState (self.las, spins, smults, charges, 0, nlas=self.nlas,
                                nelelas=self.nelelas, stdout=self.stdout, verbose=self.verbose)
 
     def get_singles (self):
@@ -80,7 +110,7 @@ class SingleLASState (object):
         # move 1 beta electron
         has_eb = np.where (self.nelecb > 0)[0]
         has_hb = np.where (self.nholeb > 0)[0]
-        for i, a in itertools.product (has_ea, has_ha):
+        for i, a in itertools.product (has_eb, has_hb):
             if i==a: continue
             for si, sa in itertools.product (range (-1,2,2), repeat=2):
                 try:
@@ -91,30 +121,64 @@ class SingleLASState (object):
 
 def all_single_excitations (las):
     from mrh.my_pyscf.mcscf.lasci import get_state_info
-    ref_states = [SingleLASState (las, m, s, c) for c,m,s,w in zip (*get_state_info (las))]
+    ref_states = [SingleLASState (las, m, s, c, 0) for c,m,s,w in zip (*get_state_info (las))]
+    for weight, state in zip (las.weights, ref_states): state.weight = weight
     new_states = []
     for ref_state in ref_states:
         new_states.extend (ref_state.get_singles ())
-    return new_states
+    seen = set (ref_states)
+    all_states = ref_states + [state for state in new_states if not ((state in seen) or seen.add (state))]
+    weights = [state.weight for state in all_states]
+    charges = [state.charges for state in all_states]
+    spins = [state.spins for state in all_states]
+    smults = [state.smults for state in all_states]
+    #wfnsyms = [state.wfnsyms for state in all_states]
+    return las.state_average (weights=weights, charges=charges, spins=spins, smults=smults)
 
 
 if __name__=='__main__':
     from mrh.tests.lasscf.c2h4n4_struct import structure as struct
-    from pyscf import scf
     from mrh.my_pyscf.mcscf.lasscf_o0 import LASSCF
+    from mrh.my_pyscf.fci import csf_solver
+    from pyscf import scf, mcscf
+    from pyscf.tools import molden
     mol = struct (2.0, 2.0, '6-31g', symmetry=False)
+    mol.spin = 8
     mol.verbose = logger.INFO
     mol.output = 'lassi_states.log'
     mol.build ()
     mf = scf.RHF (mol).run ()
-    las = LASSCF (mf, (4,4), (4,4), spin_sub=(1,1))
-    #las.state_average_(weights=[1.0/5.0,]*5,
-    #    spins=[[0,0],[0,0],[2,-2],[-2,2],[2,2]],
-    #    smults=[[1,1],[3,3],[3,3],[3,3],[3,3]])
-    print (len (all_single_excitations (las)))
-
-
-
+    las = LASSCF (mf, (4,2,4), ((2,2),(1,1),(2,2)), spin_sub=(1,1,1))
+    mo_coeff = las.localize_init_guess ([[0,1,2],[3,4,5,6],[7,8,9]])
+    las.kernel (mo_coeff)
+    elas0 = las.e_tot
+    print ("LASSCF:", elas0)
+    casdm1 = las.make_casdm1 ()
+    no_coeff, no_ene, no_occ = las.canonicalize (natorb_casdm1=casdm1)[:3]
+    molden.from_mo (las.mol, 'lassi_states.lasscf.molden', no_coeff, ene=no_ene, occ=no_occ)
+    las2 = all_single_excitations (las)
+    las2.lasci ()
+    e_roots, si = las2.lassi ()
+    elas1 = e_roots[0]
+    print ("LASSI(S):", elas1)
+    from mrh.my_pyscf.mcscf import lassi
+    casdm1 = lassi.roots_make_rdm12s (las2, las2.ci, si)[0][0].sum (0)
+    no_coeff, no_ene, no_occ = las.canonicalize (natorb_casdm1=casdm1)[:3]
+    molden.from_mo (las.mol, 'lassi_states.lassis.molden', no_coeff, ene=no_ene, occ=no_occ)
+    las3 = all_single_excitations (las2)
+    las3.lasci ()
+    e_roots, si = las3.lassi ()
+    elas2 = e_roots[0]
+    print ("LASSI(SD):", elas2)
+    casdm1 = lassi.roots_make_rdm12s (las3, las3.ci, si)[0][0].sum (0)
+    no_coeff, no_ene, no_occ = las.canonicalize (natorb_casdm1=casdm1)[:3]
+    molden.from_mo (las.mol, 'lassi_states.lassisd.molden', no_coeff, ene=no_ene, occ=no_occ)
+    mc = mcscf.CASCI (mf, (10), (5,5)).set (fcisolver=csf_solver (mol, smult=1))
+    mc.kernel (mo_coeff=las.mo_coeff)
+    ecas = mc.e_tot
+    print ("CASCI:", ecas)
+    no_coeff, no_ci, no_occ = mc.cas_natorb ()
+    molden.from_mo (las.mol, 'lassi_states.casci.molden', no_coeff, occ=no_occ)
 
 
 
