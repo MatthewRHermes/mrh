2c8ede957d5871161041c6860c45de3aa8d7c4db
Bhavnesh Jangid <jangidbhavnesh@midway3-login3.rcc.local>
2023-05-09 11:57:43 -0500

    Example added for LAS-PDFT

---
+++ examples/laspdft/C2.0.LASPDFT.py
@@ -0,0 +1,34 @@
+import sys
+from pyscf import gto, scf, tools, dft
+from pyscf.tools import molden
+from mrh.my_pyscf.mcscf.lasscf_o0 import LASSCF
+from mrh.my_dmet import localintegrals, dmet, fragments
+from mrh.my_dmet.fragments import make_fragment_atom_list, make_fragment_orb_list
+from mrh.my_pyscf.mcscf import LASPDFT
+
+# Initialisation
+mol = gto.M()
+mol.atom='C2.xyz'
+mol.basis='sto3g'
+mol.spin = 0
+mol.charge = 0
+mol.verbose = 5
+mol.build()
+
+# Performing the mean field calculation
+mf = scf.ROHF(mol)
+mf.newton()
+mf.run()
+
+# Set up the localized AO basis
+las = LASSCF(mf,(2, 2),(2, 2),spin_sub=(1, 1))
+las.verbose = 5
+frag_atom_list = ([1, 6] , [2, 5]) 
+mo0 = las.localize_init_guess(frag_atom_list)
+molden.from_mo(mol, f'C2.{mol.spin}.molden', mo0)
+las.set(ah_level_shift=1e-5)
+las.kernel(mo0)
+
+
+# LAS-PDFT
+mc = LASPDFT(las, 'tPBE', 4, 4, verbose = 5)
