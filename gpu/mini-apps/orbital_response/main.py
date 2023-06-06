ee54c9ca651ad379f2ae4f69175a2717cafde48d
cjknight <cjknight2009@gmail.com>
2023-06-06 14:34:42 -0500

    small updates to mini-app

--- gpu/mini-apps/orbital_response/main.py
+++ gpu/mini-apps/orbital_response/main.py
@@ -14,10 +14,26 @@ libgpu.libgpu_set_device(gpu, gpu_id)
 # ------------------------------------------- #
 
 ncore, nocc, nmo = (7, 9, 26) # nfrags = 1
-ncore, nocc, nmo = (13, 17, 48) # nfrags = 2
-ncore, nocc, nmo = (19, 25, 70) # nfrags = 3
+#ncore, nocc, nmo = (13, 17, 48) # nfrags = 2
+#ncore, nocc, nmo = (25, 33, 92) # nfrags = 4
+#ncore, nocc, nmo = (49, 65, 180) # nfrags = 8
+#ncore, nocc, nmo = (97, 129, 356) # nfrags = 16
+#ncore, nocc, nmo = (193, 257, 708) # nfrags = 32
+#ncore, nocc, nmo = (385, 513, 1412) # nfrags = 64
 
 gap = nocc - ncore
+
+size = nmo * nmo * gap * gap
+size += nmo * gap * nmo * gap
+size += nmo * gap * gap * gap
+
+size += gap * gap * gap * nmo
+size += gap * gap * gap * gap
+size += nmo * nmo
+
+size *= 8 / (1024 * 1024 * 1024)
+
+print("Allocating ", size, "GBs")
 
 ppaa = np.random.rand(nmo, nmo, gap, gap)
 papa = np.random.rand(nmo, gap, nmo, gap)
@@ -51,6 +67,18 @@ for i in range(nmo):
         err += (ref[i,j] - f1_prime[i,j]) * (ref[i,j] - f1_prime[i,j])
 
 print("\nError= ", err)
+
+num_iter = 10
+for i in range(num_iter):
+    ocm2 = ocm2_orig
+    tcm2 = tcm2_orig
+    ref = baseline.orbital_response(ppaa, papa, paaa, ocm2, tcm2, gorb, ncore, nocc, nmo)
+
+for i in range(num_iter):
+    f1_prime = np.zeros((nmo, nmo), dtype=np.float64)
+    ocm2 = ocm2_orig
+    tcm2 = tcm2_orig
+    libgpu.libgpu_orbital_response(gpu, f1_prime, ppaa, papa, paaa, ocm2, tcm2, gorb, ncore, nocc, nmo)
 
 # ------------------------------------------- #
 
