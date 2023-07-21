4aa0d8a02f264d01c087d13dbb9b990a10e4847b
Matthew R Hermes <mrhermes@uchicago.edu>
2023-07-21 17:27:59 -0500

    lassi_vec.get_rootspace_central_moment

--- my_pyscf/mcscf/lassi_vec.py
+++ my_pyscf/mcscf/lassi_vec.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pyscf import lib
 from scipy import linalg
+from mrh.my_pyscf.mcscf.lasci import get_space_info
 
 def decompose_sivec_by_rootspace (las, si, ci=None):
     '''Decompose a set of LASSI vectors as
@@ -56,6 +57,43 @@ def make_sdm1 (sivec, lroots, site):
     sivec = sivec.transpose (*idx).reshape (-1, lroots[site],nroots)
     return lib.einsum ('api,aqi->ipq', sivec.conj(), sivec)
     
+def get_rootspace_central_moment (las, space_weights, n=1):
+    '''Compute either the mean (if n==1) or the nth central moment
+    of the quantum numbers that define the rootspaces of a LASSI
+    wave function. This means the average local charge and the two
+    average local spin quantum numbers (average point group is not
+    defined)
+
+    Args:
+        las: instance of :class:`LASCINoSymm`
+        space_weights: ndarray of shape (las.nroots,nvec)
+            Rootspace coefficients from a LASSI wave function
+            (see `decompose_sivec_by_rootspace`)
+
+    Kwargs:
+        n: integer
+            Moment to return; if n==1, returns the mean
+
+    Returns:
+        charges: ndarray of shape (nvec,las.nfrags)
+            nth moment of the local charges
+        spins: ndarray of shape (nvec,las.nfrags)
+            nth moment of the local spin polarizations
+        smults: ndarray of shape (nvec,las.nfrags)
+            nth moment of the local spin magnitudes'''
+    if space_weights.ndim<2:
+        space_weights = space_weights[:,None]
+    nvec = space_weights.shape[1]
+    charges, spins, smults, wfnsyms = get_space_info (las)
+    c1 = np.dot (charges.T, space_weights).T
+    m1 = np.dot (spins.T, space_weights).T
+    s1 = np.dot (smults.T, space_weights).T
+    if n==1:
+        return c1, m1, s1
+    c2 = lib.einsum ('frv,rv->vf', np.power (charges.T[:,:,None]-c1.T[:,None,:], n), space_weights)
+    m2 = lib.einsum ('frv,rv->vf', np.power (spins.T[:,:,None]-m1.T[:,None,:], n), space_weights)
+    s2 = lib.einsum ('frv,rv->vf', np.power (smults.T[:,:,None]-s1.T[:,None,:], n), space_weights)
+    return c2, m2, s2
 
 
 
