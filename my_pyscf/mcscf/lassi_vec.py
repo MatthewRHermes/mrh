2a41bb5e162f2bd53e991a254397dc24225119f7
Matthew R Hermes <mrhermes@uchicago.edu>
2023-07-27 11:48:12 -0500

    add empty lassi_vec.py file with warning

---
+++ my_pyscf/mcscf/lassi_vec.py
@@ -0,0 +1,3 @@
+from mrh.my_pyscf.lassi.sitools import *
+from mrh.util.io import lassi_dir_warn
+lassi_dir_warn ()
--- util/io.py
+++ util/io.py
@@ -23,12 +23,15 @@ mcpdft_removal_warning = FutureWarning((
     "Most MC-PDFT and MC-DCFT modules have been moved to pyscf-forge "
     "(github.com/pyscf/pyscf-forge) and will be removed from mrh soon."
     ))
-lassi_dir_warning = FutureWarning((
-    "LASSI code is being moved from my_pyscf.mcscf.lassi_* module files "
-    "my_pyscf.lassi.* module files; the former will be removed soon."
-    ))
 warnings.filterwarnings ("once", message=str(mcpdft_removal_warning),
                          category=FutureWarning)
 def mcpdft_removal_warn (): warnings.warn (mcpdft_removal_warning,
                                            stacklevel=3)
 
+lassi_dir_warning = FutureWarning((
+    "LASSI code is being moved from my_pyscf.mcscf.lassi_* module files "
+    "to my_pyscf.lassi.* module files; the former will be removed soon."
+    ))
+warnings.filterwarnings ("once", message=str(lassi_dir_warning),
+                         category=FutureWarning)
+def lassi_dir_warn (): warnings.warn (lassi_dir_warning, stacklevel=3)
