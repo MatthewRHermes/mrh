2a72e45301e7d00abad89a05f9f624045e2e80bb
cjknight <cjknight2009@gmail.com>
2023-09-19 16:12:59 -0500

    cleanup & sync OpenMPTarget backend

--- gpu/gpu4pyscf/df/df_jk.py
+++ gpu/gpu4pyscf/df/df_jk.py
@@ -140,7 +140,8 @@ def get_jk(dfobj, dm, hermi=1, with_j=True, with_k=True, direct_scf_tol=1e-13):
             naux, nao_pair = eri1.shape
             if count == 0:
                 libgpu.libgpu_init_get_jk(gpu, eri1, dmtril, blksize, nset, nao, count)
-#            print("count= ", count, "nao= ", nao, " naux= ", naux, "  nao_pair= ", nao_pair, " blksize= ", blksize, " nset= ", nset, " eri1= ", eri1.shape, " dmtril= ", dmtril.shape, " dms= ", numpy.shape(dms), " vj= ", vj_tmp.shape, " vk= ", vk_tmp.shape)
+#            print("count= ", count, "nao= ", nao, " naux= ", naux, "  nao_pair= ", nao_pair, " blksize= ", blksize, " nset= ", nset, " eri1= ", eri1.shape, " dmtril= ", dmtril.shape, " dms= ", numpy.shape(dms))
+#            print("vj= ", vj_tmp.shape, " vk= ", vk_tmp.shape)
             
             if gpu:
                 
