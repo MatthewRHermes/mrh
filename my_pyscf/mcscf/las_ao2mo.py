241384165bb2b74bf3b75d4612264ee69f6548e3
Matthew R Hermes <mrhermes@uchicago.edu>
2024-05-20 13:40:55 -0500

    DF LAS memory management safety commit

--- my_pyscf/mcscf/las_ao2mo.py
+++ my_pyscf/mcscf/las_ao2mo.py
@@ -16,14 +16,26 @@ def get_h2eff_df (las, mo_coeff):
     mem_enough_int = mem_av > mem_int
     if mem_enough_int:
         mem_av -= mem_int
-    cderi = np.concatenate ([c for c in las.with_df.loop ()], axis=0)
-    bPmn = sparsedf_array (cderi)
-    bmuP = bPmn.contract1 (mo_cas)
-    buvP = np.tensordot (mo_cas.conjugate (), bmuP, axes=((0),(0)))
-    eri_muxy = np.tensordot (bmuP, buvP, axes=((2),(2)))
-    eri = np.tensordot (mo_coeff.conjugate (), eri_muxy, axes=((0),(0)))
-    eri = lib.pack_tril (eri.reshape (nmo*ncas, ncas, ncas)).reshape (nmo, -1)
-    eri = lib.tag_array (eri, bmPu=bmuP.transpose (0,2,1))
+        bmuP = []
+    mem_per_aux = nao*(nao+1)//2 # cderi / bPmn
+    mem_per_aux += nao*ncas # bmuP
+    mem_per_aux += ncas*ncas # buvP
+    mem_per_aux += nao*lib.num_threads () # wrk in contract1
+    mem_per_aux *= 8 // 1e6
+    mem_per_aux = max (1, mem_per_aux)
+    blksize = max (1, min (naux, int (mem_av // mem_per_aux)))
+    assert (blksize>1)
+    eri = 0
+    for cderi in las.with_df.loop (blksize=blksize):
+        bPmn = sparsedf_array (cderi)
+        bmuP1 = bPmn.contract1 (mo_cas)
+        if mem_enough_int: bmuP.append (bmuP1)
+        buvP = np.tensordot (mo_cas.conjugate (), bmuP1, axes=((0),(0)))
+        eri1 = np.tensordot (bmuP1, buvP, axes=((2),(2)))
+        eri1 = np.tensordot (mo_coeff.conjugate (), eri1, axes=((0),(0)))
+        eri += lib.pack_tril (eri1.reshape (nmo*ncas, ncas, ncas)).reshape (nmo, -1)
+    if mem_enough_int:
+        eri = lib.tag_array (eri, bmPu=np.concatenate (bmuP, axis=-1).transpose (0,2,1))
     if las.verbose > lib.logger.DEBUG:
         eri_comp = las.with_df.ao2mo (mo, compact=True)
         lib.logger.debug(las,"CDERI two-step error: {}".format(linalg.norm(eri-eri_comp)))
@@ -37,6 +49,8 @@ def get_h2eff (las, mo_coeff=None):
     mo_cas = mo_coeff[:,ncore:nocc]
     mo = [mo_coeff, mo_cas, mo_cas, mo_cas]
     mem_eris = 8*nmo*ncas*ncas*(ncas+1) // 2 // 1e6
+    mem_eris += 8*nao*ncas*ncas*ncas // 1e6 # intermediate
+    mem_eris += 8*lib.num_threads ()*nao*nmo // 1e6 # intermediate
     mem_remaining = las.max_memory - lib.current_memory ()[0]
     if mem_eris > mem_remaining:
         raise MemoryError ("{} MB of {}/{} MB av/total for ERI array".format (
