f36df2ef0b2fac118e55af79d8cc16902fc1cba9
Matthew R Hermes <mrhermes@uchicago.edu>
2024-05-20 19:20:20 -0500

    LASCI/LASSCF DF ERI memory management
    Choose blocksize of with_df.loop () to fill max_memory. There is a
    factor of 3 in the actual memory footprint that I don't quite
    understand...

--- my_pyscf/mcscf/las_ao2mo.py
+++ my_pyscf/mcscf/las_ao2mo.py
@@ -5,35 +5,54 @@ from mrh.my_pyscf.df.sparse_df import sparsedf_array
 
 def get_h2eff_df (las, mo_coeff):
     # Store intermediate with one contracted ao index for faster calculation of exchange!
+    log = lib.logger.new_logger (las, las.verbose)
     nao, nmo = mo_coeff.shape
     ncore, ncas = las.ncore, las.ncas
     nocc = ncore + ncas
     mo_cas = mo_coeff[:,ncore:nocc]
     naux = las.with_df.get_naoaux ()
-    mem_eris = 8*nmo*ncas*ncas*(ncas+1) // 2 // 1e6
+    log.debug2 ("LAS DF ERIs: %d MB used of %d MB total available", lib.current_memory ()[0], las.max_memory)
+    mem_eris = 8*(nao+nmo)*ncas*ncas*ncas / 1e6
+    mem_eris += 8*lib.num_threads ()*nao*nmo / 1e6 
     mem_av = las.max_memory - lib.current_memory ()[0] - mem_eris
-    mem_int = 8*naux*ncas*nao // 1e6
+    mem_int = 16*naux*ncas*nao / 1e6
     mem_enough_int = mem_av > mem_int
     if mem_enough_int:
         mem_av -= mem_int
         bmuP = []
-    mem_per_aux = nao*(nao+1)//2 # cderi / bPmn
-    mem_per_aux += nao*ncas # bmuP
+        log.debug ("LAS DF ERI including intermediate cache")
+    else:
+        log.debug ("LAS DF ERI not including intermediate cache")
+    safety_factor = 1.1
+    mem_per_aux = nao*ncas # bmuP
     mem_per_aux += ncas*ncas # buvP
     mem_per_aux += nao*lib.num_threads () # wrk in contract1
-    mem_per_aux *= 8 // 1e6
+    if not isinstance (getattr (las.with_df, '_cderi', None), np.ndarray):
+        mem_per_aux += 3*nao*(nao+1)//2 # cderi / bPmn
+        # NOTE: I think a linalg.norm operation in sparsedf_array might be doubling the memory
+        # footprint of bPmn below
+    else:
+        mem_per_aux += nao*(nao+1) # see note above
+    mem_per_aux *= safety_factor * 8 / 1e6
     mem_per_aux = max (1, mem_per_aux)
-    blksize = max (1, min (naux, int (mem_av // mem_per_aux)))
+    blksize = max (1, min (naux, int (mem_av / mem_per_aux)))
     assert (blksize>1)
+    log.debug2 ("LAS DF ERI blksize = %d, mem_av = %d MB, mem_per_aux = %d MB", blksize, mem_av, mem_per_aux)
+    log.debug2 ("LAS DF ERI naux = %d, nao = %d, nmo = %d", naux, nao, nmo)
     eri = 0
     for cderi in las.with_df.loop (blksize=blksize):
         bPmn = sparsedf_array (cderi)
+        log.debug2 ("LAS DF ERI bPmn shape = %s; shares memory? %s %s; C_CONTIGUOUS? %s",
+                  str (bPmn.shape), str (np.shares_memory (bPmn, cderi)),
+                  str (np.may_share_memory (bPmn, cderi)),
+                  str (bPmn.flags['C_CONTIGUOUS']))
         bmuP1 = bPmn.contract1 (mo_cas)
         if mem_enough_int: bmuP.append (bmuP1)
         buvP = np.tensordot (mo_cas.conjugate (), bmuP1, axes=((0),(0)))
         eri1 = np.tensordot (bmuP1, buvP, axes=((2),(2)))
         eri1 = np.tensordot (mo_coeff.conjugate (), eri1, axes=((0),(0)))
         eri += lib.pack_tril (eri1.reshape (nmo*ncas, ncas, ncas)).reshape (nmo, -1)
+        cderi = bPmn = bmuP1 = buvP = eri1 = None
     if mem_enough_int:
         eri = lib.tag_array (eri, bmPu=np.concatenate (bmuP, axis=-1).transpose (0,2,1))
     if las.verbose > lib.logger.DEBUG:
@@ -48,8 +67,7 @@ def get_h2eff (las, mo_coeff=None):
     nocc = ncore + ncas
     mo_cas = mo_coeff[:,ncore:nocc]
     mo = [mo_coeff, mo_cas, mo_cas, mo_cas]
-    mem_eris = 8*nmo*ncas*ncas*(ncas+1) // 2 // 1e6
-    mem_eris += 8*nao*ncas*ncas*ncas // 1e6 # intermediate
+    mem_eris = 8*(nao+nmo)*ncas*ncas*ncas // 1e6
     mem_eris += 8*lib.num_threads ()*nao*nmo // 1e6 # intermediate
     mem_remaining = las.max_memory - lib.current_memory ()[0]
     if mem_eris > mem_remaining:
