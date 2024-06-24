28d6c30be4f5aa4a97d924a7c92c7dea5f422cbf
Matthew R Hermes <mrhermes@uchicago.edu>
2024-06-24 13:11:13 -0500

    debug workflow

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,4 +4,6 @@ set -e
 
 export PYTHONPATH=${PWD%/*}:$PYTHONPATH
 cd ./tests
-pytest -k 'not _slow and not _dupe'
+#pytest -k 'not _slow and not _dupe'
+# debug
+pytest -k 'c2h4n4 and not _slow and not _dupe'
