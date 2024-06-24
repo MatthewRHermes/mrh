c9886aacfa5f3f7f39e79f1a58c090a74a9f4f73
Matthew R Hermes <mrhermes@uchicago.edu>
2024-06-24 13:23:05 -0500

    restore unittests

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,6 +4,4 @@ set -e
 
 export PYTHONPATH=${PWD%/*}:$PYTHONPATH
 cd ./tests
-#pytest -k 'not _slow and not _dupe'
-# debug
-pytest -k 'c2h4n4 and not _slow and not _dupe'
+pytest -k 'not _slow and not _dupe'
