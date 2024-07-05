58210131c3b113fa5009702c3f50ce61bb2b8db2
Qiaohong Wang <qjwang@midway3-login3.rcc.local>
2024-07-05 11:50:10 -0500

    kernel functionals is within extremelyaysn only

--- my_pyscf/mcscf/lasscf_rdm.py
+++ my_pyscf/mcscf/lasscf_rdm.py
@@ -160,6 +160,7 @@ def rdm_cycle (las, mo_coeff, casdm1frs, veff, h2eff_sub, log, max_cycle_rdmjk=3
 def kernel (las, mo_coeff=None, casdm1frs=None, casdm2fr=None, conv_tol_grad=1e-4, verbose=lib.logger.NOTE):
     if mo_coeff is None: mo_coeff = las.mo_coeff#this would be our initial guess orbital,save
     ################
+    '''
     if not os.path.exists("../orbital"):
         os.makedirs("../orbital")
         file_path = os.path.join("../orbital", 'guessOrb.h5')
@@ -171,6 +172,7 @@ def kernel (las, mo_coeff=None, casdm1frs=None, casdm2fr=None, conv_tol_grad=1e-
         with h5py.File(file_path, 'r') as f:
             mo_coeff = f['guessOrb'][:]
             log.info("read the previous orbital")
+    '''
     ###############
     conv_tol_rdmjkde = 1e-8
     conv_tol_rdmjkddm = las.conv_tol_rdmjkddm or 3*conv_tol_grad
@@ -634,9 +636,10 @@ class extremeAsynLASSCF (LASSCFNoSymm):
             ci1.append (fcivec)
             t1 = log.timer ('FCI box for subspace {}'.format (isub), *t1)
         return e_cas, ci1
-    def kernel (las, mo_coeff=None, casdm1frs=None, casdm2fr=None, conv_tol_grad=1e-4, verbose=lib.logger.NOTE):
-    if mo_coeff is None: mo_coeff = las.mo_coeff#this would be our initial guess orbital,save
-    ################ 
+
+    def kernel (self, mo_coeff=None, casdm1frs=None, casdm2fr=None, conv_tol_grad=1e-4, verbose=lib.logger.NOTE):
+    if mo_coeff is None: mo_coeff = self.mo_coeff#this would be our initial guess orbital,save
+    #-----edit: save orbitals-----# 
     if not os.path.exists("../orbital"):
         os.makedirs("../orbital")
         file_path = os.path.join("../orbital", 'guessOrb.h5')
@@ -648,53 +651,53 @@ class extremeAsynLASSCF (LASSCFNoSymm):
         with h5py.File(file_path, 'r') as f:
             mo_coeff = f['guessOrb'][:]
             log.info("read the previous orbital")
-    ###############  
+    #--------------------------------##  
     conv_tol_rdmjkde = 1e-8
-    conv_tol_rdmjkddm = las.conv_tol_rdmjkddm or 3*conv_tol_grad
-    log = lib.logger.new_logger(las, verbose)
+    conv_tol_rdmjkddm = self.conv_tol_rdmjkddm or 3*conv_tol_grad
+    log = lib.logger.new_logger(self, verbose)
     t0 = (lib.logger.process_clock(), lib.logger.perf_counter())
     log.debug('Start LASSCF')
                      
-    h2eff_sub = las.get_h2eff (mo_coeff)
+    h2eff_sub = self.get_h2eff (mo_coeff)
     t1 = log.timer('integral transformation to LAS space', *t0)
                      
-    if casdm1frs is None: casdm1frs, casdm2fr = get_init_guess_rdm (las, mo_coeff, h2eff_sub)
-    casdm1fs = las.make_casdm1s_sub (casdm1frs=casdm1frs)
-    dm1 = las.make_rdm1 (casdm1s_sub=casdm1fs)
-    veff = las.get_veff (dm1s=dm1)
-    veff = las.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs)
+    if casdm1frs is None: casdm1frs, casdm2fr = get_init_guess_rdm (self, mo_coeff, h2eff_sub)
+    casdm1fs = self.make_casdm1s_sub (casdm1frs=casdm1frs)
+    dm1 = self.make_rdm1 (casdm1s_sub=casdm1fs)
+    veff = sekf.get_veff (dm1s=dm1)
+    veff = self.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs)
     t1 = log.timer('LASSCF initial get_veff', *t1)
                      
     ugg = None       
     converged = False
     t2 = (t1[0], t1[1])
     it = 0           
-    for it in range (las.max_cycle_macro): ###las.rdm_cycle
-        e_cas, casdm1frs, casdm2fr, rdmjk_conv = las.rdm_cycle (mo_coeff, casdm1frs,
-            veff, h2eff_sub, log, max_cycle_rdmjk=las.max_cycle_rdmjk,
+    for it in range (self.max_cycle_macro): ###las.rdm_cycle
+        e_cas, casdm1frs, casdm2fr, rdmjk_conv = self.rdm_cycle (mo_coeff, casdm1frs,
+            veff, h2eff_sub, log, max_cycle_rdmjk=self.max_cycle_rdmjk,
             conv_tol_rdmjkddm=conv_tol_rdmjkddm,
             conv_tol_rdmjkde=conv_tol_rdmjkde)
-        if ugg is None: ugg = las.get_ugg (mo_coeff)
+        if ugg is None: ugg = self.get_ugg (mo_coeff)
         log.info ('LASSCF subspace CI energies: {}'.format (e_cas))
         t1 = log.timer ('LASSCF rdm_cycle', *t1)
                      
-        casdm1fs_new = las.make_casdm1s_sub (casdm1frs=casdm1frs)
+        casdm1fs_new = self.make_casdm1s_sub (casdm1frs=casdm1frs)
         veff = veff.sum (0)/2
-        if not isinstance (las, _DFLASCI) or las.verbose > lib.logger.DEBUG:
-            dm1 = las.make_rdm1 (mo_coeff=mo_coeff, casdm1s_sub=casdm1fs_new)
-            veff_new = las.get_veff (dm1s=dm1)
-            if not isinstance (las, _DFLASCI): veff = veff_new
-        if isinstance (las, _DFLASCI):
+        if not isinstance (self, _DFLASCI) or self.verbose > lib.logger.DEBUG:
+            dm1 = self.make_rdm1 (mo_coeff=mo_coeff, casdm1s_sub=casdm1fs_new)
+            veff_new = self.get_veff (dm1s=dm1)
+            if not isinstance (self, _DFLASCI): veff = veff_new
+        if isinstance (self, _DFLASCI):
             ddm = [dm_new - dm_old for dm_new, dm_old in zip (casdm1fs_new, casdm1fs)]
-            veff += las.fast_veffa (ddm, h2eff_sub, mo_coeff=mo_coeff)
-            if las.verbose > lib.logger.DEBUG:
+            veff += self.fast_veffa (ddm, h2eff_sub, mo_coeff=mo_coeff)
+            if self.verbose > lib.logger.DEBUG:
                 errmat = veff - veff_new
-                lib.logger.debug (las, 'fast_veffa error: {}'.format (linalg.norm (errmat)))
-        veff = las.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs_new)
+                lib.logger.debug (self, 'fast_veffa error: {}'.format (linalg.norm (errmat)))
+        veff = self.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs_new)
         casdm1fs = casdm1fs_new
                      
         t1 = log.timer ('LASSCF get_veff after ci', *t1)
-        H_op = las.get_hop (ugg=ugg, mo_coeff=mo_coeff, casdm1frs=casdm1frs,
+        H_op = self.get_hop (ugg=ugg, mo_coeff=mo_coeff, casdm1frs=casdm1frs,
             casdm2fr=casdm2fr, h2eff_sub=h2eff_sub, veff=veff, do_init_eri=False)
         g_vec = H_op.get_grad ()
         gx = H_op.get_gx ()
@@ -704,12 +707,12 @@ class extremeAsynLASSCF (LASSCFNoSymm):
         norm_gx = linalg.norm (gx) if gx.size else 0.0
         x0 = prec_op._matvec (-g_vec)
         norm_xorb = linalg.norm (x0) if x0.size else 0.0
-        lib.logger.info (las, 'LASSCF macro %d : E = %.15g ; |g_int| = %.15g ; |g_x| = %.15g',
+        lib.logger.info (self, 'LASSCF macro %d : E = %.15g ; |g_int| = %.15g ; |g_x| = %.15g',
             it, H_op.e_tot, norm_gorb, norm_gx)
         if ((norm_gorb < conv_tol_grad) or (norm_gorb < norm_gx/10)) and rdmjk_conv:
             converged = True
             break    
-        las.dump_chk (mo_coeff=mo_coeff, ci=[casdm1frs, casdm2fr])
+        self.dump_chk (mo_coeff=mo_coeff, ci=[casdm1frs, casdm2fr])
         H_op._init_eri_() # Take this part out of the true initialization b/c 
                           # if I'm already converged I don't want to waste the cycles
         t1 = log.timer ('LASSCF Hessian constructor', *t1)
@@ -723,7 +726,7 @@ class extremeAsynLASSCF (LASSCFNoSymm):
             id_max = ugg.addr2idstr (addr_max)
             x_max = x[addr_max]/np.pi
             log.debug ('Maximum step vector element x[{}] = {}*pi ({})'.format (addr_max, x_max, id_max))
-            if las.verbose > lib.logger.INFO:
+            if self.verbose > lib.logger.INFO:
                 Hx = H_op._matvec (x) # This doubles the price of each iteration!!
                 resid = g_vec + Hx
                 norm_gorb = linalg.norm (resid) if resid.size else 0.0
@@ -747,7 +750,7 @@ class extremeAsynLASSCF (LASSCFNoSymm):
         my_tol = max (conv_tol_grad, norm_gx/10)
         try:
             x, info_int = sparse.linalg.cg (H_op, -g_vec, x0=x0, atol=my_tol,
-                                            maxiter=las.max_cycle_micro,
+                                            maxiter=self.max_cycle_micro,
                                             callback=my_callback, M=prec_op)
             t1 = log.timer ('LASSCF {} microcycles'.format (microit[0]), *t1)
             mo_coeff, h2eff_sub = H_op.update_mo_eri (x, h2eff_sub)
@@ -760,8 +763,8 @@ class extremeAsynLASSCF (LASSCFNoSymm):
             ################################################     
             t1 = log.timer ('LASSCF Hessian update', *t1)
      
-            veff = las.get_veff (dm1s = las.make_rdm1 (mo_coeff=mo_coeff, casdm1s_sub=casdm1fs))
-            veff = las.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs)
+            veff = self.get_veff (dm1s = self.make_rdm1 (mo_coeff=mo_coeff, casdm1s_sub=casdm1fs))
+            veff = self.split_veff (veff, h2eff_sub, mo_coeff=mo_coeff, casdm1s_sub=casdm1fs)
             t1 = log.timer ('LASSCF get_veff after secondorder', *t1)
         except MicroIterInstabilityException as e:
             log.info ('Unstable microiteration aborted: %s', str (e))
@@ -770,10 +773,10 @@ class extremeAsynLASSCF (LASSCFNoSymm):
             for i in range (3): # Make up to 3 attempts to scale-down x if necessary
                 mo2, h2eff_sub2 = H_op.update_mo_eri (x, h2eff_sub)
                 t1 = log.timer ('LASCF Hessian update', *t1)
-                veff2 = las.get_veff (dm1s = las.make_rdm1 (mo_coeff=mo2, casdm1s_sub=casdm1fs))
-                veff2 = las.split_veff (veff2, h2eff_sub2, mo_coeff=mo2, casdm1s_sub=casdm1fs)
+                veff2 = self.get_veff (dm1s = self.make_rdm1 (mo_coeff=mo2, casdm1s_sub=casdm1fs))
+                veff2 = self.split_veff (veff2, h2eff_sub2, mo_coeff=mo2, casdm1s_sub=casdm1fs)
                 t1 = log.timer ('LASSCF get_veff after secondorder', *t1)
-                e2 = las.energy_nuc () + las.energy_elec (mo_coeff=mo2, h2eff=h2eff_sub2,
+                e2 = self.energy_nuc () + self.energy_elec (mo_coeff=mo2, h2eff=h2eff_sub2,
                                                           casdm1frs=casdm1frs,
                                                           casdm2fr=casdm2fr,
                                                           veff=veff2)
@@ -788,26 +791,26 @@ class extremeAsynLASSCF (LASSCFNoSymm):
      
     t2 = log.timer ('LASSCF {} macrocycles'.format (it), *t2)
      
-    e_tot = las.energy_nuc () + las.energy_elec (mo_coeff=mo_coeff,
+    e_tot = self.energy_nuc () + self.energy_elec (mo_coeff=mo_coeff,
         casdm1frs=casdm1frs, casdm2fr=casdm2fr, h2eff=h2eff_sub, veff=veff)
-    e_tot_test = las.get_hop (ugg=ugg, mo_coeff=mo_coeff, casdm1frs=casdm1frs,
+    e_tot_test = self.get_hop (ugg=ugg, mo_coeff=mo_coeff, casdm1frs=casdm1frs,
         casdm2fr=casdm2fr, h2eff_sub=h2eff_sub, veff=veff, do_init_eri=False).e_tot
-    veff_a = np.stack ([las.fast_veffa ([d[state] for d in casdm1frs], h2eff_sub, mo_coeff=mo_coeff, _full=True)
-        for state in range (las.nroots)], axis=0)
-    veff_c = (veff.sum (0) - np.einsum ('rsij,r->ij', veff_a, las.weights))/2 
+    veff_a = np.stack ([self.fast_veffa ([d[state] for d in casdm1frs], h2eff_sub, mo_coeff=mo_coeff, _full=True)
+        for state in range (self.nroots)], axis=0)
+    veff_c = (veff.sum (0) - np.einsum ('rsij,r->ij', veff_a, self.weights))/2 
     veff = veff_c[None,None,:,:] + veff_a
-    veff = lib.tag_array (veff, c=veff_c, sa=np.einsum ('rsij,r->sij', veff, las.weights))
-    e_states = las.energy_nuc () + np.array (las.states_energy_elec (
+    veff = lib.tag_array (veff, c=veff_c, sa=np.einsum ('rsij,r->sij', veff,self.weights))
+    e_states = self.energy_nuc () + np.array (self.states_energy_elec (
         mo_coeff=mo_coeff, h2eff=h2eff_sub, veff=veff, casdm1frs=casdm1frs,
         casdm2fr=casdm2fr))
-    assert (np.allclose (np.dot (las.weights, e_states), e_tot)), '{} {} {} {}'.format (
-        e_states, np.dot (las.weights, e_states), e_tot, e_tot_test)
+    assert (np.allclose (np.dot (self.weights, e_states), e_tot)), '{} {} {} {}'.format (
+        e_states, np.dot (self.weights, e_states), e_tot, e_tot_test)
      
-    lib.logger.info (las, 'LASSCF %s after %d cycles', ('not converged', 'converged')[converged], it+1)
-    lib.logger.info (las, 'LASSCF E = %.15g ; |g_int| = %.15g ; |g_ext| = %.15g', e_tot, norm_gorb, norm_gx)
+    lib.logger.info (self, 'LASSCF %s after %d cycles', ('not converged', 'converged')[converged], it+1)
+    lib.logger.info (self, 'LASSCF E = %.15g ; |g_int| = %.15g ; |g_ext| = %.15g', e_tot, norm_gorb, norm_gx)
     t1 = log.timer ('LASSCF wrap-up', *t1)
      
-    mo_coeff, mo_energy, mo_occ, casdm1frs, casdm2fr, h2eff_sub = las.canonicalize (
+    mo_coeff, mo_energy, mo_occ, casdm1frs, casdm2fr, h2eff_sub = self.canonicalize (
         mo_coeff, casdm1frs, casdm2fr, veff=veff.sa, h2eff_sub=h2eff_sub)
     t1 = log.timer ('LASSCF canonicalization', *t1)
      
