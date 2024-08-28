34db161fe0ebfb4f5e953c0f4a8d56cfcf4d6d9e
Matthew R Hermes <mrhermes@uchicago.edu>
2024-08-28 12:27:44 -0500

    debug debug_opt57_slow test

--- debug/lassi/debug_opt57_slow.py
+++ debug/lassi/debug_opt57_slow.py
@@ -214,11 +214,11 @@ class KnownValues(unittest.TestCase):
             ham = op[opt].ham (las, h1, h2, ci_fr, nelec)[0]
             hket_fr_pabq = op[opt].contract_ham_ci (las, h1, h2, ci_fr, nelec, ci_fr, nelec)
             for f, (ci_r, hket_r_pabq) in enumerate (zip (ci_fr, hket_fr_pabq)):
-                current_order = list (range (las.nfrags-1, -1, -1)) + [las.nfrags]
-                current_order.insert (0, current_order.pop (f))
+                current_order = list (range (las.nfrags)) + [las.nfrags]
+                current_order.insert (0, current_order.pop (las.nfrags-1-f))
                 for r, (ci, hket_pabq) in enumerate (zip (ci_r, hket_r_pabq)):
                     if ci.ndim < 3: ci = ci[None,:,:]
-                    proper_shape = np.append (lroots[:,r], ndim)
+                    proper_shape = np.append (lroots[::-1,r], ndim)
                     current_shape = proper_shape[current_order]
                     to_proper_order = list (np.argsort (current_order))
                     hket_pq = lib.einsum ('rab,pabq->rpq', ci.conj (), hket_pabq)
