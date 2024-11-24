c40b41d2ba1ef1aba3685f886fceec97300422ee
Matthew R Hermes <mrhermes@uchicago.edu>
2024-11-23 20:13:47 -0600

    docstring

--- my_pyscf/fci/direct_halfelectron.py
+++ my_pyscf/fci/direct_halfelectron.py
@@ -4,6 +4,31 @@ from pyscf.fci import direct_spin1
 from pyscf.fci.addons import _unpack_nelec
 
 def contract_1he (h1he, cre, spin, ci, norb, nelec, link_index=None):
+    '''Act a one-half-electron operator on a ci vector
+
+    Args:
+        h1he: ndarray of shape (norb,)
+            Hamiltonian vector elements
+        cre: logical
+            If True, create an electron; otherwise, destroy
+        spin: integer
+            Spin of electron created or destroyed
+        ci: ndarray of shape (ndeta, ndetb)
+            CI vector
+        norb: integer
+            Number of orbitals in ci
+        nelec: integer or tuple of length 2
+            Number of electrons in ci
+
+    Kwargs:
+        link_index: tuple of length 2 of "linkstr" type ndarray
+            linkstr arrays for the nelec+1/nelec (cre = True/False) electrons in norb+1 orbitals
+            Hilbert space. See pyscf.fci.gen_linkstr_index for the shape of "linkstr".
+
+    Returns:
+        hci: ndarray
+            Hamiltonian-vector product
+    '''
     neleca, nelecb = _unpack_nelec (nelec)
     nelec_bra = [neleca, nelecb]
     ket_occ = [0,0]
@@ -27,6 +52,30 @@ def contract_1he (h1he, cre, spin, ci, norb, nelec, link_index=None):
     return dummy.read_orbital (hci, norb, nelec_bra, occ_a=bra_occ[0], occ_b=bra_occ[1])
 
 def absorb_h1he (h1he, h3he, cre, spin, norb, nelec, fac=1):
+    '''Combine one-half-electron and three-half-electron elements into a packed form.
+
+    Args:
+        h1he: ndarray of shape (norb,)
+            One-half-electron Hamiltonian vector elements
+        h3he: ndarray of shape (norb,norb,norb)
+            Three-half-electron Hamiltonian vector elements
+        cre: logical
+            If True, create an electron; otherwise, destroy
+        spin: integer
+            Spin of electron created or destroyed
+        norb: integer
+            Number of orbitals in ci
+        nelec: integer or tuple of length 2
+            Number of electrons in ci
+
+    Kwargs:
+        fac: number
+            Factor of three-half-electron part
+
+    Returns:
+        h3heff: ndarray
+            Packed Hamiltonian vector elements
+    '''
     neleca, nelecb = _unpack_nelec (nelec)
     nelecd = [neleca, nelecb]
     if cre: nelecd[spin] += 1
@@ -43,6 +92,31 @@ def absorb_h1he (h1he, h3he, cre, spin, norb, nelec, fac=1):
     return direct_spin1.absorb_h1e (f1e, f2e, norb+1, nelecd, fac=fac)
 
 def contract_3he (h3heff, cre, spin, ci, norb, nelec, link_index=None):
+    '''Act a three-half-electron operator on a ci vector
+
+    Args:
+        h3eff: ndarray
+            Packed Hamiltonian vector elements
+        cre: logical
+            If True, create an electron; otherwise, destroy
+        spin: integer
+            Spin of electron created or destroyed
+        ci: ndarray of shape (ndeta, ndetb)
+            CI vector
+        norb: integer
+            Number of orbitals in ci
+        nelec: integer or tuple of length 2
+            Number of electrons in ci
+
+    Kwargs:
+        link_index: tuple of length 2 of "linkstr" type ndarray
+            linkstr arrays for the nelec+1/nelec (cre = True/False) electrons in norb+1 orbitals
+            Hilbert space. See pyscf.fci.gen_linkstr_index for the shape of "linkstr".
+
+    Returns:
+        hci: ndarray
+            Hamiltonian-vector product
+    '''
     neleca, nelecb = _unpack_nelec (nelec)
     nelec_bra = [neleca, nelecb]
     ket_occ = [0,0]
