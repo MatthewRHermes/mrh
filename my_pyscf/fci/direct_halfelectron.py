7c90aaea20d4cc366cf0bbc9b3d65d061dd4f92d
Matthew R Hermes <mrhermes@uchicago.edu>
2024-11-23 18:13:16 -0600

    debug direct_halfelectron

--- my_pyscf/fci/direct_halfelectron.py
+++ my_pyscf/fci/direct_halfelectron.py
@@ -40,14 +40,17 @@ def absorb_h1he (h1he, h3he, cre, spin, norb, nelec, fac=1):
 
 def contract_3he (h3heff, cre, spin, ci, norb, nelec, link_index=None):
     neleca, nelecb = _unpack_nelec (nelec)
+    nelec_bra = [neleca, nelecb]
     ket_occ = [0,0]
     bra_occ = [0,0]
     if cre: 
         ket_occ[spin] = 1
+        nelec_bra[spin] += 1
     else:
         bra_occ[spin] = 1
+        nelec_bra[spin] -= 1
     nelecd = [neleca + ket_occ[0], nelecb + ket_occ[1]]
     ci = dummy.add_orbital (ci, norb, nelec, occ_a=ket_occ[0], occ_b=ket_occ[1])
     hci = direct_spin1.contract_2e (h3heff, ci, norb+1, nelecd, link_index=link_index)
-    return dummy.read_orbital (hci, norb, nelec, occ_a=bra_occ[0], occ_b=bra_occ[1])
+    return dummy.read_orbital (hci, norb, nelec_bra, occ_a=bra_occ[0], occ_b=bra_occ[1])
 
--- tests/fci/test_halfelectron.py
+++ tests/fci/test_halfelectron.py
@@ -42,6 +42,39 @@ def case_contract_1he (self, norb, nelec, cre, spin):
         h1he, cre, spin, ci, norb, nelec
     )
     self.assertAlmostEqual (lib.fp (hci_test), lib.fp (hci_ref), 8)
+
+def case_contract_3he (self, norb, nelec, cre, spin, _incl_1he=True):
+    if (not cre) and (not nelec[spin]): return
+    ci = rng.random ((cistring.num_strings (norb,nelec[0]),
+                         cistring.num_strings (norb,nelec[1])), dtype=float)
+    ci /= linalg.norm (ci)
+    ops = [[des_a, des_b], [cre_a, cre_b]]
+    h1he = rng.random ((norb), dtype=float)
+    h3he = rng.random ((norb,norb,norb), dtype=float)
+    if cre:
+        h3he = (h3he + h3he.transpose (0,2,1)) / 2
+    else:
+        h3he = (h3he + h3he.transpose (1,0,2)) / 2
+    op = ops[int(cre)][spin]
+    if not _incl_1he: h1he[:] = 0
+    hci_ref = np.tensordot (h1he, np.stack ([op (ci, norb, nelec, i)
+                                             for i in range (norb)],
+                                            axis=0),
+                            axes=1)
+    nelecl = [nelec[0], nelec[1]]
+    nelecl[spin] -= 1
+    for i in range (norb):
+        if cre:
+            hci_ref += op (direct_spin1.contract_1e (h3he[i], ci, norb, nelec),
+                           norb, nelec, i)
+        else:
+            hci_ref += direct_spin1.contract_1e (h3he[:,:,i], op (ci, norb, nelec, i),
+                                                 norb, nelecl)
+    h3heff = direct_halfelectron.absorb_h1he (h1he, h3he, cre, spin, norb, nelec, fac=.5)
+    hci_test = direct_halfelectron.contract_3he (
+        h3heff, cre, spin, ci, norb, nelec
+    )
+    self.assertAlmostEqual (lib.fp (hci_test), lib.fp (hci_ref), 8)
  
 class KnownValues(unittest.TestCase):
 
@@ -51,6 +84,21 @@ class KnownValues(unittest.TestCase):
                 for spin in range (2):
                     with self.subTest (cre=cre, spin=spin, norb=norb, nelec=nelec):
                         case_contract_1he (self, norb, nelec, cre, spin)
+
+    def test_contract_3he (self):
+        for norb, nelec in cases:
+            for cre in (True, False):
+                for spin in range (2):
+                    with self.subTest (cre=cre, spin=spin, norb=norb, nelec=nelec):
+                        case_contract_3he (self, norb, nelec, cre, spin, _incl_1he=False)
+
+    def test_contract_13he (self):
+        for norb, nelec in cases:
+            for cre in (True, False):
+                for spin in range (2):
+                    with self.subTest (cre=cre, spin=spin, norb=norb, nelec=nelec):
+                        case_contract_3he (self, norb, nelec, cre, spin)
+
 
 if __name__ == "__main__":
     print("Full Tests for direct_halfelectron")
