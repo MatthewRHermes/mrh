3dbbde317199d6e276bb2349c590f5a42af1ae8e
Shreya Verma <vermashreya03@gmail.com>
2024-12-04 11:34:54 -0600

    restored the lasuccsd.py and lasci_ominus1.py to the mrh master branch form

--- exploratory/unitary_cc/lasuccsd.py
+++ exploratory/unitary_cc/lasuccsd.py
@@ -1,12 +1,11 @@
 import numpy as np
-from mrh.exploratory.unitary_cc import uccsd_sym1, usccsd_sym1
+from mrh.exploratory.unitary_cc import uccsd_sym1
 from mrh.exploratory.citools import lasci_ominus1
 from itertools import combinations, combinations_with_replacement
 
 
 def gen_uccsd_op (norb, nlas, t1_s2sym=True):
     ''' Build the fragment-interaction singles and doubles UCC operator!! '''
-    print ("SV norb, nlas in gen = ", norb, nlas)
     t1_idx = np.zeros ((norb, norb), dtype=np.bool_)
     nfrag = len (nlas)
     for ifrag, afrag in combinations (range (nfrag), 2):
@@ -24,17 +23,10 @@ def gen_uccsd_op (norb, nlas, t1_s2sym=True):
         nfint = len (np.unique (frag_idx[abij]))
         if nfint > 1:
             a_idxs.append (ab)
-            #print (" SV a_idxs =", a_idxs)
             i_idxs.append (ij)
     uop = uccsd_sym1.FSUCCOperator (norb, a_idxs, i_idxs, s2sym=t1_s2sym)
-    #print ("SV a_idxs = ", a_idxs, len(a_idxs), i_idxs)
     return uop
-
-#SV
-def gen_usccsd_op(norb, nlas, a_idxs, i_idxs,t1_s2sym=False):  
-    uop = usccsd_sym1.FSUCCOperator (norb, a_idxs, i_idxs, s2sym=False)
-    return uop
-
+        
 class FCISolver (lasci_ominus1.FCISolver):
     def get_uop (self, norb, nlas, t1_s2sym=None):
         frozen = str (getattr (self, 'frozen', None))
@@ -44,17 +36,5 @@ class FCISolver (lasci_ominus1.FCISolver):
             return uccsd_sym1.get_uccsd_op (norb, s2sym=t1_s2sym)
         return gen_uccsd_op (norb, nlas, t1_s2sym=t1_s2sym) 
         
-#SV
 
-class FCISolver2(lasci_ominus1.FCISolver):
-    def __init__(self, mol, a_idxs, i_idxs):
-        super().__init__(mol)
-        self.a_idxs = a_idxs
-        self.i_idxs = i_idxs
 
-    def get_uop(self, norb, nlas, t1_s2sym=None):
-        if getattr(self, 'frozen', '').upper() == 'CI':
-            return uccsd_sym1.get_uccsd_op2(norb, epsilon=0.0)
-        else:
-            return gen_usccsd_op(norb, nlas, self.a_idxs, self.i_idxs)
-            #Just pass in norb, nlas, a_idxs_lst, i_idxs_lst
