lassis hessian fix most orb sectors
orb - ci_ref still broken. ci_ref everywhere is wrong, as is si_ref
diagonal

my_pyscf/lassi/lassis/hessian_orb_ci_si.py
@@ -20,7 +20,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
         self.nroots = len (ugg.ci[0])
         self.lsi = lsi = ugg.lsi
         self.opt = lsi.opt
-        self.si = ugg.si
+        self.si = ugg.raw2orth.H (ugg.si)
         self.nprods = ugg.nprods
         self.nroots_si = ugg.nroots_si
         self.shape = (ugg.nvar_tot,ugg.nvar_tot)
@@ -32,6 +32,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
         self.h2_paaa = []
         for i in range (self.nmo):
             self.h2_paaa.append (self.eris.ppaa[i][ncore:nocc])
+        
         self.casdm1, self.casdm2 = lsi.make_casdm12 (
             ci=self.ci, si=self.si, opt=self.opt
         )
@@ -144,6 +145,9 @@ class HessianOperator (sparse_linalg.LinearOperator):
         rorb = self.hoo (xham_2q, kappa)
         rorb += self.hoa (ci1, si0, si1)
 
+        # TODO: why divide by 2?
+        xham_2q = [h/2 for h in xham_2q]
+
         rci_01 = self.hci_op (xham_2q, ci1, si0, si0)
         rci_001 = self.hci_op (ham_2q, ci1, si0, si1)
         for i in range (self.nfrags):
@@ -160,9 +164,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
     def hoo (self, xham_2q, kappa):
         h0, h1, h2 = xham_2q
         fx = self.get_fock1 (h1, h2, self.casdm1, self.casdm2)
-        fx += np.dot (kappa, self.fock1) / 2
-        fx -= np.dot (self.fock1, kappa) / 2
-        return fx - fx.T
+        return (fx - fx.T) / 2
 
     def hoa (self, ci1, si0, si1):
         casdm1, casdm2 = self.lsi.trans_casdm12 (ci=ci1, si_bra=si0, si_ket=si1, opt=self.opt)

tests/lassi/addons.py
@@ -244,11 +244,17 @@ def case_lassis_hessian (ks, lsis):
         si = (lsis.si[:,0] + lsis.si[:,i]) * np.sqrt (0.5)
     else:
         si = si[:,0]
-    g0 = grad_orb_ci_si.get_grad (lsis, si=si, pack=True)
+    #si[:] = 0
+    #si[0] = 1
+    ci_ref = lsis.get_ci_ref ()
+    #for ci_i in ci_ref:
+    #    ci_i[:,:] = 0
+    #    ci_i[0,0] = 1
+    g0 = grad_orb_ci_si.get_grad (lsis, ci_ref=ci_ref, si=si, pack=True)
     ugg = coords.UnitaryGroupGenerators (
         lsis,
         lsis.mo_coeff,
-        lsis.get_ci_ref (),
+        ci_ref, #lsis.get_ci_ref (),
         lsis.ci_spin_flips,
         lsis.ci_charge_hops,
         si
@@ -285,13 +291,16 @@ def case_lassis_hessian (ks, lsis):
                 err_table[z] += '{:e} {:e} {:e} {:e} {:.1f}\n'.format (
                     1/div, linalg.norm (g2_ref), linalg.norm (g2_test), err[0], err[1]
                 )
-                rel_err[z] = (err[0] / err_last[z])
-                err_last[z] = err[0] + np.finfo (float).tiny
+                err = err[0]
+                rel_err[z] = (err / err_last[z])
+                if linalg.norm (g2_test) < 1e-16:
+                    err = linalg.norm (g2_ref)
+                    rel_err[z] = (err / err_last[z]) * 2
+                err_last[z] = err + np.finfo (float).tiny
             div *= 2
         for rel_err_i, err_table_i, lbl1 in zip (rel_err, err_table, sec_lbls):
             with ks.subTest ((lbl1,lbl0)):
                 ks.assertAlmostEqual (rel_err_i, .5, 1, msg=err_table_i)
-
 
 
 