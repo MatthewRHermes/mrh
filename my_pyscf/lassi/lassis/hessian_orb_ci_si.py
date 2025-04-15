A couple of lassis hessian fixes
c2h4n4 test failing semi-successfully. There are weird overlaps
that suggest the coords module isn't using the right spin
multiplicity for the ch states. Also, raw2orth seems to change
randomly between successive builds, which MIGHT be just a real
gauge invariance or it might be related to the other thing.

my_pyscf/lassi/lassis/hessian_orb_ci_si.py
@@ -120,9 +120,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
         nelec_frs = self.get_nelec_frs (nr=len(ci[0]))
         ncore, ncas = self.lsi.ncore, self.lsi.ncas
         nocc = ncore+ncas
-        h0, h1_pp, h2_paaa = ham_2q
-        h1 = h1_pp[ncore:nocc,ncore:nocc]
-        h2 = h2_paaa[ncore:nocc]
+        h0, h1, h2 = ham_2q
         return op[self.opt].contract_ham_ci (
             self.lsi, h1, h2, ci, nelec_frs, ci, nelec_frs,
             si_bra=si_bra, si_ket=si_ket, h0=h0
@@ -132,9 +130,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
         nelec_frs = self.get_nelec_frs (nr=len(ci[0]))
         ncore, ncas = self.lsi.ncore, self.lsi.ncas
         nocc = ncore+ncas
-        h0, h1_pp, h2_paaa = ham_2q
-        h1 = h1_pp[ncore:nocc,ncore:nocc]
-        h2 = h2_paaa[ncore:nocc]
+        h0, h1, h2 = ham_2q
         ham_op, _, ovlp_op = op[self.opt].gen_contract_op_si_hdiag (
             self.lsi, h1, h2, ci, nelec_frs
         )[:3]
@@ -154,6 +150,10 @@ class HessianOperator (sparse_linalg.LinearOperator):
 
         # TODO: why divide by 2?
         xham_2q = [h/2 for h in xham_2q]
+        ncore, ncas = self.lsi.ncore, self.lsi.ncas
+        nocc = ncore + ncas
+        xham_2q[1] = xham_2q[1][ncore:nocc,ncore:nocc]
+        xham_2q[2] = xham_2q[2][ncore:nocc]
 
         rci_01 = self.hci_op (xham_2q, ci1, si0, si0)
         rci_001 = self.hci_op (ham_2q, ci1, si0, si1)
@@ -214,7 +214,7 @@ def xham_2q (lsi, kappa, mo_coeff=None, eris=None, veff_c=None):
         h0 = 2*np.sum (h1_1.diagonal ()[:ncore])
         h1 += h1_1
 
-    h2 = np.stack ([eris.ppaa[i] for i in range (nmo)], axis=0)
+    h2 = np.stack ([eris.ppaa[i][ncore:nocc] for i in range (nmo)], axis=0)
     if ncas:
         h2 = -lib.einsum ('pq,qabc->pabc',kappa,h2)
         kpa = kappa[:,ncore:nocc]

tests/lassi/addons.py
@@ -260,7 +260,7 @@ def case_lassis_hessian (ks, lsis):
         si
     )
     g0_debug = grad_orb_ci_si.get_grad (lsis, *ugg.update_wfn (np.zeros_like (g0)), pack=True)
-    ks.assertLess (np.amax (np.abs (g0_debug-g0)), 1e-10, msg='sanity fail')
+    #ks.assertLess (np.amax (np.abs (g0_debug-g0)), 1e-10, msg='sanity fail')
     h_op = hessian_orb_ci_si.HessianOperator (ugg)
     x0 = np.random.rand (ugg.nvar_tot)
     x0 = ugg.pack (*ugg.unpack (x0)) # apply some projections
@@ -269,6 +269,8 @@ def case_lassis_hessian (ks, lsis):
     sec_offs = ugg.get_sector_offsets ()
     for (i, j), lbl0 in zip (sec_offs, sec_lbls):
         if i==j: continue
+        with ks.subTest ("sanity", sector=lbl0):
+            ks.assertLess (np.amax (np.abs (g0_debug[i:j]-g0[i:j])), 1e-10)
         x1 = np.zeros_like (x0)
         x1[i:j] = x0[i:j]
         div = 1.0