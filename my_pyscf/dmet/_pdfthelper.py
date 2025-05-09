df4c12bc387957845f940f19d4d9f6f33048b1db
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-05-09 11:25:03 -0500

    restructuring the func

--- my_pyscf/dmet/_pdfthelper.py
+++ my_pyscf/dmet/_pdfthelper.py
@@ -16,15 +16,21 @@ Point to note: DMET-PDFT hasn't been tested for
 
 get_basis_transform = BasisTransform._get_basis_transformed
 
-def assemble_mo(mf, trans_coeff, mc_mo_coeff):
+def assemble_mo(mf, ao2eo, ao2co, mc_mo_coeff):
     '''
-    Assemble the mo_coeff
+    Assemble the mo_coeff to run the PDFT with the dmet_mf object.
     args:
         mf: RHF/ROHF object
             mean-field object for the full system
-        trans_coeff: dict
-            transformation coefficients
-        mc_mo_coeff: np.ndarray
+        ao2eo: np.array (nao, neo)
+            transformation matrix from the full system to the embedded system. Note that
+            nao: number of orbitals in the full system
+            neo: number of orbitals in the embedded system
+            ncore: number of core orbitals from the environment. (Don't get confuse with the ncore of mcscf)
+            nao = neo + ncore
+        ao2co: np.array (nao, ncore)
+            transformation matrix from the full system to the core space
+        mc_mo_coeff: np.array (neo, neo)
             mo_coeff for the embedded CASSCF calculation
     returns:
         mo_coeff: np.ndarray
@@ -33,30 +39,30 @@ def assemble_mo(mf, trans_coeff, mc_mo_coeff):
 
     dm = mf.make_rdm1()
     s = mf.get_ovlp()
-    ao2co = trans_coeff['ao2co']
-    ao2eo = trans_coeff['ao2eo']
-
+    
     cor2ao = ao2co.T @ s
 
     if dm.ndim > 2:
-        core_dm = [get_basis_transform(dm[0], cor2ao.T),get_basis_transform(dm[1], cor2ao.T)] 
-        core_dm = core_dm[0] + core_dm[1]
-    else:
-        core_dm = get_basis_transform(dm, cor2ao.T)
+        dm = dm[0] + dm[1]
     
+    # Generate the core density matrix and using that transform the ao2co
+    # to the canonical basis.
+    core_dm = get_basis_transform(dm, cor2ao.T)
     e, eigvec = np.linalg.eigh(core_dm)
     sorted_indices = np.argsort(e)[::-1]
     eigvec_sorted = eigvec[:, sorted_indices]  
     ao2co = ao2co @ eigvec_sorted
-
+    core_nelec = int(round(np.sum(e[sorted_indices] > 1e-10)))
+    assert core_nelec % 2 == 0, "Core nelec should be even., Something went wrong."
     ao2eo = ao2eo @ mc_mo_coeff
-    core_nelec = int(round(trans_coeff['core_nelec']))
-    ncore = core_nelec//2
-    emb_size = ao2eo.shape[1]
 
+    ncore = core_nelec//2
+    neo = ao2eo.shape[1]
+
+    # Now we can assemble the full space mo_coeffs.
     mo_coeff = np.empty_like(mf.mo_coeff)
     mo_coeff[:, :ncore] = ao2co[:, :ncore]
-    mo_coeff[:, ncore:ncore+emb_size] = ao2eo
-    mo_coeff[:, ncore+emb_size:] = ao2co[:, ncore:]
+    mo_coeff[:, ncore:ncore+neo] = ao2eo
+    mo_coeff[:, ncore+neo:] = ao2co[:, ncore:]
     return mo_coeff
 
