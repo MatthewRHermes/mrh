769f522e5db52436e91bd62ad57e63b1bb544079
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-05-08 23:02:57 -0500

    Examples updated

--- examples/dmetandpdmet/02_dmet_pdft.py
+++ examples/dmetandpdmet/02_dmet_pdft.py
@@ -1,8 +1,8 @@
 import numpy as np
-from pyscf import gto, scf, mcscf, mcpdft, lib
+from pyscf import gto, scf, mcscf, mcpdft
 from mrh.my_pyscf.fci import csf_solver
 from mrh.my_pyscf.dmet import runDMET, getorbindex
-from mrh.my_pyscf.dmet._pdfthelper import get_mc_for_dmet_pdft
+from mrh.my_pyscf.dmet._pdfthelper import assemble_mo
 
 '''
 Note: In this example, I am only showing CAS-PDFT and MCPDFT based on the SA-CASSCF calculations. Basically
@@ -16,13 +16,14 @@ mol.atom='''
 P  -5.64983   3.02383   0.00000
 H  -4.46871   3.02383   0.00000
 H  -6.24038   2.19489   0.59928
-Ne 0 0 10
+# Ne 0 0 10
 '''
 mol.build()
 
 mf = scf.ROHF(mol).density_fit()
 mf.kernel()
 
+# Running DMET
 dmet_mf, trans_coeff = runDMET(mf, lo_method='lowdin', bath_tol=1e-10, atmlst=[0, ])
 
 # Sanity Check
@@ -33,16 +34,19 @@ mo_coeff = trans_coeff['ao2eo'] @ dmet_mf.mo_coeff
 orblst = getorbindex(mol, mo_coeff, lo_method='meta-lowdin',
                     ao_label=['P 3s', 'P 3p', 'H 1s'], activespacesize=6, s=mf.get_ovlp())
 
-# MCPDFT based on SS-CAS Calculation
+# Running CASSCF
 mc = mcscf.CASSCF(dmet_mf, 6, 7)
+mc.max_cycle = 100
 mo = mc.sort_mo(orblst)
 mc.fcisolver  = csf_solver(mol, smult=2)
 mc.kernel(mo)
-newmc = get_mc_for_dmet_pdft(mc, trans_coeff, mf)
 
-# MC-PDFT
-mypdft = mcpdft.CASCI(newmc, 'tPBE', mc.ncas, mc.nelecas)
-mypdft.compute_pdft_energy_(mo_coeff=newmc.mo_coeff, ci=mc.ci, dump_chk=False)
+# Assembling the full space mo_coeffs
+mo_coeff = assemble_mo(mf, trans_coeff, mc.mo_coeff)
+
+# Running CAS-PDFT
+mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
+mypdft.kernel(mo_coeff=mo_coeff, ci0=mc.ci)
 
 # MCPDFT based on SA-CAS Calculation
 mc = mcscf.CASSCF(dmet_mf, 6, 7)
@@ -51,9 +55,9 @@ mc.fcisolver  = csf_solver(mol, smult=2)
 mc = mcscf.state_average_(mc, weights=[0.5, 0.5])
 mc.kernel(mo)
 
-# MC-PDFT for the more-than one state
-newmc = get_mc_for_dmet_pdft(mc, trans_coeff, mf)
+# Assembling the full space mo_coeffs
+mo_coeff = assemble_mo(mf, trans_coeff, mc.mo_coeff)
 
 for i in range(len(mc.ci)):
-    mypdft = mcpdft.CASCI(newmc, 'tPBE', mc.ncas, mc.nelecas)
-    mypdft.compute_pdft_energy_(mo_coeff=newmc.mo_coeff, ci=mc.ci[i], dump_chk=False)
+    mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
+    mypdft.kernel(mo_coeff=mo_coeff, ci0=mc.ci[i])
