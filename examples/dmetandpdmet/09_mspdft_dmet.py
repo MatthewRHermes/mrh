2599e9b79ba2727aa20305d2838216221c318a4d
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-05-08 23:24:35 -0500

    LPDFT enabled within the DMET framework

---
+++ examples/dmetandpdmet/09_mspdft_dmet.py
@@ -0,0 +1,52 @@
+import numpy as np
+from pyscf import gto, scf, mcscf, mcpdft
+from mrh.my_pyscf.fci import csf_solver
+from mrh.my_pyscf.dmet import runDMET, getorbindex
+from mrh.my_pyscf.dmet._pdfthelper import assemble_mo
+
+'''
+Note: In this example, I am only showing CAS-PDFT and MCPDFT based on the SA-CASSCF calculations. Basically
+starting from the embedded CAS object, the 1RDMs and 2RDMs are projected to the entire space.
+'''
+
+np.set_printoptions(precision=4)
+
+mol = gto.Mole(basis='6-31G', spin=1, charge=0, verbose=4, max_memory=10000)
+mol.atom='''
+P  -5.64983   3.02383   0.00000
+H  -4.46871   3.02383   0.00000
+H  -6.24038   2.19489   0.59928
+# Ne 0 0 10
+'''
+mol.build()
+
+mf = scf.ROHF(mol).density_fit()
+mf.kernel()
+
+# Running DMET
+dmet_mf, trans_coeff = runDMET(mf, lo_method='lowdin', bath_tol=1e-10, atmlst=[0, ])
+
+# Sanity Check
+assert abs((mf.e_tot - dmet_mf.e_tot)) < 1e-7, "Something went wrong."
+
+# Active space guess
+mo_coeff = trans_coeff['ao2eo'] @ dmet_mf.mo_coeff
+orblst = getorbindex(mol, mo_coeff, lo_method='meta-lowdin',
+                    ao_label=['P 3s', 'P 3p', 'H 1s'], activespacesize=6, s=mf.get_ovlp())
+
+# LPDFT based on SA-CAS Calculation
+weights=[0.5, 0.5]
+mc = mcscf.CASSCF(dmet_mf, 6, 7)
+mo = mc.sort_mo(orblst)
+mc.fcisolver  = csf_solver(mol, smult=2)
+mc = mcscf.state_average_(mc, weights)
+mc.kernel(mo)
+
+# Assembling the full space mo_coeffs
+mo_coeff = assemble_mo(mf, trans_coeff, mc.mo_coeff)
+mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
+mypdft.fcisolver = csf_solver(mol, smult=2)
+mypdft.fcisolver.nroots = 2
+mypdft.ci = mc.ci
+mypdft = mypdft.multi_state(weights, "lin")
+mypdft.kernel(mo_coeff=mo_coeff, dump_chk = False)
