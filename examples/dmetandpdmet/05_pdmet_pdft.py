cf448202dac13569b1a81b706bb1b8a5fd449d34
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-05-08 23:10:18 -0500

    Examples updated

--- examples/dmetandpdmet/05_pdmet_pdft.py
+++ examples/dmetandpdmet/05_pdmet_pdft.py
@@ -4,7 +4,7 @@ from pyscf.pbc import gto, scf, df
 from mrh.my_pyscf.pdmet import runpDMET
 from pyscf.csf_fci import csf_solver
 from mrh.my_pyscf import mcpdft
-from mrh.my_pyscf.pdmet._pdfthelper import get_mc_for_pdmet_pdft
+from mrh.my_pyscf.dmet._pdfthelper import assemble_mo
 
 np.set_printoptions(precision=4)
 
@@ -33,14 +33,16 @@ dmet_mf, trans_coeff = runpDMET(mf, lo_method='meta-lowdin', bath_tol=1e-10, atm
 assert abs((mf.e_tot - dmet_mf.e_tot)) < 1e-7, "Something went wrong."
 
 # MC-PDFT: PBC-PDFT is in mrh only.
-# CASSCF Calculation
+# Running CAS-PDFT
 mc = mcscf.CASSCF(dmet_mf,8,10)
 mc.fcisolver  = csf_solver(cell, smult=1)
 mc.kernel()
 
-newmc = get_mc_for_pdmet_pdft(mc, trans_coeff, mf)
-mypdft = mcpdft.CASCI(newmc, 'tPBE', mc.ncas, mc.nelecas)
-mypdft.compute_pdft_energy_(mo_coeff=newmc.mo_coeff, ci=mc.ci, dump_chk=False)
+# Assembling the full space mo_coeffs
+mo_coeff = assemble_mo(mf, trans_coeff, mc.mo_coeff)
+
+mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
+mypdft.kernel(mo_coeff=mo_coeff, ci0=mc.ci)
 
 # SA-CASSCF Calculation
 mc = mcscf.CASSCF(dmet_mf,8,10) 
@@ -48,8 +50,9 @@ mc.fcisolver  = csf_solver(cell, smult=1)
 mc = mcscf.state_average_(mc, weights=[0.5, 0.5])
 mc.kernel()
 
-newmc = get_mc_for_pdmet_pdft(mc, trans_coeff, mf)
-for i in range(len(mc.ci)):
-    mypdft = mcpdft.CASCI(newmc, 'tPBE', mc.ncas, mc.nelecas)
-    mypdft.compute_pdft_energy_(mo_coeff=newmc.mo_coeff, ci=mc.ci[i], dump_chk=False)
+# Assembling the full space mo_coeffs
+mo_coeff = assemble_mo(mf, trans_coeff, mc.mo_coeff)
 
+for i in range(len(mc.ci)):
+    mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
+    mypdft.kernel(mo_coeff=mo_coeff, ci0=mc.ci[i])
