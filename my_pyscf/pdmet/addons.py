a1b8af5cc9a5b4d5eac7a6d9180470ebd6dbed23
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-05-17 22:53:36 -0500

    pdmet-pdft unit test added

--- my_pyscf/pdmet/addons.py
+++ my_pyscf/pdmet/addons.py
@@ -1,3 +1,5 @@
+import numpy as np
+from functools import reduce
 from pyscf import lo, lib
 from pyscf.pbc import gto
 from pyscf.tools import mo_mapping
@@ -8,7 +10,7 @@ def mo_comps(aolabels_or_baslst, cell, mo_coeff, cart=False,
     '''For PBC: '''+mo_mapping.mo_comps.__doc__
     with lib.temporary_env(cell, cart=cart):
         assert (mo_coeff.shape[0] == cell.nao)
-        s = cell.pbc_intor_symmetric('int1e_ovlp')
+        s = cell.pbc_intor('int1e_ovlp')
         lao = lo.orth.orth_ao(cell, orth_method, s=s)
         idx = gto.mole._aolabels2baslst(cell, aolabels_or_baslst)
         if len(idx) == 0:
--- tests/dmetandpdmet/test_pdmet_pdft.py
+++ tests/dmetandpdmet/test_pdmet_pdft.py
@@ -1,59 +1,121 @@
 import unittest
 import numpy as np
-from pyscf import gto, scf, mcscf
+from pyscf import mcscf
 from pyscf.pbc import gto, scf
 from pyscf.csf_fci import csf_solver
-from mrh.my_pyscf.pdmet import runpDMET
 from mrh.my_pyscf import mcpdft
+from mrh.my_pyscf.pdmet import runpDMET, addons
 
 '''
-***** pDMET-PDFT Embedding *****
-More tests has to be added.
+***** PDMET-PDFT Embedding *****
+1. Embedded PDFT = Full-System PDFT (For CASCI)
+2. For Single Configuration=(1,1) Active Space Embedded PDFT =  CAS-PDFT  
 '''
 
-# # Integral generation
-# gdf = df.GDF(cell)
-# gdf._cderi_to_save = 'N2.h5'
-# gdf.build()
-
-# SCF: Note: use the density fitting object to build the SCF object
-
-def get_cell():
-    cell = gto.Cell(basis = 'gth-SZV',pseudo = 'gth-pade', a = np.eye(3) * 12, max_memory = 5000, verbose=0)
-    cell.atom = '''
-    N 0 0 0
-    N 0 0 1.1
-    '''
+def get_cell(basis='gth-SZV', pseudo = 'gth-pade'):
+    cell = gto.Cell(
+        atom='F 0 0 2; Ar 0 0 8;',
+        basis=basis, pseudo=pseudo, a=np.eye(3) * 12,
+        max_memory=5000, spin=1, verbose=0, output='/dev/null',
+        )
     cell.build()
     return cell
 
-
 class KnownValues(unittest.TestCase):
 
-    def test_pdft_closeshell(self):
+    def test_casci_pdft(self):
         cell = get_cell()
-        mf = scf.RHF(cell,exxdiv = None).density_fit()
-        mf.verbose=0
-        mf.run()
-        dmet_mf, mypdmet = runpDMET(mf, lo_method='meta-lowdin', bath_tol=1e-10, atmlst=[0, 1])
-        assert abs((mf.e_tot - dmet_mf.e_tot)) < 1e-7, "Something went wrong."
+        mf = scf.RHF(cell, exxdiv=None).density_fit()
+        mf.kernel()
 
-        mc = mcpdft.CASSCF(mf, 'tPBE', 8, 10).run()
-        e_ref = mc.e_tot
+        orblst = addons.mo_comps(['F 2s', 'F 2p'], cell, 
+                                 mf.mo_coeff, orth_method='meta-lowdin').argsort()
+        # Generating the reference values
+        mc = mcpdft.CASCI(mf, 'tPBE', 4, 7)
+        mo = mc.sort_mo(orblst[-mc.ncas:], base=0)
+        mc.fcisolver  = csf_solver(cell, smult=2)
+        mc.kernel(mo)
+
+        e_ref0 = mc._scf.e_tot
+        e_ref1 = mc.e_cas
+        e_ref2 = mc.e_tot
+
         del mc
 
-        mc = mcscf.CASSCF(dmet_mf,8,10)
-        mc.fcisolver  = csf_solver(cell, smult=1)
-        mc.kernel()
+        # Run the pDMET
+        dmet_mf, mypdmet = runpDMET(mf, lo_method='lowdin', 
+                            bath_tol=1e-10, atmlst=[0,], 
+                            density_fit=True)
         
-        mo_coeff = mypdmet.assemble_mo(mc.mo_coeff)
-        mypdft = mcpdft.CASCI(mf, 'tPBE', mc.ncas, mc.nelecas)
-        mypdft.compute_pdft_energy_(mo_coeff=mo_coeff, ci=mc.ci, dump_chk=False)
-        e_check = mypdft.e_tot
-        self.assertAlmostEqual(e_ref, e_check, 6)
-        del cell, mf, dmet_mf, mc, mypdft, e_check, e_ref
-        
+        # Active space orbitals
+        mo_coeff = mypdmet.assemble_mo(dmet_mf.mo_coeff)
+        orblst = addons.mo_comps(['F 2s', 'F 2p'], cell, mo_coeff, 
+                                 orth_method='meta-lowdin').argsort()
+        mypdft = mcpdft.CASCI(mf, 'tPBE', 4, 7)
+        mo = mypdft.sort_mo(orblst[-mypdft.ncas:], base=0, mo_coeff=mo_coeff)
+        mypdft.fcisolver  = csf_solver(cell, smult=2)
+        mypdft.kernel(mo_coeff=mo)
 
+        # Now compare:
+        e_check0 = dmet_mf.e_tot
+        e_check1 = mypdft.e_cas
+        e_check2 = mypdft.e_tot
+
+        del cell, mf, dmet_mf, mypdft, mypdmet
+
+        self.assertAlmostEqual(e_ref0, e_check0, 6)
+        self.assertAlmostEqual(e_ref1, e_check1, 6)
+        self.assertAlmostEqual(e_ref2, e_check2, 6)
+
+    def test_casscf_pdft(self):
+        cell = get_cell()
+        mf = scf.RHF(cell, exxdiv=None).density_fit()
+        mf.kernel()
+
+        orblst = addons.mo_comps(['F 2pz'], cell, 
+                                 mf.mo_coeff, orth_method='meta-lowdin').argsort()
+        # Generating the reference values
+        mc = mcpdft.CASSCF(mf, 'tPBE', 1, 1)
+        mo = mc.sort_mo(orblst[-mc.ncas:], base=0)
+        mc.fcisolver  = csf_solver(cell, smult=2)
+        mc.kernel(mo)
+
+        del mc
+        
+        e_ref0 = mc._scf.e_tot
+        e_ref1 = mc.e_cas
+        e_ref2 = mc.e_tot
+
+        # Run the pDMET
+        dmet_mf, mypdmet = runpDMET(mf, lo_method='lowdin', 
+                            bath_tol=1e-10, atmlst=[0,], 
+                            density_fit=True)
+        
+        mc = mcscf.CASSCF(dmet_mf, 1, 1)
+        mo = mc.sort_mo(orblst[-mc.ncas:], base=0)
+        mc.fcisolver  = csf_solver(cell, smult=2)
+        mc.kernel(mo)
+
+        # Active space orbitals
+        mo_coeff = mypdmet.assemble_mo(mc.mo_coeff)
+
+        orblst = addons.mo_comps(['F 2pz'], cell, mo_coeff, 
+                                 orth_method='meta-lowdin').argsort()
+        mypdft = mcpdft.CASCI(mf, 'tPBE', 1, 1)
+        mo = mypdft.sort_mo(orblst[-mypdft.ncas:], base=0, mo_coeff=mo_coeff)
+        mypdft.fcisolver  = csf_solver(cell, smult=2)
+        mypdft.kernel(mo_coeff=mo, ci0=mc.ci)
+
+        # Now compare:
+        e_check0 = dmet_mf.e_tot
+        e_check1 = mypdft.e_cas
+        e_check2 = mypdft.e_tot
+
+        del cell, mf, dmet_mf, mc, mypdft, mypdmet
+
+        self.assertAlmostEqual(e_ref0, e_check0, 6)
+        self.assertAlmostEqual(e_ref1, e_check1, 6)
+        self.assertAlmostEqual(e_ref2, e_check2, 6)
 
 if __name__ == "__main__":
     # See the description of the tests at the top of the file.
