Fix LASSIS orbital Hessian coordinate shift
When numerically computing the orbital-rotation Hessian-vector
product, there is a correction that has to be added for the change
in the orbitals that define the coordinate space. This corresponds
to a missing terms in the analytical expression which resolves the
inconsistencies between this work and the LASSCF reference
equations.

my_pyscf/lassi/lassis/hessian_orb_ci_si.py
@@ -43,9 +43,13 @@ class HessianOperator (sparse_linalg.LinearOperator):
         self.h2_paaa = np.stack (self.h2_paaa, axis=0)
         dm0 = 2*mo_coeff[:,:ncore] @ mo_coeff[:,:ncore].conj ().T
         self.veff_c = np.squeeze (las.get_veff (dm=dm0))
+        dm1 = mo_coeff[:,ncore:nocc] @ self.casdm1 @ mo_coeff[:,ncore:nocc].conj ().T
+        self.veff_a = np.squeeze (las.get_veff (dm=dm1))
+        v1_pc = mo_coeff.conj ().T @ self.veff_a @ mo_coeff
+        v1_pc[:,ncore:] = 0
         self.ham_2q = lsi.ham_2q (mo_coeff)
         h1 = lsi._las.get_hcore () + self.veff_c
-        self.h1 = mo_coeff.conj ().T @ h1 @ mo_coeff
+        self.h1 = mo_coeff.conj ().T @ h1 @ mo_coeff + v1_pc
         self.fock1 = self.get_fock1 (self.h1, self.h2_paaa, self.casdm1, self.casdm2)
         self.spaces = list_spaces (lsi)
         self.e_roots_si = np.zeros (self.nroots_si)
@@ -146,7 +150,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
 
     def get_xham_2q (self, kappa):
         return xham_2q (self.lsi, kappa, mo_coeff=self.mo_coeff, eris=self.eris,
-                        veff_c=self.veff_c, casdm1=self.casdm1)
+                        veff_c=self.veff_c, veff_a=self.veff_a, casdm1=self.casdm1)
 
     def _matvec (self, x):
         log = self.log
@@ -194,6 +198,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
     def hoo (self, xham_2q, kappa):
         h0, h1, h2 = xham_2q
         fx = self.get_fock1 (h1, h2, self.casdm1, self.casdm2)
+        fx += kappa @ (self.fock1 - self.fock1.T)
         return (fx - fx.T) / 2
 
     def hoa (self, ci1, si0, si1):
@@ -214,7 +219,7 @@ class HessianOperator (sparse_linalg.LinearOperator):
         fx[:,:self.lsi.ncore] += 2 * veff[:,:self.lsi.ncore]
         return fx - fx.T
 
-def xham_2q (lsi, kappa, mo_coeff=None, eris=None, veff_c=None, casdm1=None):
+def xham_2q (lsi, kappa, mo_coeff=None, eris=None, veff_c=None, veff_a=None, casdm1=None):
     las = lsi._las
     if mo_coeff is None: mo_coeff=lsi.mo_coeff
     if eris is None: eris = lsi.get_casscf_eris (mo_coeff)
@@ -227,6 +232,12 @@ def xham_2q (lsi, kappa, mo_coeff=None, eris=None, veff_c=None, casdm1=None):
             veff_c = np.squeeze (las.get_veff (dm=dm0))
         else:
             veff_c = 0
+    if veff_a is None:
+        if ncas:
+            dm1 = mo_coeff[:,ncore:nocc] @ casdm1 @ mo_coeff[:,ncore:nocc].conj ().T
+            veff_a = np.squeeze (las.get_veff (dm=dm1))
+        else:
+            veff_a = 0
 
     h0 = 0
 
@@ -238,11 +249,7 @@ def xham_2q (lsi, kappa, mo_coeff=None, eris=None, veff_c=None, casdm1=None):
     h1 = mo0H @ h1_0 @ mo1 - mo1H @ h1_0 @ mo0
     h2 = np.stack ([eris.ppaa[i][ncore:nocc] for i in range (nmo)], axis=0)
     if ncore:
-        dm1 = np.zeros ((nmo,nmo), dtype=casdm1.dtype)
-        dm1[ncore:nocc,ncore:nocc] = casdm1
-        dm1 = mo0 @ dm1 @ mo0H
-        v_c1a0 = np.squeeze (las.get_veff (dm=dm1))
-        v_c1a0 = mo0H @ v_c1a0 @ mo1 - mo1H @ v_c1a0 @ mo0
+        v_c1a0 = mo0H @ veff_a @ mo1 - mo1H @ veff_a @ mo0
         v_c1a0[:,ncore:] = 0
         dm1 = 2*np.eye (nmo)
         dm1[ncore:] = 0

tests/lassi/addons.py
@@ -80,7 +80,7 @@ def case_contract_hlas_ci (ks, las, h0, h1, h2, ci_fr, nelec_frs, si_bra=None, s
                     hket_ref_s = hket_ref[:,k:l]
                     # TODO: opt>0 for things other than single excitation
                     #if opt>0 and not spaces[r].is_single_excitation_of (spaces[s]): continue
-                    #elif opt==1: print (r,s, round (lib.fp (hket_pq_s)-lib.fp (hket_ref_s),3))
+                    #elif opt==1: print (r,s, round (lib.fp (hket_pq_s)-lib.fp (hket_ref_s),2))
                     with ks.subTest (opt=opt, frag=f, bra_space=r, ket_space=s,
                                        intyp=interactions[interidx[r,s]],
                                        dneleca=nelec[:,r,0]-nelec[:,s,0],
@@ -278,7 +278,7 @@ def case_lassis_hessian (ks, lsis):
     #for ci_i in ci_ref:
     #    ci_i[:,:] = 0
     #    ci_i[0,0] = 1
-    g0 = grad_orb_ci_si.get_grad (lsis, ci_ref=ci_ref, si=si, pack=True)
+    g0_sec = list (grad_orb_ci_si.get_grad (lsis, ci_ref=ci_ref, si=si, pack=False))
     ugg = coords.UnitaryGroupGenerators (
         lsis,
         lsis.mo_coeff,
@@ -287,8 +287,8 @@ def case_lassis_hessian (ks, lsis):
         lsis.ci_charge_hops,
         si
     )
+    g0 = ugg.pack (*g0_sec)
     g0_debug = grad_orb_ci_si.get_grad (lsis, *ugg.update_wfn (np.zeros_like (g0)), pack=True)
-    #ks.assertLess (np.amax (np.abs (g0_debug-g0)), 1e-10, msg='sanity fail')
     h_op = hessian_orb_ci_si.HessianOperator (ugg)
     np.random.seed (1)
     x0 = np.random.rand (ugg.nvar_tot)
@@ -296,12 +296,26 @@ def case_lassis_hessian (ks, lsis):
     assert (len (x0) == len (g0))
     sec_lbls = ['orb', 'ci_ref', 'ci_sf', 'ci_ch', 'si_avg', 'si_ext']
     sec_offs = ugg.get_sector_offsets ()
+    nao, nmo = lsis.mo_coeff.shape
+    ncas, ncore = sum (lsis.ncas_sub), lsis.ncore
+    nocc = ncore + ncas
+    # A lot rides on this correction being accurate
+    def orbital_frame_correction (xi):
+        i, j = sec_offs[0]
+        gorb = g0_sec[0]
+        xorb = ugg.unpack (xi)[0]
+        dgorb = (xorb @ gorb - gorb @ xorb) / 2
+        dgi = [dgorb,] + g0_sec[1:]
+        dgi = ugg.pack (*dgi)
+        dgi[j:] = 0
+        return dgi
     for (i, j), lbl0 in zip (sec_offs, sec_lbls):
         if i==j: continue
         with ks.subTest ("sanity", sector=lbl0):
             ks.assertLess (np.amax (np.abs (g0_debug[i:j]-g0[i:j])), 1e-9)
         x1 = np.zeros_like (x0)
         x1[i:j] = x0[i:j]
+        dg1_ref = orbital_frame_correction (x1)
         g1_test = h_op (x1)
         err_last = [np.finfo (float).tiny,]*len(sec_lbls)
         err_table = ['\n{:s} {:s}\n'.format (lbl1, lbl0) for lbl1 in sec_lbls]
@@ -320,6 +334,7 @@ def case_lassis_hessian (ks, lsis):
             mg1_ref = ugg.pack (*mg1_ref) - g0
             g1_ref -= mg1_ref
             g1_ref *= .5 * div
+            g1_ref += dg1_ref
             for z, (k,l) in enumerate (sec_offs):
                 if k==l:
                     rel_err[z] = .25