b33240e87362f51d1fe46bac2eced6887d20755e
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-08-06 17:43:47 -0500

    Example added

--- my_pyscf/dkh/__init__.py
+++ my_pyscf/dkh/__init__.py
@@ -1,14 +1,14 @@
 
 #
 # Source code is obtained from Prof. Marcus Reiher
-# Bhavnesh Jangid cleaned it, and patched it with PySCF
+# Bhavnesh Jangid has interfaced it with PySCF
 
-# Please consider citinh
+# Please consider citing
 # M. Reiher, A. Wolf, J. Chem. Phys. 121 (2004) 10944-10956           
 # A. Wolf, M. Reiher, B. A. Hess, J. Chem. Phys. 117 (2002) 9215-9226 
 
 
-from MySlate.DKH._dkh import dkhscalar
+from mrh.my_pyscf.dkh._dkh import dkhscalar
 
 dkh = dkhscalar
 
