6d6c7eb46a26fb75a037c30e673d10190efd394c
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-09-08 22:34:48 -0500

    Correct the geometries

--- examples/contracted_ano_basis/general_ano_rcc_basis.py
+++ examples/contracted_ano_basis/general_ano_rcc_basis.py
@@ -18,21 +18,21 @@ Returns: a dictionary that can be directly used as basis argument in mol object.
 '''
 
 # Example-1
-mol1 = gto.M(atom='Fe 0 0 0; Xe 0 0 0; Cd 0 0 0', basis='ano', charge=2, verbose=0)
+mol1 = gto.M(atom='Fe 0 0 0; Xe 0 0 10; Cd 0 0 20', basis='ano', charge=2, verbose=0)
 basis = get_ano_rcc_basis (mol1, 'VTZP')
 print(basis)
 mol1.basis = basis
 mol1.build ()
 
 # Example-2: Using mix-basis sets
-mol1 = gto.Mole(atom='Fe 0 0 0; Xe 0 0 0; Cd 0 0 0', charge=2, verbose=0)
+mol1 = gto.Mole(atom='Fe 0 0 0; Xe 0 0 10; Cd 0 0 20', charge=2, verbose=0)
 basis = get_ano_rcc_basis (mol1, {'Fe':'ANO-RCC-VTZP', 'Cd':'ANO-RCC-VDZP', 'Xe':'ANO-RCC-MB'})
 print(basis)
 mol1.basis = basis
 mol1.build ()
 
 # Example-3: Using 'default' keyword in basis dictionary
-mol1 = gto.Mole(atom='Fe 0 0 0; He 0 0 0; Mg 0 0 0', charge=2, verbose=0)
+mol1 = gto.Mole(atom='Fe 0 0 0; He 0 0 10; Mg 0 0 20', charge=2, verbose=0)
 basis = get_ano_rcc_basis (mol1, {'Fe':'ANO-RCC-VQZP', 'default':'cc-pvdz-dk'})
 print(basis)
 mol1.basis = basis
