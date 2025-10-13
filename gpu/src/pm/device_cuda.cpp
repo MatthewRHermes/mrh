cbe97e04aed3f15c9448910b8b826e7cf84fd890
cjknight <cjknight2009@gmail.com>
2025-10-13 14:24:48 -0500

    use batched vecadd in reduce_buf3_to_rdm()

--- gpu/src/pm/device_cuda.cpp
+++ gpu/src/pm/device_cuda.cpp
@@ -1915,10 +1915,14 @@ void Device::reduce_buf3_to_rdm(const double * buf3, double * dm2, int size_tdm2
   dim3 grid_size (_TILE(size_tdm2, block_size.x),1,1);
 
   //  printf("reduce_buf3_to_rdm : size= %i  num_batches= %i\n", size_tdm2, num_gemm_batches);
-  
+
+#if 1
+  vecadd_batch(buf3, dm2, size_tdm2, num_gemm_batches);
+#else
   for (int i=0;i<num_gemm_batches; ++i){
     _vecadd<<<grid_size, block_size, 0, s>>>( &(buf3[i*size_tdm2]), dm2, size_tdm2);
   }
+#endif
   _CUDA_CHECK_ERRORS();
 }
 
