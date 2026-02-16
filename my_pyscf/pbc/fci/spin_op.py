f70833b63de9618f397481efe837d387cc8742a0
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-02-16 17:10:14 -0600

    Spin operator for the complex CI added. TODO: Spin-penalty method for CASCI.

---
+++ my_pyscf/pbc/fci/spin_op.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from pyscf.fci.spin_op import contract_ss
+
+# Spin square operator for the complex FCI wave function.
+def spin_square0(fcivec, norb, nelec):
+    '''
+    Spin square for complex RHF-FCI CI wfn only.
+    (a-ib)*S^2*(a+ib) = a*S^2*a + b*S^2*b
+    '''
+    assert fcivec.dtype == np.complex128
+    
+    def s2(ci1):
+        ci1ssket = contract_ss(ci1, norb, nelec)
+        return np.vdot(ci1, ci1ssket)
+    
+    ss = s2(fcivec.real)
+    ss += s2(fcivec.imag)
+    
+    s = np.sqrt(ss + 0.25) - 0.5
+    multip = 2*s + 1
+    return ss, multip
