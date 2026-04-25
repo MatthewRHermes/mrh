1e159ea29cff7e2519f878d26c317caeb03ebffe
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-04-25 14:06:22 -0500

    kCASSCF unit test added

--- tests/pbc/test_pbc_casscf.py
+++ tests/pbc/test_pbc_casscf.py
@@ -1,14 +1,9 @@
 import unittest
 import numpy as np
-
 from pyscf.pbc import gto as pgto
 from pyscf.pbc import scf
-
 from mrh.my_pyscf.pbc import mcscf
-from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
 from mrh.my_pyscf.pbc.mcscf import avas
-from mrh.my_pyscf.pbc.mcscf.k2R import get_mo_coeff_k2R
-
 
 def get_xyz(nU=1, dintra= 1.3, dinter=1.4):
     unit = [("H", 0.0, 0.0, 0.0), ("H", 0.0, 0.0, dintra)]
@@ -25,16 +20,38 @@ def get_cell(nU=1, dintra=1.3, dinter=1.4, basis='6-31G', pseudo=None, maxMem=40
                     pseudo = pseudo,
                     precision = 1e-10,
                     verbose = verbose,
-                    output = '/dev/null',
                     max_memory = maxMem,
                     ke_cutoff = 40)
     cell.build()
     return cell
 
+def get_cell2():
+    cell = pgto.Cell(
+        a=np.diag([3.0, 10.0, 10.0]),
+        atom="N 0.0 0.0 0.0; N 0.0 0.0 1.1",
+        basis="STO-3G",
+        unit="Angstrom",
+        ke_cutoff=50,
+        precision=1e-12,
+        verbose=0,)
+    return cell
+
+def get_cell3():
+    cell = pgto.Cell(
+        a=np.diag([3.0, 10.0, 10.0]),
+        atom="Be 0.0 0.0 0.0",
+        basis="6-31G",
+        unit="Angstrom",
+        max_memory=100000,
+        ke_cutoff=50,
+        precision=1e-12,
+        verbose=0,
+    )
+    return cell
 
 class KnownValues(unittest.TestCase):
 
-    def test_krhf_limit(self):
+    def test_krhf_limit_ncore0(self):
         cell = get_cell(nU=1, dintra=1.3, dinter=1.4)
         kmesh1D = [3, 1, 1]
         kpts = cell.make_kpts(kmesh1D, wrap_around=True)
@@ -49,7 +66,27 @@ class KnownValues(unittest.TestCase):
         msg = "KRHF energy for the limit of 1 unit cell does not match with the RHF energy."
         self.assertAlmostEqual(e_scf, e_cas, places=6, msg=msg)
 
-    def test_kCASSCF(self):
+        del cell, kpts, kmf, kmc
+
+    def test_krhf_limit(self):
+        cell = get_cell2()
+        cell.build()
+        kmesh1D = [3, 1, 1]
+        kpts = cell.make_kpts(kmesh1D, wrap_around=True)
+        kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
+        kmf.max_cycle=100
+        kmf.exxdiv = None
+        e_scf = kmf.kernel()
+
+        kmc = mcscf.CASSCF(kmf, 1, 2)
+        e_cas = kmc.kernel(kmf.mo_coeff)[0]
+
+        msg = "KRHF energy for the limit of 1 unit cell does not match with the RHF energy."
+        self.assertAlmostEqual(e_scf, e_cas, places=6, msg=msg)
+
+        del cell, kpts, kmf, kmc
+
+    def test_kCASSCF_ncore0(self):
         cell = get_cell(nU=1, dintra=1.3, dinter=1.4)
         kmesh1D = [3, 1, 1]
         kpts = cell.make_kpts(kmesh1D, wrap_around=True)
@@ -63,6 +100,27 @@ class KnownValues(unittest.TestCase):
 
         kmc = mcscf.CASSCF(kmf, 2, 2)
         e_cas = kmc.kernel(kmf.mo_coeff)[0]
+
+        msg = "KRHF energy for the limit of 1 unit cell does not match with the RHF energy."
+        self.assertAlmostEqual(e_ref, e_cas, places=6, msg=msg)
+    
+    def test_kCASSCF(self):
+        cell = get_cell3()
+        cell.build()
+        kmesh1D = [2, 1, 1]
+        kpts = cell.make_kpts(kmesh1D, wrap_around=True)
+        kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
+        kmf.max_cycle=100
+        kmf.exxdiv = None
+        e_scf = kmf.kernel()
+
+        e_ref = -14.26898884190738
+        mo_coeff = avas.kernel(kmf, ['Be 2s', 'Be 2p'], minao=cell.basis)[2]
+        mo_coeff = np.array(mo_coeff)
+
+        # mo_coeff = mo_coeff[None, :, :]
+        kmc = mcscf.CASSCF(kmf, 4, (1, 1))
+        e_cas = kmc.kernel(mo_coeff)[0]
 
         msg = "KRHF energy for the limit of 1 unit cell does not match with the RHF energy."
         self.assertAlmostEqual(e_ref, e_cas, places=6, msg=msg)
