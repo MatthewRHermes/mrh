cdbd1623375c1760238c58478d9b78b8b9281bf3
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-04-26 15:37:28 -0500

    Example added for the more optimized FCI solver

---
+++ examples/pbc/05-cplx-fci_opt.py
@@ -0,0 +1,123 @@
+
+import numpy as np
+from ase import Atoms
+
+from pyscf import lib
+from pyscf.pbc import scf, gto as pgto
+
+from mrh.my_pyscf.pbc import mcscf
+from mrh.my_pyscf.pbc.fci import direct_spin1_cplx_opt, direct_spin0_cplx, direct_spin1_cplx
+
+'''
+In this file, we are comparing different type of cplx FCI solvers.
+1. direct_spin1_cplx.FCISolver: 
+2. direct_spin1_cplx_opt.FCISolver: This is the optimized version of the above code. (Low level optimization of the contract_2e function.)
+3. direct_spin0_cplx.FCISolver: This is the optimized version of the above code for spin0/singlet symmetry.
+
+I would also added the time taken by the different solvers to compare the performance.
+'''
+
+
+def make_h2_1d_cell(intraH=0.74, interH=1.5, nx=1, vacuum=17.5,
+                    basis='6-31G', ke_cutoff=100, precision=1e-10, verbose=4):
+    Lx = nx * (intraH + interH)
+
+    atoms = Atoms(
+        symbols=['H'] * (2 * nx),
+        positions=[
+            [i * (intraH + interH) + dx, vacuum / 2, vacuum / 2]
+            for i in range(nx)
+            for dx in (0.0, intraH)
+        ],
+        cell=np.diag([Lx, vacuum, vacuum]),
+    )
+    atoms.center()
+
+    cell = pgto.Cell()
+    cell.a = atoms.cell.array
+    cell.atom = list(zip(atoms.get_chemical_symbols(), map(tuple, atoms.get_positions())))
+    cell.basis = basis
+    cell.unit = 'Angstrom'
+    cell.max_memory = 100000
+    cell.ke_cutoff = ke_cutoff
+    cell.precision = precision
+    cell.verbose = verbose
+    cell.build()
+
+    return cell
+
+cell = make_h2_1d_cell()
+cell.build()
+
+kmesh1D = [7, 1, 1]
+
+kpts = cell.make_kpts(kmesh1D, wrap_around=True)
+
+kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
+kmf.max_cycle=1000
+kmf.exxdiv = None
+kmf.conv_tol = 1e-10
+kmf.kernel()
+
+logger = lib.logger
+log = lib.logger.Logger(cell.stdout, lib.logger.TIMER_LEVEL)
+cput1 = cput0 = (logger.process_clock(), logger.perf_counter())
+
+with lib.temporary_env(kmf):
+    kmc = mcscf.CASCI(kmf, 2, 2)
+    kmc.fcisolver = direct_spin1_cplx.FCISolver(cell)
+    kmc.kernel(kmf.mo_coeff)
+    e1 = kmc.e_tot
+    print("Direct Spin1 Complex FCI Energy: ", e1)
+
+    del kmc
+
+cput1 = log.timer('direct_spin1_cplx takes: ', *cput1)
+
+with lib.temporary_env(kmf):
+    kmc = mcscf.CASCI(kmf, 2, 2)
+    kmc.fcisolver = direct_spin1_cplx_opt.FCISolver(cell)
+    kmc.kernel(kmf.mo_coeff)
+    e2 = kmc.e_tot
+    print("Direct Spin1 Complex Optimized FCI Energy: ", e2)
+    del kmc
+
+cput1 = log.timer('direct_spin1_cplx_opt takes: ', *cput1)
+
+with lib.temporary_env(kmf):
+    kmc = mcscf.CASCI(kmf, 2, 2)
+    kmc.fcisolver = direct_spin0_cplx.FCISolver(cell)
+    kmc.kernel(kmf.mo_coeff)
+    e3 = kmc.e_tot
+    print("Direct Spin0 Complex FCI Energy: ", e3)
+cput1 = log.timer('direct_spin0_cplx takes: ', *cput1)
+
+# Now compare the energy and time taken by the different solvers.
+print("\n------Summary of Energies:---------")
+print(f"Direct Spin1 Complex FCI Energy          : {e1.real:.12f}")
+print(f"Direct Spin1 Complex Optimized FCI Energy: {e2.real:.12f}")
+print(f"Direct Spin0 Complex FCI Energy          : {e3.real:.12f}")
+
+# Currently (commit d8eb4113d4055e5e851a3deaa2221665767a3770) 
+# on 36 threads and 100 GB of memory: 
+# The noted time (total wall time) is for one CASCI iteration.
+
+# Case-1: for (12e, 12o)
+# direct_spin1_cplx takes    :  113.89 seconds (CPU time)
+# direct_spin1_cplx_opt takes:  24.33 seconds (CPU time)
+# direct_spin0_cplx takes    :  16.33 seconds (CPU time)
+
+# Molecular code for the same size of active space
+# direct_spin1 takes    :  2.75 seconds (CPU time)
+# direct_spin0 takes    :  3.36 seconds (CPU time)
+
+
+# Case-2: for (14e, 14o)
+# direct_spin1_cplx takes    :  980.45 seconds (CPU time)
+# direct_spin1_cplx_opt takes:  438.56 seconds (CPU time)
+# direct_spin0_cplx takes    :  254.5 seconds (CPU time)
+
+# Molecular code for the same size of active space
+# direct_spin1 takes    :  194.41 seconds (CPU time)
+# direct_spin0 takes    :  143.03 seconds (CPU time)
+
--- tests/pbc/test_pbc_cplx_fci_opt.py
+++ tests/pbc/test_pbc_cplx_fci_opt.py
@@ -1,26 +1,22 @@
 #!/bin/bash
 import unittest
 import numpy as np
-from ase import Atoms
-from functools import reduce
 
 from pyscf.fci import cistring
 
-from pyscf import ao2mo, fci
-from pyscf.pbc import gto as pgto, scf
-from pyscf.pbc.lib import kpts_helper
-from pyscf.pbc.tools import k2gamma
-
 from mrh.my_pyscf.pbc.fci import direct_spin1_cplx, direct_spin1_cplx_opt, direct_spin0_cplx
-from mrh.my_pyscf.pbc.mcscf.k2R import get_mo_coeff_k2R
-
 
 # Author: Bhavnesh Jangid
 
 '''
 In this file, there are test for the optimization of the cplx FCI solver.
 
-Unit-Test-1: Optimization of the contract_2e function.
+Unit-Test-1: This will check the correctness of the optimized code `direct_spin1_cplx_opt` 
+    with the reference code `direct_spin1_cplx`. We will compare the FCI energies and the 
+    output of the `contract_2e` function for a random CI vector.
+
+Unit-Test-2: This will check the correctness of the optimized spin0 code `direct_spin0_cplx` 
+    with the reference spin1 code `direct_spin1_cplx`. 
 '''
 
 
@@ -32,8 +28,8 @@ def gen_hermi_ham(norb):
     h1 = np.asarray(h1).astype(np.complex128)
     h2 = np.asarray(h2).astype(np.complex128)
 
-    h1.imag += 1e-2
-    h2.imag += 1e-2
+    h1.imag += 1e-2 * np.random.random(h1.shape)
+    h2.imag += 1e-2 * np.random.random(h2.shape)
 
     # Restore symmetries
     h1 = h1 + h1.conj().T
@@ -43,33 +39,34 @@ def gen_hermi_ham(norb):
     return h1, h2
 
 class KnownValues(unittest.TestCase):
-    def test_cplx_fci_opt1(self):
-        # Doing the calculation of (4o, 4e)
+    def test_direct_spin1_cplx_opt(self):
+        # Doing the calculation of (4o, 4e) and various spin states
         norb = 4
-        nelecas = (2, 2)
+        nelecas = [(2, 2), (3, 1), (4, 0)]
         h1, h2 = gen_hermi_ham(norb)
         
         # Reference values
-        cisolver1 = direct_spin1_cplx.FCISolver()
-        eref, ciref = cisolver1.kernel(h1, h2, norb, nelecas)
-        
-        # My computed values
-        cisolver2 = direct_spin1_cplx_opt.FCISolver()
-        e_com = cisolver2.kernel(h1.copy(), h2.copy(), norb, nelecas)[0]
-        msg = "There is the mismatch in the FCI energies between the optimized and the reference code."
-        self.assertAlmostEqual(eref, e_com, places=8, msg=msg)
+        for nele in nelecas:
+            cisolver1 = direct_spin1_cplx.FCISolver()
+            eref, ciref = cisolver1.kernel(h1, h2, norb, nele)
 
-        h2eff = h2
-        ci0 = np.random.random(ciref.shape) + 1j * np.random.random(ciref.shape)
-        ci0 = ci0.astype(np.complex128)
+            # My computed values
+            cisolver2 = direct_spin1_cplx_opt.FCISolver()
+            e_com = cisolver2.kernel(h1.copy(), h2.copy(), norb, nele)[0]
+            msg = "There is the mismatch in the FCI energies between the optimized and the reference code."
+            self.assertAlmostEqual(eref, e_com, places=8, msg=msg)
 
-        ci0 /= np.linalg.norm(ci0)
-        sigma_ref = cisolver1.contract_2e(h2eff, ci0, norb, nelecas)
-        sigma_com = cisolver2.contract_2e(h2eff, ci0, norb, nelecas)
-        msg = "There is the mismatch in the contract_2e outputs between the optimized and the reference code."
-        self.assertTrue(np.allclose(sigma_ref, sigma_com, atol=1e-8), msg=msg)
+            h2eff = h2
+            ci0 = np.random.random(ciref.shape) + 1j * np.random.random(ciref.shape)
+            ci0 = ci0.astype(np.complex128)
 
-    def test_cplx_fci_opt_spin0(self):
+            ci0 /= np.linalg.norm(ci0)
+            sigma_ref = cisolver1.contract_2e(h2eff, ci0, norb, nele)
+            sigma_com = cisolver2.contract_2e(h2eff, ci0, norb, nele)
+            msg = "There is the mismatch in the contract_2e outputs between the optimized and the reference code."
+            self.assertTrue(np.allclose(sigma_ref, sigma_com, atol=1e-8), msg=msg)
+
+    def test_direct_spin0_cplx(self):
         # Doing the calculation of (4o, 4e)
         norb = 4
         nelecas = (2, 2)
@@ -115,58 +112,6 @@ class KnownValues(unittest.TestCase):
             "the optimized spin0 code and the symmetrized spin1 reference."
         )
         self.assertTrue(np.allclose(sigma_ref, sigma_spin0, atol=1e-8), msg=msg)
-
-    def test_cplx_fci_opt2(self):
-        # Doing the calculation of (4o, 4e)
-        norb = 4
-        nelecas = (3, 1)
-        h1, h2 = gen_hermi_ham(norb)
-        
-        # Reference values
-        cisolver1 = direct_spin1_cplx.FCISolver()
-        eref, ciref = cisolver1.kernel(h1, h2, norb, nelecas)
-        
-        # My computed values
-        cisolver2 = direct_spin1_cplx_opt.FCISolver()
-        e_com = cisolver2.kernel(h1, h2, norb, nelecas)[0]
-        msg = "There is the mismatch in the FCI energies between the optimized and the reference code."
-        self.assertAlmostEqual(eref, e_com, places=8, msg=msg)
-
-        # Also comparing the contract_2e function outputs
-        h2eff = h2
-        ci0 = np.random.random(ciref.shape) + 1j * np.random.random(ciref.shape)
-        ci0 = ci0.astype(np.complex128)
-        ci0 /= np.linalg.norm(ci0)
-        sigma_ref = cisolver1.contract_2e(h2eff, ci0, norb, nelecas)
-        sigma_com = cisolver2.contract_2e(h2eff, ci0, norb, nelecas)
-        msg = "There is the mismatch in the contract_2e outputs between the optimized and the reference code."
-        self.assertTrue(np.allclose(sigma_ref, sigma_com, atol=1e-8), msg=msg)
-
-    def test_cplx_fci_opt3(self):
-        # Doing the calculation of (4o, 4e)
-        norb = 4
-        nelecas = (4, 0)
-        h1, h2 = gen_hermi_ham(norb)
-        
-        # Reference values
-        cisolver1 = direct_spin1_cplx.FCISolver()
-        eref, ciref = cisolver1.kernel(h1, h2, norb, nelecas)
-        
-        # My computed values
-        cisolver2 = direct_spin1_cplx_opt.FCISolver()
-        e_com = cisolver2.kernel(h1, h2, norb, nelecas)[0]
-        msg = "There is the mismatch in the FCI energies between the optimized and the reference code."
-        self.assertAlmostEqual(eref, e_com, places=8, msg=msg)
-
-        # Also comparing the contract_2e function outputs
-        h2eff = h2
-        ci0 = np.random.random(ciref.shape) + 1j * np.random.random(ciref.shape)
-        ci0 = ci0.astype(np.complex128)
-        ci0 /= np.linalg.norm(ci0)
-        sigma_ref = cisolver1.contract_2e(h2eff, ci0, norb, nelecas)
-        sigma_com = cisolver2.contract_2e(h2eff, ci0, norb, nelecas)
-        msg = "There is the mismatch in the contract_2e outputs between the optimized and the reference code."
-        self.assertTrue(np.allclose(sigma_ref, sigma_com, atol=1e-8), msg=msg)
 
 if __name__ == "__main__":
     unittest.main()
