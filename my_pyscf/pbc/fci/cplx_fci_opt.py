e13e0d730c5f68999598ce114822afde463d8910
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-04-26 12:00:07 -0500

    Add the strb_blksize wrapper in python code

--- my_pyscf/pbc/fci/cplx_fci_opt.py
+++ my_pyscf/pbc/fci/cplx_fci_opt.py
@@ -35,16 +35,27 @@ def contract_2e(eri, fcivec, norb, nelec, link_index=None):
     fcivec = np.asarray(fcivec, dtype=np.complex128, order='C')
     eri = np.asarray(eri, dtype=np.complex128, order='C')
     out_CI = np.zeros_like(fcivec)
-    libpbcfci.FCIcontract_2es1_zgemm(eri.ctypes.data_as(ctypes.c_void_p),
+
+    # Currently the strb_blksize is hard coded to 240. But that makes it so slow for the 
+    # AS > (8e, 8o), hence I am setting the strb_blksize here. Currently I am setting the nb to less
+    # than 1000, which would require atmost of 1.5-2.0 GB memory for (12e, 12o) AS. Later on based
+    # on the memory available, we can set the nb and strb_blksize accordingly.
+    if nb < 1000: 
+        strb_blksize = nb
+    else: 
+        x = int(np.ceil(nb / 1000.0))
+        strb_blksize = int(np.ceil(nb / x))
+
+    libpbcfci.FCIcontract_2es1_zgemm_blksize(eri.ctypes.data_as(ctypes.c_void_p),
                                 fcivec.ctypes.data_as(ctypes.c_void_p),
                                 out_CI.ctypes.data_as(ctypes.c_void_p),
                                 ctypes.c_int(norb),
                                 ctypes.c_int(na), ctypes.c_int(nb),
                                 ctypes.c_int(nlinka), ctypes.c_int(nlinkb),
                                 link_indexa.ctypes.data_as(ctypes.c_void_p),
-                                link_indexb.ctypes.data_as(ctypes.c_void_p))
+                                link_indexb.ctypes.data_as(ctypes.c_void_p),
+                                ctypes.c_int(strb_blksize))
     return out_CI.view(direct_spin1.FCIvector)
-
 
 class FCISolver(direct_spin1_cplx_FCISolver):
     def contract_2e(self, eri, fcivec, norb, nelec, link_index=None):
