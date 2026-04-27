e30c174d6a83e6f941751143f98c9f52cdf0cda2
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-04-27 10:41:00 -0500

    Update the CASCI example

--- examples/pbc/01-cplx-casci.py
+++ examples/pbc/01-cplx-casci.py
@@ -1,198 +1,65 @@
 
-import os
 import numpy as np
-from ase import Atoms
 from pyscf import lib
-from pyscf.pbc import scf, df
+
+from pyscf.pbc import scf
 from pyscf.pbc import gto as pgto
+
+from mrh.my_pyscf.pbc.fci import csf_solver
 from mrh.my_pyscf.pbc.mcscf import avas
 from mrh.my_pyscf.pbc import mcscf
 
-# Example to run the CASCI for kmf object (1D, 2D or 3D)
-
-# TODO: Write one general function to make the H2 chain in 1D, 2D and 3D. 
-# The current code is a bit redundant.
-def make_h2_1D(intraH=1.0, interH=1.5, nx=1, vacuum=17.5):
-    Lx = nx * (intraH + interH)
-    cell = np.diag([Lx, vacuum, vacuum])
-    y0 = vacuum / 2.0
-    z0 = vacuum / 2.0
-    positions = []
-    symbols = []
-    for i in range(nx):
-        x0 = i * (intraH + interH)
-        positions.append([x0,        y0, z0])   # H1
-        positions.append([x0+intraH, y0, z0])   # H2
-        symbols += ['H', 'H']
-    atoms = Atoms(symbols=symbols, positions=positions, cell=cell, )
-    atoms.center()
-    return atoms
-
-def make_h2_2D(intraH=1.0, interH=1.5, nx=1, ny=1, vacuum=17.5):
-    ax = nx * (intraH + interH)
-    by = ny * (intraH + interH)
-    cz = vacuum
-    cell = np.diag([ax, by, cz])
-    
-    positions = []
-    symbols = []
-    for ix in range(nx):
-        for iy in range(ny):
-            x0 = ix * (intraH + interH)
-            y0 = iy * (intraH + interH)
-            positions.append([x0, y0, vacuum / 2.0])
-            positions.append([x0 + intraH, y0, vacuum / 2.0])
-            symbols += ['H', 'H']
-
-    atoms = Atoms(symbols, positions=positions, cell=cell, pbc=True)
-    atoms.center()
-    return atoms
-
-def make_h2_3D(intraH=1.0, interH=1.5, nx=1, ny=1, nz=1, vacuum=17.5):
-    pitch = intraH + interH
-    ax = nx * pitch
-    by = ny * pitch
-    cz = nz * pitch
-    cell = np.diag([ax, by, cz])
-
-    positions = []
-    symbols = []
-
-    for ix in range(nx):
-        for iy in range(ny):
-            for iz in range(nz):
-                x0 = ix * pitch
-                y0 = iy * pitch
-                z0 = iz * pitch
-                positions.append([x0,          y0, z0])
-                positions.append([x0 + intraH, y0, z0])
-                symbols += ['H', 'H']
-
-    atoms = Atoms(symbols, positions=positions, cell=cell, pbc=True)
-    atoms.center()
-    return atoms
-
-
-# H2 Molecule in 1D
-atoms1D = make_h2_1D(intraH=0.74, interH=1.5, nx = 1, vacuum=17.5)
+# Step-1: Define the cell, basis and related computational settings.
 cell = pgto.Cell()
-cell.a = atoms1D.cell.array
-pos = atoms1D.get_positions()
-sym = atoms1D.get_chemical_symbols()
-cell.atom = [(sym[i], tuple(pos[i])) for i in range(len(sym))]
-cell.basis = 'STO-6G'
-cell.unit = 'Angstrom'
+cell.a = [[4.175, 0.0, 0.0],
+          [0.0, 20.0, 0.0],
+          [0.0, 0.0, 20.0]]
+cell.atom = '''
+Ni 2.73619  10.00000  10.00000
+O 0.64880  10.00000  10.00000
+O 2.73619  12.08739  10.00000
+O 2.73619   7.91261  10.00000
+O 2.73619  10.00000   7.91261
+O 2.73619  10.00000  12.08739
+H 2.73619  12.65763   9.21000
+H 2.73619  12.65763  10.78999
+H 1.94620  10.00000  12.65763
+H 3.52619  10.00000  12.65763
+H 2.73619   7.34236  10.78999
+H 2.73619   7.34236   9.21000
+H 1.94620   9.99999   7.34236
+H 3.52619  10.00000   7.34236
+'''
+cell.basis = {'Ni': 'gth-szv-molopt-sr', 'default': 'gth-szv'}
+cell.pseudo = 'gth-pade'
 cell.max_memory = 100000
 cell.ke_cutoff = 100
 cell.precision = 1e-10
 cell.verbose = lib.logger.INFO
-cell.output = 'h2_1D_CASCI.log'
+cell.output = 'niaqua.log'
+cell.max_memory = 100000
 cell.build()
 
-kmesh1D = [7, 1, 1]
+# Choose the k-mesh
+kmesh1D = [3, 1, 1]
 
 kpts = cell.make_kpts(kmesh1D, wrap_around=True)
 
-kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
+# Step-2: Perform the mean-field calculation
+kmf = scf.KRHF(cell, kpts=kpts).density_fit()
 kmf.max_cycle=1000
 kmf.exxdiv = None
 kmf.conv_tol = 1e-10
 kmf.kernel()
 
-# This is equivalent to (14e, 14o) in the supercell
+# Step-3: Initial guess: Use the p-AVAS not AVAS from PySCF.
+mo_coeff = avas.kernel(kmf, ['Ni 3dz^2', 'Ni 3dx2-y2'], minao=cell.basis)[2]
+mo_coeff = np.array(mo_coeff)
+
+# Step-4: Perform the k-CASCI calculation: Note the FCI problem is solved in wannier basis.
 kmc = mcscf.CASCI(kmf, 2, 2)
-kmc.kernel(kmf.mo_coeff)
+kmc.fcisolver = csf_solver(cell, smult=1)
+kmc.kernel(mo_coeff)
 
-
-del cell, kpts, kmf, kmc
-
-# H2 Molecule in 2D 
-atoms2D = make_h2_2D(intraH=0.74, interH=1.5, nx = 1, ny = 1, vacuum=17.5)
-cell = pgto.Cell()
-cell.a = atoms2D.cell.array
-pos = atoms2D.get_positions()
-sym = atoms2D.get_chemical_symbols()
-cell.atom = [(sym[i], tuple(pos[i])) for i in range(len(sym))]
-cell.basis = 'STO-6G'
-cell.unit = 'Angstrom'
-cell.max_memory = 100000
-cell.ke_cutoff = 100
-cell.precision = 1e-10
-cell.verbose = lib.logger.INFO
-cell.output = 'h2_2D_CASCI.log'
-cell.build()
-
-kmesh2D = [2, 2, 1] 
-kpts = cell.make_kpts(kmesh2D, wrap_around=True)
-
-kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
-kmf.max_cycle=1000
-kmf.exxdiv = None
-kmf.conv_tol = 1e-10
-kmf.kernel()
-
-# This is equivalent to (8e, 8o) in the supercell
-kmc = mcscf.CASCI(kmf, 2, 2)
-kmc.kernel(kmf.mo_coeff)
-
-
-# H2 Molecule in 2D 
-atoms2D = make_h2_2D(intraH=0.74, interH=1.5, nx = 1, ny = 1, vacuum=17.5)
-cell = pgto.Cell()
-cell.a = atoms2D.cell.array
-pos = atoms2D.get_positions()
-sym = atoms2D.get_chemical_symbols()
-cell.atom = [(sym[i], tuple(pos[i])) for i in range(len(sym))]
-cell.basis = 'STO-6G'
-cell.unit = 'Angstrom'
-cell.max_memory = 100000
-cell.ke_cutoff = 100
-cell.precision = 1e-10
-cell.verbose = lib.logger.INFO
-cell.output = 'h2_2D_CASCI.log'
-cell.build()
-
-kmesh2D = [2, 2, 1] 
-kpts = cell.make_kpts(kmesh2D, wrap_around=True)
-
-kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
-kmf.max_cycle=1000
-kmf.exxdiv = None
-kmf.conv_tol = 1e-10
-kmf.kernel()
-
-# This is equivalent to (8e, 8o) in the supercell
-kmc = mcscf.CASCI(kmf, 2, 2)
-kmc.kernel(kmf.mo_coeff)
-
-del cell, kpts, kmf, kmc
-
-# H2 Molecule in 3D 
-atoms3D = make_h2_3D(intraH=0.74, interH=1.5, nx = 1, ny = 1, nz=1, vacuum=17.5)
-cell = pgto.Cell()
-cell.a = atoms3D.cell.array
-pos = atoms3D.get_positions()
-sym = atoms3D.get_chemical_symbols()
-cell.atom = [(sym[i], tuple(pos[i])) for i in range(len(sym))]
-cell.basis = 'STO-6G'
-cell.unit = 'Angstrom'
-cell.max_memory = 100000
-cell.ke_cutoff = 100
-cell.precision = 1e-10
-cell.verbose = lib.logger.INFO
-cell.output = 'h2_3D_CASCI.log'
-cell.build()
-
-kmesh3D = [2, 2, 2] 
-kpts = cell.make_kpts(kmesh3D, wrap_around=True)
-
-kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
-kmf.max_cycle=1000
-kmf.exxdiv = None
-kmf.conv_tol = 1e-10
-kmf.kernel()
-
-# This is equivalent to (16e, 16o) in the supercell
-# Note: this is very slow: c-CASCI beyond (14e,14o) is very expensive.
-kmc = mcscf.CASCI(kmf, 2, 2)
-kmc.kernel(kmf.mo_coeff)
+print("k-RHF energy  : ", kmf.e_tot.real)
+print("k-CASCI energy: ", kmc.e_tot.real)
