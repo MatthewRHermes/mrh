f01c8721bb84c5f42624f75ca145c8762bd11b10
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-05-07 00:24:00 -0500

    Fix the init file

--- my_pyscf/pbc/fci/__init__.py
+++ my_pyscf/pbc/fci/__init__.py
@@ -2,6 +2,14 @@ from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
 from mrh.my_pyscf.pbc.fci import csf_cplx
 from mrh.my_pyscf.pbc.fci import spin_op
 from mrh.my_pyscf.pbc.fci import addons
+
+
+# Author: Bhavnesh Jangid
+
+# TODO: 
+# 1. Add the option of direct_spin0 during the initialization.
+# 2. Add the point group symmetry option.
+
 try:
     from pyscf import dmrgscf
     from mrh.my_pyscf.pbc.fci import dmrgci_cplx_helper
@@ -11,11 +19,14 @@ except ImportError:
 
 def solver(cell, singlet, symm=None):
     # Will add the singlet and symm options later.
+    if symm is not None:
+        raise NotImplementedError("Symmetry is not implemented for FCI in PBC yet.")
     return direct_spin1_cplx.FCISolver(cell)
 
 def csf_solver(cell, smult, symm=None):
-    # TODO: A separate FCISolver is causing the problem in CASSCF. Fix it later.
-    # if smult == 1:
-    #     return csf_cplx.FCISolverSpin0(cell, smult)
-    # else:
-    return csf_cplx.FCISolver(cell, smult)
+    if symm is not None:
+        raise NotImplementedError("Symmetry is not implemented for CSF-FCI in PBC yet.")
+    if smult == 1:
+        return csf_cplx.FCISolverSpin0(cell, smult)
+    else:
+        return csf_cplx.FCISolver(cell, smult)
