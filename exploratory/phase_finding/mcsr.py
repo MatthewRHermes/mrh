f3249a0671c66370e75380ea06f1e2085914037d
Matthew R Hermes <mrhermes@uchicago.edu>
2026-05-08 12:13:00 -0500

    debug and cleanup mcsr

--- exploratory/phase_finding/mcsr.py
+++ exploratory/phase_finding/mcsr.py
@@ -4,8 +4,10 @@ from scipy.sparse import linalg as sparse_linalg
 
 rng = np.random.default_rng ()
 
-def select (vec, mask):
+def select (vec, mask=None):
     ''' Select an entry from vec proportional to the magnitude of the value in mask'''
+    if mask is None:
+        return _select (vec)
     mask_idx = np.where (mask)[0]
     return mask_idx[_select (vec[mask])]
 
@@ -18,7 +20,77 @@ def _select (vec):
     idx = np.where (offs > card)[0][0]
     return idx
 
-def find_phase (a_op, a_diag, amps, maxiter=20, conv_tol=1e-10):
+class MonteCarloSignRecoverer:
+    def __init__(self, a_op, a_diag, amps, maxiter=20, conv_tol=1e-10, log=print, num_tol=1e-16):
+        if (log is None) or (log==False): log = lambda * args: None
+        self.a_op = a_op
+        self.a_diag = a_diag
+        self.amps = amps
+        self.maxiter = maxiter
+        self.conv_tol = conv_tol
+        self.log = log
+        self.v = v = amps.copy ()
+        self.av = av = a_op (v)
+        self.w = w = np.dot (v.conj ().T, av)
+        self.av = av = av - (w*v)
+        self.err = linalg.norm (av)
+        self.non0 = np.abs (amps) > num_tol
+        self.v1 = v.copy ()
+        self.it = 0
+
+    def get_ai (self, i):
+        self.v1[:] = 0
+        self.v1[i] = self.v[i]
+        return self.a_op (self.v1)
+
+    def get_step (self, i):
+        self.v1[:] = self.v
+        self.v1[i] *= -1
+        av1 = self.a_op (self.v1)
+        w1 = np.dot (self.v1.conj (), av1)
+        av1 = av1 - w1*self.v1
+        return av1, w1
+
+    def get_element (self):
+        i = select (self.av, self.non0)
+        ai = self.get_ai (i) - self.w*self.v[i]
+        vai = self.v * ai
+        vav = self.v * self.av
+        idx_i = (np.sign (vai) == np.sign (vav[i]))
+        idx_j = (np.sign (vai) == np.sign (vav))
+        mask = self.non0 & (idx_i | idx_j)
+        mask[i] = False
+        assert (np.count_nonzero (mask) > 0), '{} {} {} {}'.format (i, vai, vav, non0)
+        j = select (vav, mask)
+        return i, j
+
+    def __call__(self):
+        i = j = err_i = err_j = None
+        for self.it in range (self.maxiter):
+            self.log (self.it, self.w, self.err, i, err_i, j, err_j)
+            if self.err < self.conv_tol:
+                break
+            i, j = self.get_element ()
+            av_i, w_i = self.get_step (i)
+            av_j, w_j = self.get_step (j)
+            err_i = linalg.norm (av_i)
+            err_j = linalg.norm (av_j)
+            if (err_i > self.err) and (err_j > self.err):
+                pass
+            elif err_i < err_j:
+                self.av[:] = av_i
+                self.err = err_i
+                self.w = w_i
+                self.v[i] *= -1
+            elif err_j < err_i:
+                self.av = av_j
+                self.err = err_j
+                self.w = w_j
+                self.v[j] *= -1
+        return self.w, self.v
+
+
+def find_phase (*args, **kwargs):
     '''
     Args:
         a_op : LinearOperator of shape (n,n)
@@ -33,72 +105,20 @@ def find_phase (a_op, a_diag, amps, maxiter=20, conv_tol=1e-10):
             Contains "eigenvector" with amplitudes given by amps but with signs determined by
             the iterative algorithm
     '''
-    n = len (amps)
-    v = amps.copy ()
-    non0 = amps > 0
-    w = np.dot (v.conj ().T, a_op (v))
-    av = a_op (v) - (w*v)
-    err = linalg.norm (av)
-    v1 = v.copy ()
-    def get_ai (i):
-        v1[:] = 0
-        v1[i] = v[i]
-        return a_op (v1)
-    def get_step (i, w0=0, ai=None):
-        if ai is None: ai = get_ai (i)
-        #ai = get_ai (i)
-        #w1 = w0 - 4*np.dot (v, ai-ai[i])
-        v1[:] = v
-        v1[i] *= -1
-        #av1 = (av-(2*ai)) + (w0*v) - (w1*v1)
-        av1 = a_op (v1)
-        w1 = np.dot (v1.conj (), av1)
-        av1 = av1 - w1*v1
-        return av1, w1
-    def get_element (w):
-        i = select (av, non0)
-        ai = get_ai (i) - w*v[i]
-        idx_i = (np.sign (ai) == np.sign (av[i]))
-        idx_j = (np.sign (ai) == np.sign (av))
-        mask = non0 & (idx_i | idx_j)
-        mask[i] = False
-        assert (np.count_nonzero (mask) > 0), '{} {} {} {}'.format (i, np.sign (ai), np.sign (av), non0)
-        j = select (av, mask)
-        return i, j, ai
-    for it in range (maxiter):
-        i, j, ai = get_element (w)
-        av_i, w_i = get_step (i, w, ai)
-        av_j, w_j = get_step (j, w, ai)
-        err_i = linalg.norm (av_i)
-        err_j = linalg.norm (av_j)
-        print (it, w, err, i, err_i, j, err_j)
-        if (err_i > err) and (err_j > err):
-            continue
-        elif err_i < err_j:
-            av[:] = av_i
-            err = err_i
-            w = w_i
-            v[i] *= -1
-        elif err_j < err_i:
-            av = av_j
-            err = err_j
-            w = w_j
-            v[j] *= -1
-        if err < conv_tol:
-            print (it+1, w, err, i, err_i, j, err_j)
-            break
-    return w, v
+    return MonteCarloSignRecoverer (*args, **kwargs) ()
 
 def find_phase_matrix (hmat):
     w, v = linalg.eigh (hmat)
     h_diag = hmat.diagonal ()
     h_op = sparse_linalg.aslinearoperator (hmat)
     for i in range (len (w)):
-        w_test, v_test = find_phase (h_op, h_diag, np.abs (v[:,i]))
-        print ("result", i, w[i], w_test, np.dot (v[:,i].conj (), v_test))
+        f = MonteCarloSignRecoverer (h_op, h_diag, np.abs (v[:,i]), log=False)
+        w_test, v_test = f ()
+        print ("result", i, w[i], w_test, np.dot (v[:,i].conj (), v_test), f.err, f.it)
 
 if __name__=='''__main__''':
-    hmat = (2*rng.random ((3,3))) - 1
-    hmat += hmat.T
-    find_phase_matrix (hmat)
+    for i in range (100):
+        hmat = (2*rng.random ((3,3))) - 1
+        hmat += hmat.T
+        find_phase_matrix (hmat)
 
