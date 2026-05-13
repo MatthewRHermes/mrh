faaea59b71060211b987d5a1783658ff0bc24a78
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-05-13 11:45:58 -0500

    Remove the pbmolden

--- my_pyscf/pbc/util/pbcmolden.py
+++
@@ -1,4 +0,0 @@
-
-
-# TODO: Write a general function which takes the cell, kmesh, kpts, and mo_coeff and print the mo_coeff to the molden file or cubegen.
-# Not adding but adding the placeholder to filled later.
